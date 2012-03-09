a09b0a3d32f90b3299d2be30b324f350e2418351
kehoste <kenneth.hoste@ugent.be>
2012-03-09 16:17:13 +0100

    code cleanup of easyblocksSetup.py script

--- scripts/easyblocksSetup.py
+++ scripts/easyblocksSetup.py
@@ -11,28 +11,28 @@ import os
 import sys
 
 if len(sys.argv) > 2:
-    sys.stderr.write("Usage: %s [prefix]\n"%sys.argv[0])
+    sys.stderr.write("Usage: %s [prefix]\n" % sys.argv[0])
 
-PREFIX="easyblocks"
+PREFIX = "easyblocks"
 if len(sys.argv) == 2:
     PREFIX = sys.argv[1]
 
 # create root dir, with default init
 os.mkdir(PREFIX)
-f=open(os.path.join(PREFIX,"__init__.py"),'w')
+f = open(os.path.join(PREFIX, "__init__.py"), 'w')
 f.write("""import pkg_resources
 pkg_resources.declare_namespace("%s")
-"""%os.path.basename(PREFIX))
+""" % os.path.basename(PREFIX))
 
 # create subdirectories Aa, Bb, ..., Zz, 09, _-
 def createDir(dr):
-    os.mkdir(os.path.join(PREFIX,dr))
-    f = open(os.path.join(PREFIX,dr,"__init__.py"),'w')
+    os.mkdir(os.path.join(PREFIX, dr))
+    f = open(os.path.join(PREFIX, dr, "__init__.py"), 'w')
     f.close()
 
-alphabet=map(chr,xrange(ord('a'),ord('z')+1))
+alphabet = map(chr, xrange(ord('a'), ord('z') + 1))
 for letter in alphabet:
-    dr = "%s%s"%(letter.upper(),letter)
+    dr = "%s%s" % (letter.upper(), letter)
     createDir(dr)
 
 createDir("09")
