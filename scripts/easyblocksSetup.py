5b8ff58800f075c7c7868aa4d61274e15e6c0a46
kehoste <kenneth.hoste@ugent.be>
2012-03-09 10:10:21 +0100

    use camel case for naming, fix bug (didn't create Zz directory), clean up nasty stuff

--- scripts/easyblocksSetup.py
+++ scripts/easyblocksSetup.py
@@ -9,26 +9,31 @@ you might want to put this directory under revision control
 
 import os
 import sys
+
+if len(sys.argv) > 2:
+    sys.stderr.write("Usage: %s [prefix]\n"%sys.argv[0])
+
 PREFIX="easyblocks"
-if len(sys.argv) > 1:
+if len(sys.argv) == 2:
     PREFIX = sys.argv[1]
 
+# create root dir, with default init
 os.mkdir(PREFIX)
 f=open(os.path.join(PREFIX,"__init__.py"),'w')
 f.write("""import pkg_resources
 pkg_resources.declare_namespace("%s")
 """%os.path.basename(PREFIX))
 
-def createdir(dr):
+# create subdirectories Aa, Bb, ..., Zz, 09, _-
+def createDir(dr):
     os.mkdir(os.path.join(PREFIX,dr))
     f = open(os.path.join(PREFIX,dr,"__init__.py"),'w')
     f.close()
 
-for i in xrange(ord('a'),ord('z')):
-    dr = "%s%s"%( chr(ord('A')-ord('a')+i),chr(i))
-    createdir(dr)
+alphabet=map(chr,xrange(ord('a'),ord('z')+1))
+for letter in alphabet:
+    dr = "%s%s"%(letter.upper(),letter)
+    createDir(dr)
 
-createdir("09")
-createdir("_-")
-
-   
+createDir("09")
+createDir("_-")
