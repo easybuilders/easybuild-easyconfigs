46facd0df2b74784391e252d39405bd512010f84
kehoste <kenneth.hoste@ugent.be>
2012-03-12 08:47:58 +0100

    cleaned up buildLog.py, fixed change to tools from buildsoft

--- easybuild/tools/buildLog.py
+++ easybuild/tools/buildLog.py
@@ -1,50 +1,62 @@
 """
-Set of logging parameters
+EasyBuild logger and log utilities, including our own EasybuildError class.
 """
 
 import sys, os, logging
 from socket import gethostname
 import easybuild
 
-class myError(Exception):
-    def __init__(self, value):
-        self.value = value
+class EasyBuildError(Exception):
+    """
+    EasyBuildError is thrown when EasyBuild runs into something horribly wrong.
+    """
+    def __init__(self, msg):
+        super(EasyBuildError,self).__init__(msg)
+        self.msg = msg
     def __str__(self):
-        return repr(self.value)
+        return repr(self.msg)
 
-class newLog(logging.Logger):
-    # self.rais can be set to false disable raising the exception which is
+class EasyBuildLog(logging.Logger):
+    """
+    The EasyBuild logger, with its own error and exception functions.
+    """
+    # self.raiseError can be set to False disable raising the exception which is
     # necessary because logging.Logger.exception calls self.error
-    rais=True
+    raiseError = True
 
     def error(self,msg,*args,**kwargs):
-        newmsg="err %s %s"%(self.findCaller(),msg)
+        
+        newMsg = "err %s %s"%(self.findCaller(),msg)
         logging.Logger.error(self,msg,*args,**kwargs)
-        if self.rais:
-            raise myError(newmsg)
-    
+        if self.raiseError:
+            raise EasyBuildError(newMsg)
+
     def exception(self,msg,*args):
         ## don't raise the exception from within error
-        newmsg="exc %s %s" % (self.findCaller(), msg)
-        self.rais=False
+        newMsg="exc %s %s" % (self.findCaller(), msg)
+
+        self.raiseError=False
         logging.Logger.exception(self, msg, *args)
-        self.rais=True
-        raise myError(newmsg)
+        self.raiseError=True
 
-fm='%(asctime)s %(name)s %(levelname)s %(message)s'
-formatter=logging.Formatter(fm)
+        raise EasyBuildError(newMsg)
 
-## redirect standard handler of rootlogger to /dev/null
-logging.basicConfig(level=logging.ERROR, format=fm, filename='/dev/null')
-logging.setLoggerClass(newLog)
+# set format for logger
+loggingFormat='%(asctime)s %(name)s %(levelname)s %(message)s'
+formatter=logging.Formatter(loggingFormat)
+
+# redirect standard handler of root logger to /dev/null
+logging.basicConfig(level=logging.ERROR, format=loggingFormat, filename='/dev/null')
+
+logging.setLoggerClass(EasyBuildLog)
 
 def getLog(name=None):
     """
     Generate logger object
     """
-    tmp = logging.getLogger(name)
-    tmp.info("Logger started for %s." % name)
-    return tmp
+    log = logging.getLogger(name)
+    log.info("Logger started for %s." % name)
+    return log
 
 def removeLogHandler(hnd):
     """
@@ -55,17 +67,17 @@ def removeLogHandler(hnd):
 
 def initLogger(name=None, version=None, debug=False, filename=None, typ='UNKNOWN'):
     """
-    Return filename where the logfile is being written
-    - Does not append.
-    - Set loghandlers
+    Return filename of the log file being written
+    - does not append
+    - sets log handlers
     """
     log = logging.getLogger()
 
-    ## set loglevel
+    # set log level
     if debug:
-        defaultloglevel = logging.DEBUG
+        defaultLogLevel = logging.DEBUG
     else:
-        defaultloglevel = logging.INFO
+        defaultLogLevel = logging.INFO
 
     if (name and version) or filename:
         if not filename:
@@ -73,40 +85,48 @@ def initLogger(name=None, version=None, debug=False, filename=None, typ='UNKNOWN
         hand = logging.FileHandler(filename)
     else:
         hand = logging.StreamHandler(sys.stdout)
-        
+
     hand.setFormatter(formatter)
     log.addHandler(hand)
 
-    tmp = logging.getLogger(typ)
-    tmp.setLevel(defaultloglevel)
-    
+    log = logging.getLogger(typ)
+    log.setLevel(defaultLogLevel)
+
     ## init message
-    tmp.info("Log initialised with name %s version %s to file %s on host %s" % 
-            (name, version, filename, gethostname()))
-    tmp.info("This is EasyBuild %s" % easybuild.VERBOSE_VERSION) 
-    return filename, tmp, hand
-    
+    log.info("Log initialized with name %s version %s to file %s on host %s" % (name,
+                                                                                version,
+                                                                                filename,
+                                                                                gethostname()
+                                                                                ))
+    log.info("This is EasyBuild %s" % easybuild.VERBOSE_VERSION)
+
+    return filename, log, hand
+
 def logFilename(name, version):
     """
     Generate a filename to be used
     """
     import time
     date = time.strftime("%Y%m%d")
-    time = time.strftime("%H%M%S")
+    timeStamp = time.strftime("%H%M%S")
 
-    from easybuild.buildsoft.config import logFormat
-    name = '/tmp/' + (logFormat() % locals())
+    from easybuild.tools.config import logFormat
+    filename = '/tmp/' + (logFormat() % {'name':name,
+                                         'version':version,
+                                         'date':date,
+                                         'time':timeStamp
+                                         })
 
-    # Append numbers if the logfile already exist
+    # Append numbers if the log file already exist
     counter = 1
-    while os.path.isfile(name):
+    while os.path.isfile(filename):
         counter += 1
-        name = "%s.%d" % (name, counter)
+        filename = "%s.%d" % (filename, counter)
 
     return name
 
 if __name__ == '__main__':
     initLogger('test', '1.0.0')
-    fn, tmplog, hnd = initLogger(typ='buildLog')
-    tmplog.info('Testing buildLog...')
-    
+    fn, testlog, _ = initLogger(typ='buildLog')
+    testlog.info('Testing buildLog...')
+    print "Tested buildLog, see %s"%fn
