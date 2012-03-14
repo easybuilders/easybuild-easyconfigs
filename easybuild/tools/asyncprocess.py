f6c0fa3b011e61bd79bb4b94c713317190df7674
Jens Timmerman <jens.timmerman@ugent.be>
2012-03-14 11:26:59 +0100

    fixed license of asyncprocess.py

--- easybuild/tools/asyncprocess.py
+++ easybuild/tools/asyncprocess.py
@@ -1,26 +1,83 @@
 ##
-# Copyright 2009-2012 Stijn Deweirdt, Dries Verdegem, Kenneth Hoste, Pieter De Baets, Jens Timmerman
+# Copyright 2005 Josiah Carlson 
+# Copyright 2009 Stijn Deweirdt
+# Copyright 2011 Pieter De Baets
+# Copyright 2012 Kenneth Hoste
+
+# original code from http://code.activestate.com/recipes/440554/
+# Released under the PSF License:
 #
-# This file is part of EasyBuild,
-# originally created by the HPC team of the University of Ghent (http://ugent.be/hpc).
+# 1. This LICENSE AGREEMENT is between the Python Software Foundation
+# ("PSF"), and the Individual or Organization ("Licensee") accessing and
+# otherwise using this software ("Python") in source or binary form and
+# its associated documentation.
 #
-# http://github.com/hpcugent/easybuild
+# 2. Subject to the terms and conditions of this License Agreement, PSF
+# hereby grants Licensee a nonexclusive, royalty-free, world-wide
+# license to reproduce, analyze, test, perform and/or display publicly,
+# prepare derivative works, distribute, and otherwise use Python
+# alone or in any derivative version, provided, however, that PSF's
+# License Agreement and PSF's notice of copyright, i.e., "Copyright (c)
+# 2001, 2002, 2003, 2004, 2005, 2006 Python Software Foundation; All Rights
+# Reserved" are retained in Python alone or in any derivative version
+# prepared by Licensee.
 #
-# EasyBuild is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation v2.
+# 3. In the event Licensee prepares a derivative work that is based on
+# or incorporates Python or any part thereof, and wants to make
+# the derivative work available to others as provided herein, then
+# Licensee hereby agrees to include in any such work a brief summary of
+# the changes made to Python.
 #
-# EasyBuild is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# 4. PSF is making Python available to Licensee on an "AS IS"
+# basis. PSF MAKES NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR
+# IMPLIED. BY WAY OF EXAMPLE, BUT NOT LIMITATION, PSF MAKES NO AND
+# DISCLAIMS ANY REPRESENTATION OR WARRANTY OF MERCHANTABILITY OR FITNESS
+# FOR ANY PARTICULAR PURPOSE OR THAT THE USE OF PYTHON WILL NOT
+# INFRINGE ANY THIRD PARTY RIGHTS.
 #
-# You should have received a copy of the GNU General Public License
-# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+# 5. PSF SHALL NOT BE LIABLE TO LICENSEE OR ANY OTHER USERS OF PYTHON
+# FOR ANY INCIDENTAL, SPECIAL, OR CONSEQUENTIAL DAMAGES OR LOSS AS
+# A RESULT OF MODIFYING, DISTRIBUTING, OR OTHERWISE USING PYTHON,
+# OR ANY DERIVATIVE THEREOF, EVEN IF ADVISED OF THE POSSIBILITY THEREOF.
+#
+# 6. This License Agreement will automatically terminate upon a material
+# breach of its terms and conditions.
+#
+# 7. Nothing in this License Agreement shall be deemed to create any
+# relationship of agency, partnership, or joint venture between PSF and
+# Licensee. This License Agreement does not grant permission to use PSF
+# trademarks or trade name in a trademark sense to endorse or promote
+# products or services of Licensee, or any third party.
+#
+# 8. By copying, installing or otherwise using Python, Licensee
+# agrees to be bound by the terms and conditions of this License
+# Agreement.
 ##
-"""
-Asynchronous communication with process
-see http://code.activestate.com/recipes/440554/ (PSF license)
+
+"""Module to allow Asynchronous subprocess use on Windows and Posix platforms 
+
+The 'subprocess' module in Python 2.4 has made creating and accessing subprocess
+streams in Python relatively convenient for all supported platforms,
+but what if you want to interact with the started subprocess? 
+That is, what if you want to send a command, read the response, 
+and send a new command based on that response?
+
+Now there is a solution.
+The included subprocess.Popen subclass adds three new commonly used methods:
+ recv(maxsize=None)
+ recv_err(maxsize=None)
+ and send(input)
+ 
+along with a utility method:
+ send_recv(input='', maxsize=None).
+
+recv() and recv_err() both read at most maxsize bytes from the started subprocess.
+send() sends strings to the started subprocess. send_recv() will send the provided input,
+and read up to maxsize bytes from both stdout and stderr.
+
+If any of the pipes are closed, the attributes for those pipes will be set to None,
+and the methods will return None.
+
 - downloaded 05/08/2010
 - modified
 -- added STDOUT handle
