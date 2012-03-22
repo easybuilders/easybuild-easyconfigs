49df5e4a584da831db387b6f6848b514e8042fee
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-03-22 18:15:01 +0100

    fix path for lib64 libraries in sanitycheck

--- easybuild/easyblocks/g/GCC.py
+++ easybuild/easyblocks/g/GCC.py
@@ -273,7 +273,7 @@ class GCC(Application):
                 self.log.error("Sanity check needs to be updated for CLooG/PPL?!?")
 
             bin_files = ["bin/%s"%x for x in bin_files]
-            lib64_files = ["lib64/%s/%s" % (common_infix, x) for x in lib64_files]
+            lib64_files = ["lib64/%s" % x for x in lib64_files]
             libexec_files = ["libexec/%s/%s" % (common_infix, x) for x in libexec_files]
 
             self.setcfg('sanityCheckPaths',{'files':bin_files + lib64_files + libexec_files,
