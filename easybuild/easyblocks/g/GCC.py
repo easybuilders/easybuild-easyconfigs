58f651c1e0c2aa6b083ea7083b45242ead4b349d
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-03-27 18:14:42 +0200

    fix CLooG/PPL support for stand-alone GCC build (in 3-stage build) make bootstrap build non-optional, since it's required for stand-alone build add check for unrecognized configure options add version checks for CLooG/PPL

--- easybuild/easyblocks/g/GCC.py
+++ easybuild/easyblocks/g/GCC.py
@@ -1,4 +1,4 @@
-import os, shutil
+import re, os, shutil
 from copy import copy
 from distutils.version import LooseVersion
 
@@ -8,44 +8,49 @@ from easybuild.tools.filetools import run_cmd
 class GCC(Application):
     """
     Self-contained build of GCC.
-    Uses system compiler for initial build, then bootstraps (by default).
+    Uses system compiler for initial build, then bootstraps.
     """
     def __init__(self, *args, **kwargs):
         Application.__init__(self, *args, **kwargs)
 
         self.cfg.update({'languages':[[], "List of languages to build GCC for (--enable-languages) [default: []]"],
-                         'bootstrap':[True, "Enable bootstrapping (--enable-bootstrap)."],
                          'withcloog':[False, "Build GCC with CLooG support (default: False)."],
                          'withppl':[False, "Build GCC with PPL support (default: False)."],
+                         'pplwatchdog':[False, "Enable PPL watchdog (default: False)"],
+                         'clooguseisl':[False, "Use ISL with CLooG or not (use PPL otherwise) (default: False)"]
                          }
                         )
 
         self.stagedbuild = False
 
-    def create_obj_dir(self):
+    def create_dir(self, dirname):
         """
-        Create an obj dir to build in.
+        Create a dir to build in.
         """
-        
-        objdir=os.path.join(self.getcfg('startfrom'),'obj')
+        dirpath=os.path.join(self.getcfg('startfrom'),dirname)
         try:
-            while os.path.exists(objdir):
-                # for 2-staged build, create a separate obj dir
-                objdir = "%s_" % objdir
-            os.mkdir(objdir)
-            #self.setcfg('startfrom', objdir)
-            #os.chdir(self.getcfg('startfrom'))
-            os.chdir(objdir)
+            os.mkdir(dirpath)
+            os.chdir(dirpath)
+            self.log.debug("Created dir at %s" % dirpath)
+            return dirpath
         except OSError, err:
-            self.log.exception("Can't use obj dir %s to build in: %s"%(objdir, err))
+            self.log.exception("Can't use dir %s to build in: %s"%(dirpath, err))
 
-    def prep_extra_src_dirs(self, stage="stage1"):
+    def prep_extra_src_dirs(self, stage, target_prefix=None):
         """
         Prepare extra (optional) source directories, so GCC will build these as well. 
         """
 
+        known_stages = ["stage1", "stage2", "stage3"]
+        if not stage in known_stages:
+            self.log.error("Incorrect argument for prep_extra_src_dirs, should be one of: %s" % known_stages)
+
         configopts = ''
-        extra_src_dirs = ["gmp", "mpfr", "mpc"]
+        if stage == "stage2":
+            # no MPFR/MPC needed in stage 2
+            extra_src_dirs = ["gmp"]
+        else:
+            extra_src_dirs = ["gmp", "mpfr", "mpc"]
 
         ## add optional ones that were selected (e.g. CLooG, PPL, ...)
         for x in ["cloog", "ppl"]:
@@ -61,8 +66,9 @@ class GCC(Application):
                 extra_src_dirs.remove(extra)
             elif extra in ["cloog", "ppl"] and stage == "stage1":
                 ## building CLooG or PPL requires a recent compiler
-                ## our best bet is to do a 2-staged build of GCC, and
+                ## our best bet is to do a 3-staged build of GCC, and
                 ## build CLooG/PPL with the GCC we're building in stage 2
+                ## then (bootstrap) build GCC in stage 3
                 self.stagedbuild=True
                 extra_src_dirs.remove(extra)
 
@@ -70,6 +76,7 @@ class GCC(Application):
         # these sources should be included in list of sources in .eb spec file,
         # so EasyBuild can unpack them in the build dir
         found_src_dirs = []
+        versions = {}
         all_dirs = os.listdir(self.builddir)
         for d in all_dirs:
             for sd in extra_src_dirs:
@@ -77,6 +84,9 @@ class GCC(Application):
                     found_src_dirs.append({'source_dir':d,
                                            'target_dir':sd
                                            })
+                    # expected format: name-[subname]-version
+                    ver = os.path.basename(d).split('-')[-1]
+                    versions.update({sd:ver})
 
         # we need to find all dirs specified, or else...
         if not len(found_src_dirs) == len(extra_src_dirs):
@@ -85,7 +95,10 @@ class GCC(Application):
         # copy to a dir with name as expected by GCC build framework
         for d in found_src_dirs:
             src = os.path.join(self.builddir, d['source_dir'])
-            dst = os.path.join(self.getcfg('startfrom'), d['target_dir'])
+            if target_prefix:
+                dst = os.path.join(target_prefix, d['target_dir'])
+            else:
+                dst = os.path.join(self.getcfg('startfrom'), d['target_dir'])
             if not os.path.exists(dst):
                 try:
                     shutil.copytree(src, dst)
@@ -97,7 +110,27 @@ class GCC(Application):
 
         self.log.debug("Prepared extra src dirs for %s: %s (configopts: %s)" % (stage, found_src_dirs, configopts))
 
-        return configopts
+        return {
+                'configopts':configopts,
+                'versions':versions
+                }
+
+    def run_configure_cmd(self, cmd):
+        """
+        Run a configure command, with some extra checking (e.g. for unrecognized options).
+        """
+        (out, ec) = run_cmd(cmd, log_all=True, simple=False)
+
+        if ec != 0:
+            self.log.error("Command '%s' exited with exit code != 0 (%s)" % (cmd, ec))
+
+        # configure scripts tend to simply ignore unrecognized options
+        # we should be more strict here, because GCC is very much a moving target
+        unknown_re = re.compile("WARNING: unrecognized options")
+
+        unknown_options = unknown_re.findall(out)
+        if unknown_options:
+            self.log.error("Unrecognized options found during configure: %s" % unknown_options)
 
     def configure(self):
         """
@@ -112,21 +145,17 @@ class GCC(Application):
             self.log.warning("\n\nWARNING: This build procedure has only been well tested for GCC v4.6.x.\n" +
                              "If you run into any problems, please contact the EasyBuild developers.\n")
 
-        # self.configopts will be reused in a 2-staged build,
+        # self.configopts will be reused in a 3-staged build,
         # configopts is only used in first configure
         self.configopts = self.getcfg('configopts')
 
-        #
         # I) prepare extra source dirs, e.g. for GMP, MPFR, MPC (if required), so GCC can build them
-        #
+        stage1_info = self.prep_extra_src_dirs("stage1")
+        configopts = stage1_info['configopts']
 
-        configopts = self.prep_extra_src_dirs()
-
-        #
         # II) create obj dir to build in, and change to it
         #     GCC doesn't like to be built in the source dir
-        #
-        self.create_obj_dir()
+        self.stage1prefix = self.create_dir("stage1_obj")
 
         # III) update config options
 
@@ -134,13 +163,10 @@ class GCC(Application):
         if self.getcfg('languages'):
             self.configopts += " --enable-languages=%s" % ','.join(self.getcfg('languages'))
 
-        ## enable bootstrapping if desired
-        if self.getcfg('bootstrap') and not self.stagedbuild:
+        ## enable bootstrap build for self-containment (unless for staged build)
+        if not self.stagedbuild:
             configopts += " --enable-bootstrap"
         else:
-            if not self.stagedbuild:
-                self.log.info("WARNING: Building without bootstrapping, " + 
-                              "so this GCC build will be dependent on system libraries!")
             configopts += " --disable-bootstrap"
 
         ## configure for a release build
@@ -157,9 +183,12 @@ class GCC(Application):
         self.configopts += " --enable-ld --with-plugin-ld=ld.gold"
 
         if self.stagedbuild:
-            self.log.info("Starting with stage 1 of 2-staged build to enable CLooG and/or PPL support...")
-            self.stage1dir = os.path.join(self.builddir, 'GCC_stage1_eb')
-            configopts += " --prefix=%(p)s --with-local-prefix=%(p)s" % {'p' : self.stage1dir}
+            #
+            # STAGE 1: configure GCC build that will be used to build PPL/CLooG
+            #
+            self.log.info("Starting with stage 1 of 3-staged build to enable CLooG and/or PPL support...")
+            self.stage1installdir = os.path.join(self.builddir, 'GCC_stage1_eb')
+            configopts += " --prefix=%(p)s --with-local-prefix=%(p)s" % {'p' : self.stage1installdir}
 
         else:
             # unstaged build, so just run standard configure/make/make install
@@ -173,7 +202,7 @@ class GCC(Application):
                                            self.configopts,
                                            configopts
                                           )
-        run_cmd(cmd, log_all=True, simple=True)
+        self.run_configure_cmd(cmd)
 
     def make(self):
 
@@ -190,49 +219,160 @@ class GCC(Application):
             cmd = "make install %s" % (self.getcfg('installopts'))
             run_cmd(cmd, log_all=True, simple=True)
 
-            # register built GCC as compiler to use for stage 2
-            path = "%s/bin:%s"%(self.stage1dir, os.getenv('PATH'))
+            # register built GCC as compiler to use for stage 2/3
+            path = "%s/bin:%s"%(self.stage1installdir, os.getenv('PATH'))
             os.putenv('PATH', path)
 
             ld_lib_path = "%(dir)s/lib64:%(dir)s/lib:%(val)s"% {
-                                      'dir':self.stage1dir, 
+                                      'dir':self.stage1installdir, 
                                       'val':os.getenv('LD_LIBRARY_PATH')
                                       }
             os.putenv('LD_LIBRARY_PATH', ld_lib_path)
 
-            # create new obj dir and change into it
-            self.create_obj_dir()
+            #
+            # STAGE 2: build GMP/PPL/CLooG for stage 3
+            #
 
-            # reconfigure for stage 2 build
-            self.log.info("Stage 1 of 2-staged build completed, continuing with stage 2 (with CLooG and/or PPL support enabled)...")
+            ## create dir to build GMP/PPL/CLooG in
+            stage2dir = "stage2_stuff"
+            stage2prefix = self.create_dir(stage2dir)
 
-            configopts = self.prep_extra_src_dirs(stage="stage2")
+            ## prepare directories to build GMP/PPL/CLooG
+            stage2_info = self.prep_extra_src_dirs("stage2", target_prefix=stage2prefix)
+            configopts = stage2_info['configopts']
+
+            ## build PPL and CLooG (GMP as dependency)
+
+            for lib in ["gmp", "ppl", "cloog"]:
+
+                self.log.debug("Building %s in stage 2" % lib)
+
+                if lib == "gmp" or self.getcfg('with%s' % lib):
+
+                    libdir = os.path.join(stage2prefix, lib)
+                    try:
+                        os.chdir(libdir)
+                    except OSError,err:
+                        self.log.error("Failed to change to %s: %s" % (libdir, err))
+
+                    if lib == "gmp":
+
+                        cmd = "./configure --prefix=%s " % stage2prefix
+                        cmd += "--with-pic --disable-shared --enable-cxx"
+
+                    elif lib == "ppl":
+
+                        self.pplver = LooseVersion(stage2_info['versions']['ppl'])
+
+                        cmd="./configure --prefix=%s --with-pic -disable-shared " % stage2prefix
+
+                        ### enable watchdog (or not)
+                        if self.pplver <= LooseVersion("0.11"):
+                            if self.getcfg('pplwatchdog'):
+                                cmd += "--enable-watchdog "
+                            else:
+                                cmd += "--disable-watchdog "
+                        elif self.getcfg('pplwatchdog'):
+                            self.log.error("Enabling PPL watchdog only supported in PPL <= v0.11 .")
+
+                        ### make sure GMP we just built is found
+                        cmd+="--with-gmp=%s " % stage2prefix
+
+                    elif lib == "cloog":
+
+                        self.cloogver = LooseVersion(stage2_info['versions']['cloog'])
+                        v0_15 = LooseVersion("0.15")
+                        v0_16 = LooseVersion("0.16")
+
+                        cmd = "./configure --prefix=%s --with-pic --disable-shared " % stage2prefix
+                        ### use isl or PPL
+                        if self.getcfg('clooguseisl'):
+                            if self.cloogver >= v0_16:
+                                cmd += "--with-isl=bundled "
+                            else:
+                                self.log.error("Using ISL is only supported in CLooG >= v0.16 (detected v%s)." % self.cloogver)
+                        else:
+                            if self.cloogver >= v0_15 and self.cloogver < v0_16:
+                                cmd += "--with-ppl=%s " % stage2prefix
+                            else:
+                                errormsg = "PPL only supported with CLooG v0.15.x (detected v%s)" % self.cloogver
+                                errormsg += "\nNeither using PPL or ISL-based ClooG, I'm out of options..."
+                                self.log.error(errormsg)
+
+                        ### make sure GMP is found
+                        cmd += "--with-gmp=%s " % stage2prefix
+
+                    else:
+                        self.log.error("Don't know how to configure for %s" % lib)
+
+                    ### configure
+                    self.run_configure_cmd(cmd)
+
+                    ### build and 'install'
+                    cmd="make %s install" % paracmd
+                    run_cmd(cmd, log_all=True, simple=True)
+
+                    if lib == "gmp":
+                        ### make sure correct GMP is found
+                        cppflags = os.getenv('CPPFLAGS','')
+                        os.putenv('CPPFLAGS', "%s -L%s -I%s " % (cppflags, 
+                                                                os.path.join(stage2prefix, 'lib'),
+                                                                os.path.join(stage2prefix, 'include')
+                                                                )
+                                  )
+
+            #
+            # STAGE 3: bootstrap build of final GCC (with PPL/CLooG support)
+            #
+            
+            ## create new obj dir and change into it
+            self.create_dir("stage3_obj")
+
+            ## reconfigure for stage 3 build
+            self.log.info("Stage 2 of 3-staged build completed, continuing with stage 2 (with CLooG and/or PPL support enabled)...")
+
+            configopts = self.prep_extra_src_dirs("stage3")
             configopts += " --prefix=%(p)s --with-local-prefix=%(p)s" % {'p' : self.installdir }
 
-            if self.getcfg('bootstrap'):
-                configopts += " --enable-bootstrap"
-            else:   
-                self.log.info("WARNING: Building without bootstrapping, " + 
-                      "so this GCC build will be dependent on system libraries!")
-                configopts += " --disable-bootstrap"
+            ## enable bootstrapping for self-containment
+            configopts += " --enable-bootstrap "
 
+            ## PPL config options
+            if self.getcfg('withppl'):
+                ### for PPL build and CLooG-PPL linking
+                libstdcxxpath = "%s/lib64/libstdc++.a" % self.stage1installdir
+                configopts += "--with-host-libstdcxx='-static-libgcc %s -lm' " % libstdcxxpath
+
+                configopts += "--with-ppl=%s " % stage2prefix
+
+                if self.pplver <= LooseVersion("0.11"):
+                    if self.getcfg('pplwatchdog'):
+                        configopts += "--enable-watchdog "
+                    else:
+                        configopts += "--disable-watchdog "
+
+            ## CLooG config options
+            if self.getcfg('withcloog'):
+                configopts += "--with-cloog=%s " % stage2prefix
+
+                if self.getcfg('clooguseisl') and self.cloogver >= LooseVersion("0.16"):
+                    cmd+="--enable-cloog-backend=isl "
+
+            # configure
             cmd = "%s ../configure %s %s" % (
                                              self.getcfg('preconfigopts'),
                                              self.configopts,
                                              configopts
                                              )
-            run_cmd(cmd, log_all=True, simple=True)
+            self.run_configure_cmd(cmd)
 
-        else:
-            # unstaged build, so just run standard configure/make/make install
+        # build with bootstrapping for self-containment
+        self.setcfg('makeopts', '%s bootstrap' % self.getcfg('makeopts'))
 
-            if self.getcfg('bootstrap'):
-                self.setcfg('makeopts', '%s bootstrap' % self.getcfg('makeopts'))
-
-        # call original make
+        # call standard make
         Application.make(self)
 
-    # make install is just default makeInstall, nothing special there
+    # make install is just standard makeInstall, nothing special there
 
     def sanitycheck(self):
         """
@@ -268,9 +408,6 @@ class GCC(Application):
 
             if 'lto' in self.getcfg('languages'):
                 libexec_files.extend(['liblto_plugin.so', 'lto1', 'lto-wrapper'])
-
-            if self.getcfg('withcloog') or self.getcfg('withppl'):
-                self.log.error("Sanity check needs to be updated for CLooG/PPL?!?")
 
             bin_files = ["bin/%s"%x for x in bin_files]
             lib64_files = ["lib64/%s" % x for x in lib64_files]
