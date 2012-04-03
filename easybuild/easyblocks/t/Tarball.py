f2d8b11d471806cc3464bcf2796f8b65c6fc5de8
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-04-03 16:18:02 +0200

    port support for installing precompiled software from tarball in Tarball easyblock (refs #248)

---
+++ easybuild/easyblocks/t/Tarball.py
@@ -0,0 +1,33 @@
+import shutil
+from easybuild.framework.application import Application
+
+class Tarball(Application):
+    """
+    Precompiled software packaged as tarball: 
+    - will unpack binary and copy it to the install dir
+    """
+
+    def configure(self):
+        """
+        Dummy configure
+        """
+        pass
+
+    def make(self):
+        """
+        Dummy make: nothing to build
+        """
+        pass
+
+    def make_install(self):
+
+        src=self.getcfg('startfrom')
+        # shutil.copytree cannot handle destination dirs that exist already.
+        # On the other hand, Python2.4 cannot create entire paths during copytree.
+        # Therefore, only the final directory is deleted.
+        shutil.rmtree(self.installdir)
+        try:
+            # self.getcfg('keepsymlinks') is False by default except when explicitly put to True in .eb file
+            shutil.copytree(src,self.installdir, symlinks=self.getcfg('keepsymlinks'))
+        except:
+            self.log.exception("Copying %s to installation dir %s failed"%(src,self.installdir))
