6c5a436c118b2b13565fec110595359af7660ce5
Jens Timmerman <jens.timmerman@ugent.be>
2012-04-06 17:43:42 +0200

    added get_cpu_count in systemtools and use it

---
+++ easybuild/tools/systemtools.py
@@ -0,0 +1,103 @@
+#from http://stackoverflow.com/questions/1006289/how-to-find-out-the-number-of-cpus-in-python/1006301#1006301
+"""
+This module contains some usefull functions for getting system information
+"""
+import os, re, subprocess
+def  get_cpu_count():
+    """ Number of virtual or physical CPUs on this system, i.e.
+    user/real as output by time(1) when called with an optimally scaling
+    userspace-only program"""
+
+    # Python 2.6+
+    try:
+        import multiprocessing
+        return multiprocessing.cpu_count()
+    except (ImportError, NotImplementedError):
+        pass
+
+    # POSIX
+    try:
+        res = int(os.sysconf('SC_NPROCESSORS_ONLN'))
+
+        if res > 0:
+            return res
+    except (AttributeError, ValueError):
+        pass
+
+    # Windows
+    try:
+        res = int(os.environ['NUMBER_OF_PROCESSORS'])
+
+        if res > 0:
+            return res
+    except (KeyError, ValueError):
+        pass
+
+    # jython
+    try:
+        from java.lang import Runtime
+        runtime = Runtime.getRuntime()
+        res = runtime.availableProcessors()
+        if res > 0:
+            return res
+    except ImportError:
+        pass
+
+    # BSD
+    try:
+        sysctl = subprocess.Popen(['sysctl', '-n', 'hw.ncpu'],
+                                      stdout=subprocess.PIPE)
+        scstdout = sysctl.communicate()[0]
+        res = int(scstdout)
+
+        if res > 0:
+            return res
+    except (OSError, ValueError):
+        pass
+
+    # Linux
+    try:
+        res = open('/proc/cpuinfo').read().count('processor\t:')
+
+        if res > 0:
+            return res
+    except IOError:
+        pass
+
+    # Solaris
+    try:
+        pseudodevices = os.listdir('/devices/pseudo/')
+        expr = re.compile('^cpuid@[0-9]+$')
+
+        res = 0
+        for pdev in pseudodevices:
+            if expr.match(pdev) != None:
+                res += 1
+
+        if res > 0:
+            return res
+    except OSError:
+        pass
+
+    # Other UNIXes (heuristic)
+    try:
+        try:
+            dmesg = open('/var/run/dmesg.boot').read()
+        except IOError:
+            dmesgprocess = subprocess.Popen(['dmesg'], stdout=subprocess.PIPE)
+            dmesg = dmesgprocess.communicate()[0]
+
+        res = 0
+        while '\ncpu' + str(res) + ':' in dmesg:
+            res += 1
+
+        if res > 0:
+            return res
+    except OSError:
+        pass
+
+    raise SystemException('Can not determine number of CPUs on this system')
+
+class SystemException(Exception):
+    """raised when a system (os) specific thing went wrong"""
+
