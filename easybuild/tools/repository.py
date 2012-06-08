3bb32fe2ac97e3c00b6eb3d046bc4b670324ebe1
Andy Georges <andy.georges@ugent.be>
2012-06-08 11:09:33 +0200

    fix git import check

--- easybuild/tools/repository.py
+++ easybuild/tools/repository.py
@@ -181,7 +181,7 @@ class GitRepository(Repository):
         self.remote = None
 
         # check whether git module was loaded (globally)
-        if not 'git' in sys.modules or not locals()['git'] == sys.modules['git']:
+        if not 'git' in sys.modules or not (locals().has_key('git') and locals()['git'] == sys.modules['git']):
             log.exception("Failed to load GitPython. Make sure it is installed "
                           "properly. Run 'python -c \"import git\"' to test.")
 
