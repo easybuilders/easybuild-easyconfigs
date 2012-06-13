cf1ff85bdb3b6ceec1150965a202f8718ff78031
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-06-13 11:03:11 +0200

    complete porting script: check except blocks and PyLint warnings/errors

--- easybuild/scripts/port_easyblock.py
+++ easybuild/scripts/port_easyblock.py
@@ -33,7 +33,6 @@ It checks (and fixes, if needed and possible) whether:
 
 usage: check_code_cleanup.py
 """
-
 import re
 import os
 import shutil
@@ -45,6 +44,16 @@ def error(msg):
     sys.stderr.write("ERROR: %s\n" % msg)
     sys.exit(1)
 
+try:
+    import pylint.lint
+except ImportError:
+    error("Failed to import PyLint module, I need it.")
+
+# warning function
+def warning(msg):
+    """Warning function: print message to stderr."""
+    sys.stderr.write("WARNING: %s\n" % msg)
+
 # ensure lowercase module name
 def rename_module(path):
     """Rename module is it's not lowercase."""
@@ -52,14 +61,13 @@ def rename_module(path):
         if os.path.isfile(path):
             d = os.path.dirname(path)
             name = os.path.basename(path)
-            print name
             if name != name.lower():
                 shutil.move(os.path.join(d, name),
                             os.path.join(d, name.lower()))
-                print "Module name was not lowercase, fixed that for you."
+                warning("Module name was not lowercase, fixed that for you.")
                 return os.path.join(d, name.lower())
             else:
-                print "Module naming OK."
+                print "Module naming OK.\n"
                 return path
         else:
             error("Specified easyblock %s not found!")
@@ -92,6 +100,7 @@ def refactor(txt):
                     'findPackagePatches':'find_package_patches',
                     'genInstallDir':'gen_installdir',
                     'getCfg':'getcfg',
+                    'getSoftwareRoot':'get_software_root',
                     'getInstance':'get_instance',
                     'importCfg':'process_ebfile',
                     'logall':'log_all',
@@ -131,6 +140,8 @@ def refactor(txt):
 
     totn = 0
 
+    print "Refactoring..."
+
     for old, new in refactor_map.items():
 
         regexp = re.compile("^(.*\W)%s(\W.*)$" % old, re.M)
@@ -143,15 +154,94 @@ def refactor(txt):
 
         print "%s => %s (%d), " % (old, new, n),
 
-    print "\nreplaced %d names in total" % totn
+    print ""
+
+    if totn > 0:
+        warning("replaced %d names in total\n" % totn)
+    else:
+        print "replaced %d names in total\n" % totn
 
     return txt
 
 # check for use of Exception in except blocks, or lack of error class specification
 def check_exception(txt):
-    return txt
+    """Check for lack of error specification in except block, or use of general Exception class."""
 
-# 
+    print "Checking except blocks..."
+
+    empty_except_re = re.compile("except\s*:")
+    exception_re = re.compile("except\s*Exception")
+
+    if empty_except_re.search(txt) or exception_re.search(txt):
+        warning("One or multiple except blocks found that don't specify an error class or use Exception.\n")
+        return False
+    else:
+        print "All except blocks seem to be OK!\n"
+        return True
+
+# inspiration: http://stackoverflow.com/questions/2028268/invoking-pylint-programmatically
+
+# dummy output stream for PyLint
+class WritableObject(object):
+    "dummy output stream for PyLint"
+    def __init__(self):
+        self.content = []
+    def write(self, st):
+        "dummy write"
+        self.content.append(st)
+    def read(self):
+        "dummy read"
+        return self.content
+
+# check whether PyLint still reports warnings or error
+def run_pylint(fn):
+
+    from pylint.reporters.text import TextReporter
+
+    print "checking for PyLint warnings or errors..."
+
+    # run PyLint on file, catch output
+    pylint_output = WritableObject()
+    pylint.lint.Run([fn, "-r", "n"], reporter=TextReporter(pylint_output), exit=False)
+
+    # count number of warnings/errors
+    warning_re = re.compile("^W:")
+    error_re = re.compile("^E:")
+
+    # warnings/errors we choose to ignore
+    ignores_re = [
+                  re.compile("^W:\s*[0-9,]*:[A-Za-z]*.configure: Arguments number differs from overridden method"),
+                  re.compile("^W:\s*[0-9,]*:[A-Za-z]*.make: Arguments number differs from overridden method")
+                  ]
+
+    warning_cnt = 0
+    error_cnt = 0
+    for line in pylint_output.read():
+        ignore = False
+        for ignore_re in ignores_re:
+            if ignore_re.match(line):
+                print "Ignoring %s" % line
+                ignore = True
+        if ignore:
+            continue
+        # check for warnings
+        if warning_re.match(line):
+            warning("PyLint: %s" % line)
+            warning_cnt += 1
+        # check for errors
+        if error_re.match(line):
+            warning("PyLint: %s" % line)
+            error_cnt += 1
+
+    print
+    if warning_cnt > 0 or error_cnt > 0:
+        warning("PyLint is still reporting warnings (%d) and/or errors (%d).\n" % (warning_cnt, error_cnt))
+        return False
+
+    else:
+        print "No warnings or errors reported by PyLint we care about, nice job!\n"
+        return True 
+
 # MAIN
 # 
 
@@ -165,7 +255,7 @@ else:
 # determine EasyBuild home dir, assuming this script is in <EasyBuild home>/easybuild/scripts
 easybuild_home = os.path.sep.join(os.path.abspath(sys.argv[0]).split(os.path.sep)[:-3])
 
-print "Found EasyBuild home: %s" % easybuild_home
+print "Found EasyBuild home: %s\n" % easybuild_home
 
 # rename module if needed
 easyblock = rename_module(easyblock)
@@ -179,7 +269,6 @@ except IOError, err:
     error("Failed to read easyblock %s: %s" % (easyblock, err))
 
 # refactor
-print "Refactoring..."
 easyblock_txt = refactor(easyblock_txt)
 
 all_checks = []
@@ -194,6 +283,9 @@ try:
     f.close()
 except IOError, err:
     error("Failed to write refactored easyblock %s: %s" % (easyblock, err))
+
+# check for PyLint warnings/errors
+all_checks.append(run_pylint(easyblock))
 
 if not all(all_checks):
     error("One or multiple checks have failed, easyblock %s is not fully cleaned up yet!" % easyblock)
