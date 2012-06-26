f2d1ff88ce674585afc821f195087393c0030b55
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-06-26 13:18:48 +0200

    implement UFC easyblock, clean up UFC example easyconfig, add example easyconfig for ictce

---
+++ easybuild/easyblocks/u/ufc.py
@@ -0,0 +1,74 @@
+# Copyright 2012 Kenneth Hoste
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of the University of Ghent (http://ugent.be/hpc).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+from distutils.version import LooseVersion
+import os
+from easybuild.easyblocks.c.cmakepythonpackage import CMakePythonPackage
+from easybuild.tools.modules import get_software_root
+
+class UFC(CMakePythonPackage):
+    """Support for building UFC."""
+
+    def configure(self):
+        """Set some extra environment variables before configuring."""
+
+        # make sure that required dependencies are loaded
+        deps = ['Boost', 'Python', 'SWIG']
+        depsdict = {}
+        for dep in deps:
+            deproot = get_software_root(dep)
+            if not deproot:
+                self.log.error("Dependency %s not available." % dep)
+            else:
+                depsdict.update({dep:deproot})
+
+        # SWIG version more recent than 2.0.4 have a regression
+        # which causes problems with DOLFIN if UFC was built with it
+        # fixed in 2.0.7? see https://bugs.launchpad.net/dolfin/+bug/996398
+        if LooseVersion(os.environ['SOFTVERSIONSWIG']) > '2.0.4':
+            self.log.error("Using bad version of SWIG, expecting swig <= 2.0.4." \
+                           " See https://bugs.launchpad.net/dolfin/+bug/996398")
+
+        self.pyver = ".".join(os.getenv('SOFTVERSIONPYTHON').split(".")[:-1])
+
+        self.updatecfg('configopts', "-DBoost_DIR=%s" % depsdict['Boost'])
+        self.updatecfg('configopts', "-DBOOST_INCLUDEDIR=%s/include" % depsdict['Boost'])
+        self.updatecfg('configopts', "-DBoost_DEBUG=ON -DBOOST_ROOT=%s" % depsdict['Boost'])
+
+        self.updatecfg('configopts', '-DUFC_ENABLE_PYTHON:BOOL=ON -DSWIG_FOUND:BOOL=ON')
+        self.updatecfg('configopts', '-DPYTHON_LIBRARY=%s/lib/libpython%s.so' % (depsdict['Python'],
+                                                                                 self.pyver))
+        self.updatecfg('configopts', '-DPYTHON_INCLUDE_PATH=%s/include/python%s' % (depsdict['Python'],
+                                                                                    self.pyver))
+
+        CMakePythonPackage.configure(self)
+
+    def sanitycheck(self):
+        """Custom sanity check for UFC."""
+
+        if not self.getcfg('sanityCheckPaths'):
+
+            self.setcfg('sanityCheckPaths', {
+                                             'files': ['include/ufc.h'],
+                                             'dirs':['lib/python%s/site-packages/ufc_utils/' % self.pyver]
+                                             })
+
+            self.log.info("Customized sanity check paths: %s" % self.getcfg('sanityCheckPaths'))
+
+        CMakePythonPackage.sanitycheck(self)
