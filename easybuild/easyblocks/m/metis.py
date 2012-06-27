abb8b202ce278d0f56e52cf398955d93aef780e5
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-06-27 08:41:59 +0200

    test (with v 5.0.2 and 4.0.1) and fix METIS easyblock, add example easyconfigs

--- easybuild/easyblocks/m/metis.py
+++ easybuild/easyblocks/m/metis.py
@@ -19,63 +19,100 @@
 # along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
+from distutils.version import LooseVersion
 import os
 import shutil
 from easybuild.framework.application import Application
-from easybuild.tools.filetools import mkdir
+from easybuild.tools.filetools import run_cmd, mkdir
 
 class METIS(Application):
     """Support for building METIS."""
 
     def configure(self, *args, **kwargs):
-        pass
+        """Configure build using 'make config' (only for recent versions)."""
+
+        if LooseVersion(self.version()) >= LooseVersion("5"):
+
+            cmd = "make config prefix=%s" % self.installdir
+            run_cmd(cmd, log_all=True, simple=True)
+
+    def make(self):
+        """Add make options before building."""
+
+        self.updatecfg('makeopts', 'LIBDIR=""')
+
+        if self.tk.opts['pic']:
+            self.updatecfg('makeopts', 'CC="$CC -fPIC"')
+
+        Application.make(self)
 
     def make_install(self):
-        """Manually copy the required files to the right place.
+        """Install by manually copy files to install dir, for old versions,
+        or by running 'make install' for new versions.
         
-        And create symlinks where expected by other applications
+        Create symlinks where expected by other applications
         (in Lib instead of lib)"""
-        libdir = os.path.join(self.installdir, 'lib')
-        mkdir(libdir)
 
-        includedir = os.path.join(self.installdir, 'include')
-        mkdir(includedir)
+        if LooseVersion(self.version()) < LooseVersion("5"):
 
-        # copy libraries
-        try:
-            src = os.path.join(self.getcfg('startfrom'), 'libmetis.a')
-            dst = os.path.join(libdir, 'libmetis.a')
-            shutil.copy2(src, dst)
-        except OSError, err:
-            self.log.error("Copying file libmetis.a to lib dir failed: %s" % err)
-
-        # copy include files
-        try:
-            for f in ['defs.h', 'macros.h', 'metis.h', 'proto.h', 'rename.h', 'struct.h']:
-                src = os.path.join(self.getcfg('startfrom'), 'Lib', f)
-                dst = os.path.join(includedir, f)
+            libdir = os.path.join(self.installdir, 'lib')
+            mkdir(libdir)
+    
+            includedir = os.path.join(self.installdir, 'include')
+            mkdir(includedir)
+    
+            # copy libraries
+            try:
+                src = os.path.join(self.getcfg('startfrom'), 'libmetis.a')
+                dst = os.path.join(libdir, 'libmetis.a')
                 shutil.copy2(src, dst)
-                os.chmod(dst, 0755)
-        except OSError, err:
-            self.log.error("Copying file metis.h to include dir failed: %s" % err)
+            except OSError, err:
+                self.log.error("Copying file libmetis.a to lib dir failed: %s" % err)
+    
+            # copy include files
+            try:
+                for f in ['defs.h', 'macros.h', 'metis.h', 'proto.h', 'rename.h', 'struct.h']:
+                    src = os.path.join(self.getcfg('startfrom'), 'Lib', f)
+                    dst = os.path.join(includedir, f)
+                    shutil.copy2(src, dst)
+                    os.chmod(dst, 0755)
+            except OSError, err:
+                self.log.error("Copying file metis.h to include dir failed: %s" % err)
+    
+            # Other applications depending on ParMETIS (SuiteSparse for one) look for both ParMETIS libraries
+            # and header files in the Lib directory (capital L). The following symlinks are hence created.
+            try:
+                Libdir = os.path.join(self.installdir, 'Lib')
+                os.symlink(libdir, Libdir)
+                for f in ['defs.h', 'macros.h', 'metis.h', 'proto.h', 'rename.h', 'struct.h']:
+                    os.symlink(os.path.join(includedir, f), os.path.join(libdir, f))
+            except OSError, err:
+                self.log.error("Something went wrong during symlink creation: %s" % err)
 
-        # Other applications depending on ParMETIS (SuiteSparse for one) look for both ParMETIS libraries
-        # and header files in the Lib directory (capital L). The following symlinks are hence created.
-        try:
-            Libdir = os.path.join(self.installdir, 'Lib')
-            os.symlink(libdir, Libdir)
-            for f in ['defs.h', 'macros.h', 'metis.h', 'proto.h', 'rename.h', 'struct.h']:
-                os.symlink(os.path.join(includedir, file), os.path.join(libdir, f))
-        except OSError, err:
-            self.log.error("Something went wrong during symlink creation: %s" % err)
+        else:
+            Application.make_install(self)
 
     def sanitycheck(self):
         """Custom sanity check for METIS."""
 
         if not self.getcfg('sanityCheckPaths'):
 
-            self.setcfg('sanityCheckPaths', {'files': ['THIS_WILL_FAIL'],
-                                             'dirs':['THIS_WILL_FAIL_TOO']})
+            binfiles = []
+            if LooseVersion(self.version()) > LooseVersion("5"):
+                binfiles += ["cmpfillin","gpmetis","graphchk","m2gmetis","mpmetis","ndmetis"]
+
+            incfiles = ["metis.h"]
+            if LooseVersion(self.version()) < LooseVersion("5"):
+                incfiles += ["defs.h","macros.h","metis.h","proto.h","rename.h","struct.h"]
+
+            dirs = []
+            if LooseVersion(self.version()) < LooseVersion("5"):
+                dirs += ["Lib"]
+
+            self.setcfg('sanityCheckPaths', {'files': ['bin/%s' % x for x in binfiles] +
+                                                      ['include/%s' % x for x in incfiles] +
+                                                      ['lib/libmetis.a'],
+                                             'dirs' : dirs})
 
             self.log.info("Customized sanity check paths: %s" % self.getcfg('sanityCheckPaths'))
 
