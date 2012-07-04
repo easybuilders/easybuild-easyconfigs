f296b885cd1707ae30be24cd5ef21dccd38b2906
Jens Timmerman <jens.timmerman@ugent.be>
2012-07-04 11:35:14 +0200

    added support for installing binary packages + ecore as an example

---
+++ easybuild/easyblocks/b/binary.py
@@ -0,0 +1,53 @@
+##
+# Copyright 2009-2012 Stijn Deweirdt, Dries Verdegem, Kenneth Hoste, Pieter De Baets, Jens Timmerman
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of the University of Ghent (http://ugent.be/hpc).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild. If not, see <http://www.gnu.org/licenses/>.
+##
+import shutil
+import os
+from easybuild.framework.application import Application
+
+class Binary(Application):
+    """Support for installing a binary package."""
+
+    def configure(self):
+        """No configuration, this is a binary package"""
+        pass
+
+    def make(self):
+        """No compilation, this is a binary package"""
+        pass
+
+    def make_installdir(self):
+        """Not doing anything, copytree in make_install doesn't like it's destination directory to already exist"""
+        pass
+
+    def make_install(self):
+        """Copy the unpacked source to the install directory"""
+        shutil.copytree(os.path.join(self.builddir, '%s-%s' % (self.name().lower(), self.version())), self.installdir, symlinks=True)
+
+    def make_module_extra(self):
+        """
+        Sets optional variables (SOFTROOT, MPI tuning variables).
+        """
+        txt = Application.make_module_extra(self)
+        txt += self.moduleGenerator.prependPaths("PATH", [""])
+
+        self.log.debug("make_module_extra added this: %s" % txt)
+
+        return txt
