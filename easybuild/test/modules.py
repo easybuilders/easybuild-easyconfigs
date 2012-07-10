4ced5c79823b5ea7ba67514d87630b0e3296443d
Toon Willems <willemstoon@gmail.com>
2012-07-10 15:19:50 +0200

    add tests for Modules

---
+++ easybuild/test/modules.py
@@ -0,0 +1,20 @@
+import os
+
+from unittest import TestCase
+from easybuild.tools.build_log import EasyBuildError, initLogger
+import easybuild.tools.modules as modules
+
+class ModulesTest(TestCase):
+
+    def runTest(self):
+        testmods = modules.Modules()
+        ms = testmods.available('', None)
+        if len(ms) != 0:
+            import random
+            m = random.choice(ms)
+            testmods.addModule([m])
+            testmods.load()
+
+            tmp = {"name": m[0], "version": m[1]}
+            assert(tmp in testmods.loaded_modules())
+
