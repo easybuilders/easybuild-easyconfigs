d0ed28faa7edec8fdcbadc5ce235b5725942c402
Toon Willems <willemstoon@gmail.com>
2012-07-25 14:50:50 +0200

    use try finally

--- easybuild/framework/application.py
+++ easybuild/framework/application.py
@@ -873,11 +873,7 @@ class Application:
             ## SANITY CHECK
             try:
                 self.runstep('sanity check', [self.sanitycheck], skippable=False)
-            except:
-                ## CLEANUP
-                self.runstep('cleanup', [self.cleanup])
-                raise
-            else:
+            finally:
                 self.runstep('cleanup', [self.cleanup])
 
 
