97ae2e9d12c4db367632318d11caadc0bcd9d012
Jens Timmerman <jens.timmerman@gmail.com>
2012-07-26 17:23:54 +0300

    Processed remarks for pasha.py

--- easybuild/easyblocks/p/pasha.py
+++ easybuild/easyblocks/p/pasha.py
@@ -31,21 +31,21 @@ class Pasha(Application):
     """
 
     def configure(self):
-        """overwriting configure from Application
-        Set some extra makeopts
-        """
+        """Configure Pasha by setting make options."""
         makeopts = self.getcfg('makeopts')
         makeopts = "%s TBB_DIR=$SOFTROOTTBB/tbb MPI_CXX=$MPICXX OPM_FLAG=%s "\
-                  "MPI_DIR='' MPI_INC='' MPI_LIB='' MY_CXX=$CXX MPICH_IGNORE_CXX_SEEK=1" % (makeopts, self.tk.get_openmp_flag())
+                  "MPI_DIR='' MPI_INC='' MPI_LIB='' MY_CXX=$CXX MPICH_IGNORE_CXX_SEEK=1" % \
+                  (makeopts, self.tk.get_openmp_flag())
 
         self.setcfg('makeopts', makeopts)
 
     def make_install(self):
-        """Overwriting make_install, since there is no install defined in the makefile"""
-        shutil.copytree(os.path.join(self.builddir, "%s-%s" % (self.name(), self.version()), 'bin'), os.path.join(self.installdir, 'bin'))
+        """install by copying everything from 'bin' subdir in build dir to install dir"""
+        srcdir = os.path.join(self.builddir, "%s-%s" % (self.name(), self.version()), 'bin')
+        shutil.copytree(srcdir, os.path.join(self.installdir, 'bin'))
 
     def sanitycheck(self):
-        """Custom sanity check"""
+        """Custom sanity check for Pasha"""
         self.setcfg('sanityCheckPaths', {
                                         'files':["bin/pasha-%s" % x for x in ["kmergen", "pregraph", "graph"]],
                                         'dirs':[""],
