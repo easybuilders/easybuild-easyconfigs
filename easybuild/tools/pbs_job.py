37161cc1a1a06cbcd0d650b8cad8e404e5b92783
Toon Willems <willemstoon@gmail.com>
2012-08-01 16:43:24 +0200

    update gpl header

--- easybuild/tools/pbs_job.py
+++ easybuild/tools/pbs_job.py
@@ -1,11 +1,22 @@
-
 # Copyright 2012 Stijn De Weirdt, Toon Willems
 #
-# This file is part of HanythingOnDemand,
+# This file is part of EasyBuild,
 # originally created by the HPC team of the University of Ghent (http://ugent.be/hpc).
 #
-
-
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
 import os, sys, re
 
 from easybuild.tools.build_log import getLog
@@ -129,7 +140,6 @@ class PbsJob:
 
     def info(self, types=None):
         """Return jobinfo"""
-        ## TODO restrict to current user jobs
         if type(types) is str:
             types = [types]
         self.log.debug("Return info types %s" % types)
