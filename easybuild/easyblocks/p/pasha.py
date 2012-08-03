f3379a4290d99f98d5251b15a20f6047527042bb
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-08-03 07:09:55 +0200

    process remarks made on pull request

--- easybuild/easyblocks/p/pasha.py
+++ easybuild/easyblocks/p/pasha.py
@@ -27,9 +27,7 @@ import os
 from easybuild.framework.application import Application
 
 class Pasha(Application):
-    """
-    Support for building and installing Pasha"
-    """
+    """Support for building and installing Pasha"""
 
     def configure(self):
         """Configure Pasha by setting make options."""
@@ -38,8 +36,9 @@ class Pasha(Application):
         if not tbb:
             self.log.error("TBB module not loaded.")
 
-        self.updatecfg('makeopts', "TBB_DIR=%s/tbb MPI_CXX=$MPICXX OPM_FLAG=%s" % (tbb, self.tk.get_openmp_flag()))
-        self.updatecfg('makeopts', "MPI_DIR='' MPI_INC='' MPI_LIB='' MY_CXX=$CXX MPICH_IGNORE_CXX_SEEK=1")
+        self.updatecfg('makeopts', "TBB_DIR=%s/tbb MPI_DIR='' MPI_INC=''")
+        self.updatecfg('makeopts', "MPI_CXX=$MPICXX OPM_FLAG=%s" % (tbb, self.tk.get_openmp_flag()))
+        self.updatecfg('makeopts', "MPI_LIB='' MY_CXX=$CXX MPICH_IGNORE_CXX_SEEK=1")
 
     def make_install(self):
         """Install by copying everything from 'bin' subdir in build dir to install dir"""
@@ -50,7 +49,9 @@ class Pasha(Application):
     def sanitycheck(self):
         """Custom sanity check for Pasha"""
         self.setcfg('sanityCheckPaths', {
-                                        'files':["bin/pasha-%s" % x for x in ["kmergen", "pregraph", "graph"]],
+                                         'files':["bin/pasha-%s" % x for x in ["kmergen",
+                                                                               "pregraph",
+                                                                               "graph"]],
                                         'dirs':[""],
                                         })
 
