4a8974cad633fd20e0c2c6598a0014777fc0487d
Toon Willems <willemstoon@gmail.com>
2012-08-03 12:29:24 +0200

    typo and docstring
    Conflicts:
    
    	easybuild/scripts/prep_for_release.py

--- easybuild/scripts/prep_for_release.py
+++ easybuild/scripts/prep_for_release.py
@@ -196,8 +196,11 @@ def check_clean_master_branch(home):
         print "Current branch is clean, great work!"
 
     return ok
-# check wheter os.putenv or os.environ[]= is used inside easyblocks
+
+# check whether os.putenv or os.environ[]= is used inside easyblocks
 def check_easyblocks_for_environment(home):
+    """ check whether os.putenv or os.environ[]= is used inside easyblocks """
+
     files = glob.glob(os.path.join(home, 'easybuild/easyblocks/[a-z]/*.py'))
     eb_files = filter(lambda x: os.path.basename(x) != '__init__.py', files)
 
