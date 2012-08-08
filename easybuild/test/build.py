22342efd4ad84a9f7083dc93e679bdac31e1a595
Toon Willems <willemstoon@gmail.com>
2012-08-08 08:45:04 +0200

    add some more detailed logging

--- easybuild/test/build.py
+++ easybuild/test/build.py
@@ -267,11 +267,13 @@ def build_packages_in_parallel(packages, output_dir, script_dir):
         no_dependencies = [pkg for pkg in with_dependencies if len(pkg['unresolvedDependencies']) == 0]
         with_dependencies = [pkg for pkg in with_dependencies if pkg not in no_dependencies]
 
-        jobs.extend([submit_job(pkg, output_dir, script_dir) for pkg in no_dependencies])
+        new_jobs = [submit_job(pkg, output_dir, script_dir) for pkg in no_dependencies]
+        jobs.extend(new_jobs)
 
+        # Add some logging
         log.info("%s jobs finished completion: %s" % (len(done_jobs), [job.name for job in done_jobs]))
-        log.info("%s jobs have been submitted" % len(no_dependencies))
-        log.info("%s jobs are being run" % len(jobs))
+        log.info("%s jobs have been submitted: %s" % (len(new_jobs), [job.name for job in new_jobs]))
+        log.info("%s jobs are being run: %s" % (len(jobs), [job.name for job in jobs]))
         log.info("%s packages still need to be build" % len(with_dependencies))
 
 def submit_job(package, output_dir, script_dir):
