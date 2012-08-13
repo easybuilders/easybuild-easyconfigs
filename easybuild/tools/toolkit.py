819a59a3d6b81b1609d57114565b3e13a3d31538
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-08-13 22:19:49 +0200

    fix typo

--- easybuild/tools/toolkit.py
+++ easybuild/tools/toolkit.py
@@ -652,7 +652,7 @@ class Toolkit:
         if self.opts['pic']:
             fftwsuff = "_pic"
         fftw_libs = ["fftw3xc_intel%s" % fftwsuff,
-                     "fftw3x_cdft%" % fftwsuff]
+                     "fftw3x_cdft%s" % fftwsuff]
         self.vars['LIBFFT'] = ' '.join(["-Wl:-Bstatic",
                                         ' '.join(["-%s" % x for x in fftw_libs]),
                                         "-Wl:-Bdynamic"])
