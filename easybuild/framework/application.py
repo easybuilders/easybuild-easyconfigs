64e5c0cbcb0695a20579b9a1b500b58811c3cfe1
Fotis Georgatos <fotis.georgatos@uni.lu>
2012-08-18 14:41:17 +0200

    enabled new class name convention as per: EBCLASSPREFIX + encode_string(name)
    Signed-off-by: Fotis Georgatos <fotis.georgatos@uni.lu>

--- easybuild/framework/application.py
+++ easybuild/framework/application.py
@@ -45,11 +45,14 @@ import easybuild.tools.environment as env
 from easybuild.framework.easyconfig import EasyConfig
 from easybuild.tools.build_log import EasyBuildError, initLogger, removeLogHandler,print_msg
 from easybuild.tools.config import source_path, buildPath, installPath
-from easybuild.tools.filetools import unpack, patch, run_cmd, convertName
+from easybuild.tools.filetools import unpack, patch, run_cmd, convertName, encode_string
 from easybuild.tools.module_generator import ModuleGenerator
 from easybuild.tools.modules import Modules, get_software_root
 from easybuild.tools.toolkit import Toolkit
 from easybuild.tools.systemtools import get_core_count
+
+
+EBCLASSPREFIX = 'eb_'
 
 
 class Application:
@@ -1401,7 +1404,7 @@ class Application:
         allclassmodule = pkgdefaultclass[0]
         defaultClass = pkgdefaultclass[1]
         for pkg in self.pkgs:
-            name = pkg['name'][0].upper() + pkg['name'][1:] # classnames start with a capital
+            name = EBCLASSPREFIX + encode_string(pkg['name']) # Use the same encoding as get_class
             self.log.debug("Starting package %s" % name)
 
             try:
@@ -1566,8 +1569,8 @@ def get_class(easyblock, log, name=None):
                 name = "UNKNOWN"
 
             modulepath = module_path_for_easyblock(name)
-            # don't use capitalize, as it changes 'GCC' into 'Gcc', we want to keep the capitals that are there already
-            class_name = name[0].upper() + name[1:].replace('-','_')
+            # The following is a generic way to calculate unique class names for any funny package title
+            class_name = EBCLASSPREFIX + encode_string(name)
 
             # try and find easyblock
             easyblock_found = False
