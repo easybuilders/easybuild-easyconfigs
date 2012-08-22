0bcae45ee5436aec30704fa953622a40dbed3c5d
Jens Timmerman <jens.timmerman@gmail.com>
2012-08-22 12:14:04 +0300

    fix accidental change in last commit.

--- easybuild/framework/application.py
+++ easybuild/framework/application.py
@@ -1116,7 +1116,7 @@ class Application:
 
         return modpath
 
-    def vim _module(self, create_in_builddir=False):
+    def make_devel_module(self, create_in_builddir=False):
         """
         Create a develop module file which sets environment based on the build
         Usage: module load name, which loads the module you want to use. $EBDEVELNAME should then be the full path
