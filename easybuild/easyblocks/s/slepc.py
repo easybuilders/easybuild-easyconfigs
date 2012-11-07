8814be146deebdb1e22e8a5654a947118c2ad904
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-11-07 06:40:58 +0100

    fix typo

--- easybuild/easyblocks/s/slepc.py
+++ easybuild/easyblocks/s/slepc.py
@@ -94,7 +94,7 @@ class EB_SLEPc(ConfigureMake):
             # regular './configure --prefix=X' for non-source install
             
             # make sure old install dir is removed first
-            self.make_installdir(dontcreate=True)
+            self.make_installdir(dontcreate_=True)
 
             out = super(EB_SLEPc, self).configure_step()
 
