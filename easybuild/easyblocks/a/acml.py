47d817f60e5258ced8222fa4db664d9786057aed
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-11-23 15:15:41 +0100

    add corrected support for handling fma4 case for new ACML versions, add use_fma4 easyconfig parameter, set ACML_BASEDIR* env vars in ACML module file

--- easybuild/easyblocks/a/acml.py
+++ easybuild/easyblocks/a/acml.py
@@ -36,10 +36,18 @@ import os
 from distutils.version import LooseVersion
 
 from easybuild.framework.easyblock import EasyBlock
+from easybuild.framework.easyconfig import CUSTOM
 from easybuild.tools.filetools import run_cmd_qa
 
 
 class EB_ACML(EasyBlock):
+
+    @staticmethod
+    def extra_options():
+        extra_vars = [
+                      ('use_fma4', [False, "Use library with FMA support.", CUSTOM]),
+                     ]
+        return EasyBlock.extra_options(extra_vars)
 
     def __init__(self, *args, **kwargs):
         """Constructor, adds extra class variables."""
@@ -48,23 +56,17 @@ class EB_ACML(EasyBlock):
 
         vsuff_list = self.cfg['versionsuffix'].split('-')
 
-        comp = vsuff_list[1]
-        bits = vsuff_list[2]
-        self.basedir = None
-        if bits == "32bit":
-            self.basedir = "%s32" % comp
-        elif bits == "64bit":
-            self.basedir = "%s64" % comp
-        else:
-            self.log.error("Unexpected 'bits' entry in versionsuffix[2]: %s" % bits)
+        comp = vsuff_list[1]  # gfortran, ifort, ...
+        bits = vsuff_list[2]  # 32bit or 64bit
+        self.basedir = comp + bits[0:2]
+
+        # specialized suffix, e.g. _fma4 for fused multiply-add
+        if LooseVersion(self.version) >= LooseVersion("5") and self.cfg['use_fma4']:
+            self.basedir += '_fma4'
 
         self.suffix = ''
         if self.cfg['versionsuffix'].split('-')[-1] == "int64":
             self.suffix = '_int64'
-
-        self.altlib = '_mp'
-        if LooseVersion(self.version) >= LooseVersion("5"):
-            self.altlib = '_fma4'
 
     def configure_step(self):
         """No configure."""
@@ -89,7 +91,11 @@ class EB_ACML(EasyBlock):
 
         txt = super(EB_ACML, self).make_module_extra()
 
-        basepaths = ["%s%s" % (self.basedir, self.suffix), "%s%s%s" % (self.basedir, self.altlib, self.suffix)]
+        basepaths = ["%s%s" % (self.basedir, self.suffix),
+                     "%s_mp%s" % (self.basedir, self.suffix)]
+
+        self.moduleGenerator.set_environment('ACML_BASEDIR', basepaths[0])
+        self.moduleGenerator.set_environment('ACML_BASEDIR_MT', basepaths[1])
 
         for key in ['CPATH', 'FPATH']:
             for path in basepaths:
@@ -112,15 +118,12 @@ class EB_ACML(EasyBlock):
 
         inc_files = []
         lib_files = []
-        for suff in ['', self.altlib]:
+        for suff in ['', '_mp']:
 
-            fp = "%s%s" % (self.basedir, suff)
+            fp = "%s%s%s" % (self.basedir, suff, self.suffix)
             for inc in [''] + inc_extra:
                 inc_files.append(os.path.join(fp, 'include', 'acml%s.h' % inc))
 
-            # _mp is appended to libs, _fma4 is not
-            if suff in "_fma4":
-                suff = ''
             for lib in [suff] + lib_extra:
                 for ext in ['.a', '.so']:
                     lib_files.append(os.path.join(fp, 'lib', 'libacml%s%s' % (lib, ext)))
