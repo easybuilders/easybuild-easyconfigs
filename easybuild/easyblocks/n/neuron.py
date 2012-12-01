2862b9cf9b20d608bd5d8757f5909132b92f35e2
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-12-01 15:28:50 +0100

    implemented easyblock for NEURON, still WIP because run_test needs to be fixed

---
+++ easybuild/easyblocks/n/neuron.py
@@ -0,0 +1,159 @@
+##
+# Copyright 2009-2012 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+EasyBuild support for building and installing NEURON, implemented as an easyblock
+"""
+import os
+
+from easybuild.easyblocks.generic.configuremake import ConfigureMake
+from easybuild.framework.easyconfig import CUSTOM
+from easybuild.tools.filetools import run_cmd
+from easybuild.tools.modules import get_software_root, get_software_version
+
+
+class EB_NEURON(ConfigureMake):
+    """Support for building/installing NEURON."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialisation of custom class variables for NEURON."""
+        super(EB_NEURON, self).__init__(*args, **kwargs)
+
+        self.hostcpu = None
+        self.with_python = False
+
+    @staticmethod
+    def extra_options():
+        """Custom easyconfig parameters for NEURON."""
+
+        extra_vars = [
+                      ('runtest', [True, "Run 'neurondemo' test case after building.", CUSTOM]),
+                      ('paranrn', [True, "Enable support for distributed simulations.", CUSTOM]),
+                     ]
+        return ConfigureMake.extra_options(extra_vars)
+
+    def configure_step(self):
+        """Custom configuration procedure for NEURON."""
+
+        # enable support for distributed simulations if desired
+        if self.cfg['paranrn']:
+            self.cfg.update('configopts', '--with-paranrn')
+
+        # specify path to InterViews if it is available as a dependency
+        interviews_root = get_software_root('InterViews')
+        if interviews_root:
+            self.cfg.update('configopts', "--with-iv=%s" % interviews_root)
+        else:
+            self.cfg.update('configopts', "--without-iv")
+
+        # optionally enable support for Python as alternative interpreter
+        python_root = get_software_root('Python')
+        if python_root:
+            self.with_python = True
+            self.cfg.update('configopts', "--with-nrnpython=%s/bin/python" % python_root)
+
+        # determine host CPU type
+        cmd = "./config.guess"
+        (out, ec) = run_cmd(cmd, simple=False)
+
+        self.hostcpu = out.split('\n')[0].split('-')[0]
+        self.log.debug("Determined host CPU type as %s" % self.hostcpu)
+
+        # complete configuration with configure_method of parent
+        super(EB_NEURON, self).configure_step()
+
+    def test_step(self):
+        """Custom built-in test procedure for NEURON."""
+
+        if self.cfg['runtest']:
+            cmd = "test-command"
+            run_cmd(cmd, log_all=True, simple=True, log_output=True)
+
+    def install_step(self):
+        """Custom install procedure for NEURON."""
+
+        super(EB_NEURON, self).install_step()
+
+        if self.with_python:
+            pypath = os.path.join('src', 'nrnpython')
+            try:
+                pwd = os.getcwd()
+                os.chdir(pypath)
+            except OSError, err:
+                self.log.error("Failed to change to %s: %s" % (pypath, err))
+
+            cmd = "python setup.py install --prefix=%s" % self.installdir
+            run_cmd(cmd, simple=True, log_all=True, log_ok=True)
+
+            try:
+                os.chdir(pwd)
+            except OSError, err:
+                self.log.error("Failed to change back to %s: %s" % (pwd, err))
+
+
+    def sanity_check_step(self):
+        """Custom sanity check for NEURON."""
+
+        binpath = os.path.join(self.hostcpu, 'bin')
+        libpath = os.path.join(self.hostcpu, 'lib', 'lib%s.so')
+        custom_paths = {
+                        'files': [os.path.join(binpath, x) for x in ["bbswork.sh", "hel2mos1.sh",
+                                                                     "hoc_ed", "ivoc", "memacs",
+                                                                     "mkthreadsafe", "modlunit",
+                                                                     "mos2nrn", "mos2nrn2.sh",
+                                                                     "neurondemo", "nocmodl",
+                                                                     "oc"]] +
+                                 [os.path.join(binpath, "nrn%s" % x) for x in ["gui", "iv",
+                                                                               "iv_makefile",
+                                                                               "ivmodl",
+                                                                               "mech_makefile",
+                                                                               "oc",
+                                                                               "oc_makefile",
+                                                                               "ocmodl"]] +
+                                 [libpath % x for x in ["ivoc", "ivos", "memacs", "meschach",
+                                                        "neuron_gnu", "nrniv", "nrnmpi", "nrnoc",
+                                                        "nrnpython", "oc", "ocxt", "scopmath",
+                                                        "sparse13", "sundials"]],
+                        'dirs': ['include/nrn', 'share/nrn']
+                       }
+
+        super(EB_NEURON, self).sanity_check_step(custom_paths=custom_paths)
+
+    def make_module_req_guess(self):
+        """Custom guesses for environment variables (PATH, ...) for NEURON."""
+
+        guesses = super(EB_NEURON, self).make_module_req_guess()
+
+        guesses.update({
+                        'PATH': [os.path.join(self.hostcpu, 'bin')],
+                       })
+
+        if self.with_python:
+            pyver = '.'.join(get_software_version('Python').split('.')[0:2])
+            pylibdir = os.path.join('lib', 'python%s' % pyver, 'site-packages')
+            guesses.update({
+                            'PYTHONPATH': [pylibdir],
+                           })
+
+        return guesses
