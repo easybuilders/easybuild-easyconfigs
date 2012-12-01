079ea4e4db3fdda42fd9dfefe8b22573bd2beb7e
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-12-01 15:28:18 +0100

    ported support for QuantumESPRESSO, made it more generic, now also works with goalf compiler toolchain (refs #49)

--- easybuild/easyblocks/q/quantumespresso.py
+++ easybuild/easyblocks/q/quantumespresso.py
@@ -23,94 +23,168 @@
 # along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
 ##
 """
-EasyBuild support for QuantumESPRESSO, implemented as an easyblock
+EasyBuild support for Quantum ESPRESSO, implemented as an easyblock
 """
+import fileinput
 import os
 import re
 import shutil
-import tempfile
+import sys
 
+import easybuild.tools.toolchain as toolchain
 from easybuild.easyblocks.generic.configuremake import ConfigureMake
+from easybuild.framework.easyconfig import CUSTOM
+from easybuild.tools.modules import get_software_root
 
-class QuantumESPRESSO(ConfigureMake):
+
+class EB_QuantumESPRESSO(ConfigureMake):
+    """Support for building and installing Quantum ESPRESSO."""
+
+    @staticmethod
+    def extra_options():
+        """Custom easyconfig parameters for Quantum ESPRESSO."""
+        extra_vars = [
+                      ('hybrid', [False, "Enable hybrid build (with OpenMP)", CUSTOM]),
+                      ('with_scalapack', [True, "Enable ScaLAPACK support", CUSTOM]),
+                     ]
+        return ConfigureMake.extra_options(extra_vars)
 
     def configure_step(self):
-        oldextralibpath = os.path.join(self.builddir, 'install')
-        newextralibpath = os.path.join(self.cfg[start_dir], 'install')
-        extralibs = os.listdir(oldextralibpath)
+        """Custom configuration procedure for Quantum ESPRESSO."""
+
+        # copy stuff in 'install' directory to 'install' directory for Quantum ESPRESSO
+        # this comes from the IntelMKL*tar.gz tarball, that is custom for HPC-UGent
+        # TODO: create patch files instead, and get rid of this crap below?
+        #if self.toolchain().name == 'ictce':
+        #oldextralibpath = os.path.join(self.builddir, 'install')
+        #newextralibpath = os.path.join(self.cfg['start_dir'], 'install')
+        #extralibs = os.listdir(oldextralibpath)
+        #try:
+            #for lib in extralibs:
+                #shutil.copy2(os.path.join(oldextralibpath, lib), newextralibpath)
+        #except Exception, err:
+            #self.log.error("Failed to copy %s to %s, %s" % (lib, newextralibpath, err))
+
+        if self.cfg['hybrid']:
+            self.cfg.update('configopts', '--enable-openmp')
+
+        if not self.toolchain.options['usempi']:
+            self.cfg.update('configopts', '--disable-parallel')
+
+        if not self.cfg['with_scalapack']:
+            self.cfg.update('configopts', '--without-scalapack')
+
+        super(EB_QuantumESPRESSO, self).configure_step()
+
+        repls = []
+
+        # compose list of DFLAGS (flag, value, keep_stuff)
+        # for guidelines, see include/defs.h.README in sources
+        dflags = []
+
+        comp_fam_dflags = {
+                           toolchain.INTELCOMP: '-D__INTEL',
+                           toolchain.GCC: '-D__GFORTRAN -D__STD_F95',
+                          }
+        dflags.append(comp_fam_dflags[self.toolchain.comp_family()])
+
+        if "fftw3" in os.getenv('LIBFFT'):
+            dflags.append('-D__FFTW3')
+        elif os.getenv('LIBFFT'):
+            dflags.append('-D__FFTW')
+
+        if get_software_root('ACML'):
+            dflags.append('-D__ACML')
+
+        if self.toolchain.options['usempi']:
+            dflags.append('-D__MPI -D__PARA')
+
+        if self.cfg['hybrid']:
+            dflags.append(" -D__OPENMP")
+
+        if self.cfg['with_scalapack']:
+            dflags.append(" -D__SCALAPACK")
+
+        repls.append(('DFLAGS', ' '.join(dflags), False))
+
+        # complete C/Fortran compiler and LD flags
+        if self.cfg['hybrid']:
+            repls.append(('LDFLAGS', self.toolchain.get_flag('openmp'), True))
+            repls.append(('(?:C|F90|F)FLAGS', self.toolchain.get_flag('openmp'), True))
+
+        # obtain library settings
+        repls.append(('BLAS_LIBS', os.getenv('LIBBLAS'), False))
+        repls.append(('FFT_LIBS', os.getenv('LIBFFT'), False))
+        repls.append(('LAPACK_LIBS', os.getenv('LIBLAPACK'), False))
+        repls.append(('SCALAPACK_LIBS', os.getenv('LIBSCALAPACK'), False))
+        repls.append(('LD_LIBS', os.getenv('LIBS'), False))
+
+        # patch make.sys file
+        fn = os.path.join(self.cfg['start_dir'], 'make.sys')
         try:
-            for lib in extralibs:
-                shutil.copy2(os.path.join(oldextralibpath, lib), newextralibpath)
-        except Exception, err:
-            self.log.error("Failed to copy %s to %s, %s" % (lib, newextralibpath, err))
+            for line in fileinput.input(fn, inplace=1, backup='.orig.eb'):
+                for (k, v, keep) in repls:
+                    if keep:
+                        line = re.sub(r"^(%s\s*=[ \t]*)(.*)$" % k, r"\1\2 %s" % v, line)
+                    else:
+                        line = re.sub(r"^(%s\s*=[ \t]*).*$" % k, r"\1%s" % v, line)
 
-        ConfigureMake.configure(self)
+                    # fix preprocessing directives for .f90 files in make.sys if required
+                if self.toolchain.comp_family() in [toolchain.GCC]:
+                    line = re.sub("\$\(MPIF90\) \$\(F90FLAGS\) -c \$<",
+                                  "$(CPP) -C $(CPPFLAGS) $< -o $*.F90\n" +
+                                  "\t$(MPIF90) $(F90FLAGS) -c $*.F90 -o $*.o",
+                                  line)
 
-        if self.toolchain().name == 'ictce':
-            makesysfile = os.path.join(self.cfg[start_dir], 'make.sys')
+                sys.stdout.write(line)
+        except OSError, err:
+            self.log.error("Failed to patch %s: %s" % (fn, err))
 
-            mkllib = "%s/lib/em64t" % os.environ['SOFTROOTIMKL']
+        # move non-espresso directories to where they're expected
+        try:
+            dirnames = [d for d in os.listdir(self.builddir) if not d.startswith('espresso')]
+            targetdir = os.path.join(self.builddir, "espresso-%s" % self.version)
+            for dirname in dirnames:
+                shutil.move(os.path.join(self.builddir, dirname), targetdir)
+                self.log.info("Moved %s into %s" % (dirname, targetdir))
 
-            regopenmp = re.compile(r"openmp")
-            regscal = re.compile(r"scalapack")
+        except OSError, err:
+            self.log.error("Failed to move non-espresso directories: %s" % err)
 
-            makesysregdflags = re.compile(r"^DFLAGS\s*=.*$", re.M)
-            makesysrepldflags = "DFLAGS = -D__INTEL -D__FFTW3 -D__MPI -D__PARA"
-            if regopenmp.search(self.cfg[configopts]):
-                makesysrepldflags += " -D__OPENMP"
-            if regscal.search(self.cfg[configopts]):
-                makesysrepldflags += " -D__SCALAPACK"
-
-            makesysregflags = re.compile(r"^(?:C|F90|F)FLAGS.*$", re.M)
-            makesysreplflags = "\g<0>"
-            if regopenmp.search(self.cfg[configopts]):
-                makesysreplflags += " -openmp"
-
-            makesysregblas = re.compile(r"^BLAS_LIBS\s*=.*$", re.M)
-            makesysreplblas = "BLAS_LIBS = %s/libmkl_em64t.a\n" % (mkllib)
-
-            makesysregscal = re.compile(r"^SCALAPACK_LIBS\s*=.*$", re.M)
-            makesysreplscal = "SCALAPACK_LIBS = %s/libmkl_scalapack.a %s/libmkl_blacs_lp64.a\n" % (mkllib, mkllib)
-
-            makesysreglibs = re.compile(r"^LD_LIBS\s*=.*$", re.M)
-            makesysrepllibs = "LD_LIBS = -liomp5 -lpthread"
-
-            try:
-                f = open(makesysfile, 'r')
-                origcontent = f.read()
-                f.close()
-            except Exception, err:
-                self.log.error("Can't read from file %s: %s" % (makesysfile, err))
-
-            try:
-                os.remove(makesysfile)
-            except Exception, err:
-                self.log.error("It is not possible to remove the old version of %s" % makesysfile)
-
-            try:
-                f = open(makesysfile, 'w')
-                origcontent = makesysregscal.sub(makesysreplscal, origcontent)
-                origcontent = makesysreglibs.sub(makesysrepllibs, origcontent)
-                origcontent = makesysregblas.sub(makesysreplblas, origcontent)
-                origcontent = makesysregdflags.sub(makesysrepldflags, origcontent)
-                origcontent = makesysregflags.sub(makesysreplflags, origcontent)
-                f.write(origcontent)
-                f.close()
-                self.log.info("New version of file %s successfully written" % makesysfile)
-            except Exception, err:
-                self.log.error("Can't write to file %s: %s" % (makesysfile, err))
+        # make sure we build everything
+        self.cfg.update('makeopts', 'all gipaw vdw w90 want gww xspectra yambo')
 
     def install_step(self):
+        """Custom install procedure for Quantum ESPRESSO: just copy the binaries."""
+        # TODO: apperently the binaries are symlinked in the 'bin' directory to the build dir
+        # do we need to make extra measures to make sure the actual binaries are copied, not the symlinks?
         try:
-            shutil.copytree(os.path.join(self.cfg[start_dir], 'bin'), os.path.join(self.installdir, 'bin'))
-        except Exception, err:
-            self.log.error("Something went wrong during bin dir copying to installdir: %s" % err)
+            shutil.copytree(os.path.join(self.cfg['start_dir'], 'bin'),
+                            os.path.join(self.installdir, 'bin'))
+
+        except OSError, err:
+            self.log.error("Failed to copy binaries to install dir: %s" % err)
 
     def sanity_check_step(self):
+        """Custom sanity check for Quantum ESPRESSO."""
 
         custom_paths = {
-                        'files': [],
-                        'dirs': ["bin"]
+                        'files': ["bin/%s.x" % x for x in ["average", "band_plot", "bands_FS",
+                                                           "bands", "cppp", "cp", "d3", "dist",
+                                                           "dos", "dynmat", "epsilon", "ev",
+                                                           "gipaw", "gww_fit", "gww", "head",
+                                                           "initial_state", "iotk_print_kinds",
+                                                           "iotk", "kpoints", "kvecs_FS", "lambda",
+                                                           "ld1", "matdyn", "path_int", "phcg",
+                                                           "ph", "plan_avg", "plotband", "plotproj",
+                                                           "plotrho", "pmw", "pp", "projwfc",
+                                                           "pw2casino", "pw2gw", "pw2wannier90",
+                                                           "pw4gww", "pwcond", "pw_export",
+                                                           "pwi2xsf", "pw", "q2r", "sumpdos", "vdw",
+                                                           "wannier90", "wannier_ham",
+                                                           "wannier_plot", "wfdd", "xspectra"]] +
+                                 ["bin/iotk"],
+                        'dirs': []
                        }
 
         ConfigureMake.sanity_check_step(self, custom_paths=custom_paths)
