03d29f99ea8374f90ba0df55890b58e6da4fcf09
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-12-12 11:51:52 +0100

    NWChem easyblock, ready to start testing (but incomplete w.r.t. module file generated and testing)

---
+++ easybuild/easyblocks/n/nwchem.py
@@ -0,0 +1,200 @@
+##
+# Copyright 2009-2012 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+EasyBuild support for building and installing NWChem, implemented as an easyblock
+"""
+import os
+import shutil
+
+import easybuild.tools.environment as env
+from easybuild.easyblocks.generic.configuremake import ConfigureMake
+from easybuild.framework.easyconfig import CUSTOM
+from easybuild.tools.filetools import mkdir, run_cmd
+from easybuild.tools.modules import get_software_root, get_software_version
+
+
+class EB_NWChem(ConfigureMake):
+    """Support for building/installing NWChem."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialisation of custom class variables for NWChem."""
+        super(EB_NWChem, self).__init__(*args, **kwargs)
+
+        self.example = None
+
+    @staticmethod
+    def extra_options():
+        """Custom easyconfig parameters for NWChem."""
+
+        extra_vars = [
+                      ('target', ['LINUX64', "Target platform", CUSTOM]),
+                      # possible options for ARMCI_NETWORK on LINUX64 with Infiniband:
+                      # OPENIB, MPI-MT, MPI-SPAWN, MELLANOX
+                      ('armci_network', ['OPENIB', "Network protocol to use", CUSTOM]),
+                      ('modules', ["all", "NWChem modules to build", CUSTOM]),
+                      ('lib_defines', ['', "Additional defines for C preprocessor", CUSTOM]),
+                      ('with_nbo_support', [False, "Enable NBO support", CUSTOM]),
+                     ]
+        return ConfigureMake.extra_options(extra_vars)
+
+    def configure_step(self):
+        """Custom configuration procedure for NWChem."""
+
+        # change to actual build dir
+        try:
+            os.chdir('src')
+        except OSError, err:
+            self.log.error("Failed to change to build dir: %s" % err)
+
+        # set required NWChem environment variables
+        env.setvar('NWCHEM_TOP', self.cfg['start_dir'])
+        env.setvar('NWCHEM_TARGET', self.cfg['target'])
+        env.setvar('ARMCI_NETWORK', self.cfg['armci_network'])
+        nwchem_modules = self.cfg['modules']
+
+        if 'python' in self.cfg['modules']:
+            python_root = get_software_root('Python')
+            if not python_root:
+                self.log.error("Python module not loaded, you should add Python as a dependency.")
+            env.setvar('PYTHONHOME', python_root)
+            env.setvar('PYTHONVERSION', '.'.join(get_software_version('Python').split('.')[0:2]))
+
+        env.setvar('LARGE_FILES', 'TRUE')
+        env.setvar('USE_NOFSCHECK', 'TRUE')
+        env.setvar('LIB_DEFINES', self.cfg['lib_defines'])
+
+        if self.toolchain.options['usempi']:
+            for var in ['USE_MPI', 'USE_MPIF', 'USE_MPIF4']:
+                env.setvar(var, 'y')
+            env.setvar('LIBMPI', '')  # not needed, using MPI wrapper commands (mpicc, ...)
+            env.setvar('MPI_LIB', os.getenv('MPI_LIB_DIR'))
+            env.setvar('MPI_INCLUDE', os.getenv('MPI_INC_DIR'))
+
+        # BLAS and ScaLAPACK
+        env.setvar('HAS_BLAS', 'yes')
+        env.setvar('BLASOPT', '-L%s %s' % (os.getenv('BLAS_LIB_DIR'), os.getenv('LIBBLAS')))
+
+        env.setvar('USE_SCALAPACK', 'y')
+        env.setvar('SCALAPACK', '%s %s' % (os.getenv('LDFLAGS'), os.getenv('LIBSCALAPACK')))
+
+        # enable NBO support if desired
+        if self.cfg['with_nbo_support']:
+            nwnbo_file = 'nwnbo.f'  # where should this come from?
+            target = os.path.join(self.cfg['start_dir'], 'src', 'nbo')
+            shutil.copyfile(nwnbo_file, target)
+            nwchem_modules += ' nbo'
+
+        env.setvar('NWCHEM_MODULES', nwchem_modules)
+        self.log.info('NWCHEM_MODULES set to %s' % os.getenv('NWCHEM_MODULES'))
+
+        # configure build
+        run_cmd("make nwchem_config", simple=True, log_all=True, log_ok=True)
+
+    def build_step(self):
+        """Custom built-in test procedure for NWChem."""
+
+        # clean first (why not)
+        run_cmd("make clean", simple=True, log_all=True, log_ok=True)
+
+        # set FC
+        env.setvar('FC', os.getenv('F77'))
+        self.log.info('FC set to %s' % os.getenv('FC'))
+
+        # check whether 64-bit integers should be used, and act on it
+        if not self.toolchain.options['i8']:
+            par = ''
+            if self.cfg['parallel']:
+                par = '-j %s' % self.cfg['parallel']
+            run_cmd("make %s 64_to_32" % par, simple=True, log_all=True, log_ok=True)
+
+            # clean again to prepare for build
+            run_cmd("make realclean", simple=True, log_all=True, log_ok=True)
+
+            env.setvar('makeopts', "USE_64TO32=y")
+
+        super(EB_NWChem, self).build_step()
+
+        # TODO: run getmem.nwchem script to assess memory availability and make an educated guess,
+        # recompile the appropriate files and relink
+
+    def test_step(self):
+        """Custom built-in test procedure for NWChem."""
+
+        if self.cfg['runtest']:
+            cmd = "test command"
+            run_cmd(cmd,  simple=True, log_all=True, log_ok=True)
+
+    def install_step(self):
+        """Custom install procedure for NWChem."""
+
+        try:
+            # binary
+            bindir = os.path.join(self.installdir, 'bin')
+            mkdir(bindir)
+            shutil.copy(os.path.join(self.cfg['start_dir'], 'bin', self.cfg['target'], 'nwchem'),
+                        bindir)
+
+            # data
+            datadir = os.path.join(self.installdir, 'data')
+            mkdir(datadir)
+
+            src_datadir = os.path.join(self.cfg['start_dir'], 'data')
+            for path in os.listdir(src_datadir):
+                fullpath = os.path.join(src_datadir, path)
+                if os.path.isfile(fullpath):
+                    shutil.copy2(fullpath, datadir)
+                else:
+                    shutil.copytree(fullpath, datadir)
+
+            shutil.copytree(os.path.join(self.cfg['start_dir'], 'src', 'basis', 'libraries'), datadir)
+            shutil.copytree(os.path.join(self.cfg['start_dir'], 'src', 'nwpw', 'libraryps'), datadir)
+
+        except OSError, err:
+            self.log.error("Failed to install NWChem: %s" % err)
+
+    def sanity_check_step(self):
+        """Custom sanity check for NWChem."""
+
+        custom_paths = {
+                        'files': ['bin/nwchem'],
+                        'dirs': [os.path.join('data', x) for x in ['amber_q', 'amber_s', 'amber_t',
+                                                                   'amber_u', 'amber_x', 'charmm_s',
+                                                                   'charmm_x', 'solvents',
+                                                                   'libraries', 'libraryps']],
+                       }
+
+        super(EB_NWChem, self).sanity_check_step(custom_paths=custom_paths)
+
+    def make_module_extra(self):
+        """Custom extra module file entries for NWChem."""
+
+        txt = super(EB_NWChem, self).make_module_extra()
+
+        txt += self.moduleGenerator().set_environment('PYTHONHOME', get_software_root('Python'))
+        pyver = '.'.join(get_software_version('Python').split('.')[0:2])
+        txt += self.moduleGenerator().set_environment('PYTHONVERSION', pyver)
+        txt += self.moduleGenerator().prepend_paths('PYTHONPATH', ['path1', 'path2'])
+
+        return txt
