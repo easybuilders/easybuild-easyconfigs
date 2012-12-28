c24ecb8219257e23ffbfee71f48228384f5d43cc
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-12-28 10:37:30 +0100

    clean up contributed Chapel easyblock: derive from ConfigureMake, style cleanup, add sanity check, ...

--- easybuild/easyblocks/c/chapel.py
+++ easybuild/easyblocks/c/chapel.py
@@ -12,53 +12,46 @@ EasyBuild support for Chapel, implemented as an easyblock
 import os
 import shutil
 
-import easybuild.tools.toolkit as toolchain
-from easybuild.framework.easyblock import EasyBlock
-from easybuild.tools.filetools import run_cmd
+from easybuild.easyblocks.generic.configuremake import ConfigureMake
 
 
-class EB_Chapel(EasyBlock):
+class EB_Chapel(ConfigureMake):
     """Support for building Chapel."""
-
 
     def __init__(self, *args, **kwargs):
         """Initialize Chapel-specific variables."""
         super(EB_Chapel, self).__init__(*args, **kwargs)
         self.build_in_installdir = True
 
-
     def configure_step(self):
-        """Configure Chapel build using custom tools"""
-
+        """No configure step for Chapel."""
         pass
 
-
-    def build_step(self):
-        """Build and install Chapel"""
-
-        # enable parallel build
-        p = self.cfg['parallel']
-        self.par = ""
-        if p:
-            self.par = "-j %s" % p
-
-        # build chapel
-        cmd = "make %s" % self.par
-        run_cmd(cmd, log_all=True, simple=True, log_output=True)
-
+    # building is done via make, so taken care of by ConfigureMake easyblock
 
     def install_step(self):
-      """Installation of Chapel has alredy been done as part of the build procedure"""
+        """Installation of Chapel has already been done as part of the build procedure"""
+        pass
 
-	pass
+    def sanity_check_step(self):
+        """Custom sanity check for Chapel."""
 
+        libpath = os.path.join('lib', 'linux64', 'gnu', 'comm-none', 'substrate-none', 'seg-none',
+                               'mem-default', 'tasks-fifo', 'threads-pthreads', 'atomics-intrinsics')
+        custom_paths = {
+                        'files': ['bin/linux64/chpl', 'bin/linux64/chpldoc',
+                                  os.path.join(libpath, 'libchpl.a'), os.path.join(libpath, 'main.o')],
+                        'dirs': []
+                       }
+
+        super(EB_Chapel, self).sanity_check_step(custom_paths=custom_paths)
 
     def make_module_req_guess(self):
         """
         A dictionary of possible directories to look for; this is needed since bin/linux64 of chapel is non standard
         """
         return {
-                'PATH':['bin', 'bin/linux64', 'bin64'],
-                'LD_LIBRARY_PATH':['lib', 'lib/linux64', 'lib64'],
+                'PATH': ['bin', 'bin/linux64', 'bin64'],
+                'LD_LIBRARY_PATH': ['lib', 'lib/linux64', 'lib64'],
                }
 
