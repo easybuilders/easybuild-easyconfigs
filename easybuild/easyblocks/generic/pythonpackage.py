d154c638488d8ad953b9019a0f59317849103ba8
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-01-09 18:20:49 +0100

    initial split

--- easybuild/easyblocks/generic/pythonpackage.py
+++ easybuild/easyblocks/generic/pythonpackage.py
@@ -132,3 +132,128 @@ class PythonPackage(EasyBlock):
         txt += "prepend-path\tPYTHONPATH\t%s\n" % os.path.join(self.installdir , self.pylibdir)
 
         return txt
+
+class EB_DefaultPythonPackage(Extension):
+    """
+    Easyblock for Python packages to be included in the Python installation.
+    """
+
+    def __init__(self, mself, ext):
+        """Custom constructor for EB_DefaultPythonPackage: initialize class variables."""
+        super(EB_DefaultPythonPackage, self).__init__(mself, ext)
+
+        self.sitecfg = None
+        self.sitecfgfn = 'site.cfg'
+        self.sitecfglibdir = None
+        self.sitecfgincdir = None
+        self.testinstall = False
+        self.builddir = mself.builddir
+        self.mself = mself
+        self.installopts = ''
+        self.runtest = None
+        self.ext_dir = "%s/%s" % (self.builddir, self.name)
+        self.unpack_options = ''
+
+        self.python = get_software_root('Python')
+
+        ver = '.'.join(get_software_version('Python').split('.')[0:2])
+        self.python_libdir = os.path.join('lib', 'python%s' % ver, 'site-packages')
+
+        # make sure there's no site.cfg in $HOME, because setup.py will find it and use it
+        if os.path.exists(os.path.join(expanduser('~'), 'site.cfg')):
+            self.log.error('Found site.cfg in your home directory (%s), please remove it.' % expanduser('~'))
+
+    def configure_step(self):
+        """Configure Python package build
+        """
+
+        if self.sitecfg: # used by some extensions_step, like numpy, to find certain libs
+            finaltxt = self.sitecfg
+            if self.sitecfglibdir:
+                repl = self.sitecfglibdir
+                finaltxt = finaltxt.replace('SITECFGLIBDIR', repl)
+
+            if self.sitecfgincdir:
+                repl = self.sitecfgincdir
+                finaltxt = finaltxt.replace('SITECFGINCDIR', repl)
+
+            self.log.debug("Using %s: %s" % (self.sitecfgfn, finaltxt))
+
+            try:
+                if os.path.exists(self.sitecfgfn):
+                    txt = open(self.sitecfgfn).read()
+                    self.log.debug("Found %s: %s" % (self.sitecfgfn, txt))
+                config = open(self.sitecfgfn, 'w')
+                config.write(finaltxt)
+                config.close()
+            except IOError:
+                self.log.exception("Creating %s failed" % self.sitecfgfn)
+
+        # sanity checks for python being used
+        run_cmd("python -V")
+        run_cmd("which python")
+        run_cmd("python -c 'import sys; print(sys.executable)'")
+
+    def build_step(self):
+        """Build Python package via setup.py"""
+
+        cmd = "python setup.py build "
+
+        run_cmd(cmd, log_all=True, simple=True)
+
+    def install_step(self):
+        """Install built Python package"""
+
+        cmd = "python setup.py install --prefix=%s %s" % (self.python, self.installopts)
+        run_cmd(cmd, log_all=True, simple=True)
+
+    def test_step(self):
+        """Test the compilation
+        - default: None
+        """
+        extrapath = ""
+        testinstalldir = os.path.join(self.builddir, "eb_test_install_dir_%s" % self.name)
+        if self.testinstall:
+            # Install in test directory and export PYTHONPATH
+            try:
+                os.makedirs(testinstalldir)
+            except OSError:
+                self.log.exception("Creating testinstalldir %s failed" % testinstalldir)
+
+            cmd = "python setup.py install --prefix=%s %s" % (testinstalldir, self.installopts)
+            run_cmd(cmd, log_all=True, simple=True)
+
+            run_cmd("python -c 'import sys; print(sys.path)'")  # print Python search path (debug)
+            extrapath = "export PYTHONPATH=%s/%s:$PYTHONPATH && " % (testinstalldir, self.python_libdir)
+
+        if self.runtest:
+            cmd = "%s%s" % (extrapath, self.runtest)
+            run_cmd(cmd, log_all=True, simple=True)
+
+        if self.testinstall:
+            try:
+                rmtree2(testinstalldir)
+            except OSError, err:
+                self.log.exception("Removing testinstalldir %s failed: %s" % (testinstalldir, err))
+
+    def run(self):
+        """Perform the actual Python package build/installation procedure"""
+
+        # extract_file
+        if not self.src:
+            self.log.error("No source found for Python package %s, required for installation. (src: %s)" % \
+                           (self.name, self.src))
+        self.ext_dir = extract_file("%s" % self.src, "%s/%s" % (self.builddir, self.name), extra_options=self.unpack_options)
+
+        # patch if needed
+        if self.patches:
+            for patchfile in self.patches:
+                if not apply_patch(patchfile, self.ext_dir):
+                    self.log.error("Applying patch %s failed" % patchfile)
+
+        # configure, build_step, test, make install
+        self.configure_step()
+        self.build_step()
+        self.test_step()
+        self.install_step()
+
