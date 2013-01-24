1521340871e3c329da846a42e4be45b8bf3e12df
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-01-24 06:55:00 +0100

    Revert "don't return result of sanity check, doesn't make sense"
    This reverts commit 952ff47a0114682a65a4b601b2361c715e954516.

--- easybuild/easyblocks/generic/rpackage.py
+++ easybuild/easyblocks/generic/rpackage.py
@@ -175,7 +175,7 @@ class RPackage(ExtensionEasyBlock):
         """
         Custom sanity check for R packages
         """
-        super(RPackage, self).sanity_check_step(EXTS_FILTER_R_PACKAGES)
+        return super(RPackage, self).sanity_check_step(EXTS_FILTER_R_PACKAGES)
 
     def make_module_extra(self):
         """Add install path to R_LIBS"""
