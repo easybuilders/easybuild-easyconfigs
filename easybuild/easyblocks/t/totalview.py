f4472583c6578df2f5f892e3643eb1e2737a9c23
Fotis Georgatos <fotis.georgatos@uni.lu>
2013-02-19 03:02:09 +0100

    cleanup from noise comments
    Signed-off-by: Fotis Georgatos <fotis.georgatos@uni.lu>

--- easybuild/easyblocks/t/totalview.py
+++ easybuild/easyblocks/t/totalview.py
@@ -55,15 +55,3 @@ class EB_TotalView(EasyBlock):
                              }
 
         super(EB_TotalView, self).sanity_check_step(custom_paths=custom_paths)
-
-#    def make_module_extra(self):
-#        """Add correct path to lib to LD_LIBRARY_PATH."""
-#
-#        txt = super(EB_Tornado, self).make_module_extra()
-
-#        txt += self.moduleGenerator.prepend_paths('LD_LIBRARY_PATH', ["Tornado/bin/linux/", "ThirdParty/bin/linux/"])
-#        txt += self.moduleGenerator.prepend_paths('PATH', ["Tornado/bin/linux/"] )
-#        txt += self.moduleGenerator.set_environment('TORNADO_ROOT_PATH', "$root" )
-#        txt += self.moduleGenerator.set_environment('TORNADO_DATA_PATH', "$root/Data/WEST" )
-
-#        return txt
