d940baeca3a91c748babb350138b31b881d94793
Fotis Georgatos <fotis.georgatos@uni.lu>
2013-02-20 02:01:13 +0100

    fix all e easyblocks
    Signed-off-by: Fotis Georgatos <fotis.georgatos@uni.lu>

--- easybuild/easyblocks/e/eigen.py
+++ easybuild/easyblocks/e/eigen.py
@@ -1,12 +1,20 @@
-# This file is an EasyBuild recipy as per https://github.com/hpcugent/easybuild
+##
+# This file is an EasyBuild reciPY as per https://github.com/hpcugent/easybuild
 #
-# Copyright:: Copyright (c) 2012 University of Luxembourg / LCSB
-# Author::    Cedric Laczny <cedric.laczny@uni.lu>, Fotis Georgatos <fotis.georgatos@uni.lu>
+# Copyright:: Copyright 2012-2013 University of Luxembourg/Luxembourg Centre for Systems Biomedicine
+# Authors::   Cedric Laczny <cedric.laczny@uni.lu>, Fotis Georgatos <fotis.georgatos@uni.lu>, Kenneth Hoste
 # License::   MIT/GPL
-# File::      $File$ 
-# Date::      $Date$
+# $Id$
+#
+# This work implements a part of the HPCBIOS project and is a component of the policy:
+# http://hpcbios.readthedocs.org/en/latest/HPCBIOS_2012-94.html
+##
 """
 EasyBuild support for building and installing Eigen, implemented as an easyblock
+
+@author: Cedric Laczny (Uni.Lu)
+@author: Fotis Georgatos (Uni.Lu)
+@author: Kenneth Hoste (Ghent University)
 """
 
 import os
