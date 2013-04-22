1c24c01f286b3ddce8f95221647cbfbe8de1f465
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-04-22 15:46:28 +0200

    add unit test to test initialization of easyblocks

---
+++ easybuild/easyblocks/test/init_easyblocks.py
@@ -0,0 +1,110 @@
+##
+# Copyright 2013 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+Unit tests for initializing easyblocks.
+
+@author: Kenneth Hoste (Ghent University)
+"""
+
+import glob
+import os
+import re
+import tempfile
+from vsc import fancylogger
+from unittest import TestCase, TestLoader, main
+
+from easybuild.framework.easyblock import get_class
+from easybuild.framework.easyconfig import MANDATORY
+
+class InitTest(TestCase):
+    """ Baseclass for easyblock testcases """
+
+    def writeEC(self, easyblock, extratxt=''):
+        """ create temporary easyconfig file """
+        txt = '\n'.join([
+            'easyblock = "%s"',
+            'name = "foo"',
+            'version = "1.3.2"',
+            'homepage = "http://example.com"',
+            'description = "Dummy easyconfig file."',
+            'toolchain = {"name": "dummy", "version": "dummy"}',
+            'sources = []',
+            extratxt,
+        ])
+
+        f = open(self.eb_file, "w")
+        f.write(txt % easyblock)
+        f.close()
+
+    def setUp(self):
+        """ setup """
+        self.log = fancylogger.getLogger("EasyConfigTest", fname=False)
+        fd, self.eb_file = tempfile.mkstemp(prefix='easyblocks_init_test_', suffix='.eb')
+        os.close(fd)
+
+    def test_init_all(self):
+        """Test whether all easyconfigs can be initialized."""
+
+        class_regex = re.compile("^class (.*)\(.*", re.M)
+
+        all_pys = glob.glob('%s/*/*.py' % os.path.dirname(os.path.dirname(__file__)))
+        easyblocks = [eb for eb in all_pys if not eb.endswith('__init__.py') and not '/test/' in eb]
+        for easyblock in easyblocks:
+            self.log.debug("easyblock: %s" % easyblock)
+
+            # obtain easyblock class name using regex
+            f = open(easyblock, "r")
+            txt = f.read()
+            f.close()
+
+            res = class_regex.search(txt)
+            if res:
+                ebname = res.group(1)
+                self.log.debug("Found class name for easyblock %s: %s" % (easyblock, ebname))
+                print ("Testing initialisation for easyblock %s (in %s)" % (ebname, easyblock))
+
+                # figure out list of mandatory variables, and define with dummy values as necessary
+                app_class = get_class(ebname)
+                ec_opts = app_class.extra_options()
+                extra_txt = ''
+                for ec_opt in ec_opts:
+                    if ec_opt[1][2] == MANDATORY:
+                        extra_txt += '%s = "foo"\n' % ec_opt[0]
+
+                # write easyconfig file
+                self.writeEC(ebname, extra_txt)
+
+                # initialize easyblock
+                # if this doesn't fail, the test succeeds
+                app = app_class(self.eb_file)
+            else:
+                self.assertTrue(False, "Class found in easyblock %s" % easyblock)
+
+def suite():
+    """ return all the tests in this file """
+    return TestLoader().loadTestsFromTestCase(InitTest)
+
+if __name__ == '__main__':
+    main()
---
+++ easybuild/easyblocks/test/suite.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+##
+# Copyright 2012-2013 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+This script is a collection of all the testcases for easybuild-easyblocks.
+Usage: "python -m easybuild.easyblocks.test.suite.py" or "./easybuild/easyblocks/test/suite.py"
+
+@author: Toon Willems (Ghent University)
+@author: Kenneth Hoste (Ghent University)
+"""
+import os
+import sys
+import tempfile
+import unittest
+from vsc import fancylogger
+
+# toolkit should be first to allow hacks to work
+import easybuild.easyblocks.test.init_easyblocks as i
+
+# initialize logger for all the unit tests
+fd, log_fn = tempfile.mkstemp(prefix='easybuild-easyblocks-tests-', suffix='.log')
+os.close(fd)
+fancylogger.logToFile(log_fn)
+log = fancylogger.getLogger()
+log.setLevelName('DEBUG')
+
+# call suite() for each module and then run them all
+SUITE = unittest.TestSuite([x.suite() for x in [i]])
+
+# uses XMLTestRunner if possible, so we can output an XML file that can be supplied to Jenkins
+xml_msg = ""
+try:
+    import xmlrunner  # requires unittest-xml-reporting package
+    xml_dir = 'test-reports'
+    res = xmlrunner.XMLTestRunner(output=xml_dir, verbosity=1).run(SUITE)
+    xml_msg = ", XML output of tests available in %s directory" % xml_dir
+except ImportError, err:
+    sys.stderr.write("WARNING: xmlrunner module not available, falling back to using unittest...\n\n")
+    res = unittest.TextTestRunner().run(SUITE)
+
+fancylogger.logToFile(log_fn, enable=False)
+
+if not res.wasSuccessful():
+    sys.stderr.write("ERROR: Not all tests were successful.\n")
+    print "Log available at %s" % log_fn, xml_msg
+    sys.exit(2)
+else:
+    os.remove(log_fn)
