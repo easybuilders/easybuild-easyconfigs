20b0bef23ebbe75d59d9f6d1376aba6e18ab4bd3
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-05-21 11:18:53 +0200

    really fix remark

--- easybuild/easyblocks/s/scalapack.py
+++ easybuild/easyblocks/s/scalapack.py
@@ -87,7 +87,7 @@ class EB_ScaLAPACK(ConfigureMake):
         """Build ScaLAPACK using make after setting make options."""
 
         # MPI compiler commands
-        known_mpi_libs = [toolchain.OPENMPI, toolchain.MVAPICH2, toolchain.MPICH]  #@UndefinedVariable
+        known_mpi_libs = [toolchain.OPENMPI, toolchain.MVAPICH2, toolchain.MPICH, toolchain.QLOGICMPI]  #@UndefinedVariable
         if os.getenv('MPICC') and os.getenv('MPIF77') and os.getenv('MPIF90'):
             mpicc = os.getenv('MPICC')
             mpif77 = os.getenv('MPIF77')
@@ -162,8 +162,7 @@ class EB_ScaLAPACK(ConfigureMake):
         else:
 
             # determine interface
-            if self.toolchain.mpi_family() in [toolchain.OPENMPI, toolchain.MVAPICH2, toolchain.MPICH,  #@UndefinedVariable
-                                               toolchain.QLOGICMPI]:  #@UndefinedVariable
+            if self.toolchain.mpi_family() in known_mpi_libs:
                 interface = 'Add_'
             else:
                 self.log.error("Don't know which interface to pick for the MPI library being used.")
