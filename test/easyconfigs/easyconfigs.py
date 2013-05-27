e6469feb21fb5f8fc852b7eb83ce2f2f24ca8c74
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-05-27 13:58:25 +0200

    fix use of mkdtemp

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -64,7 +64,7 @@ class EasyConfigTest(TestCase):
 
     def setUp(self):
         """Setup."""
-        config.variables['tmp_logdir'] = mkdtemp(prefix='easyconfigs_test_')
+        config.variables['tmp_logdir'] = tempfile.mkdtemp(prefix='easyconfigs_test_')
 
     def tearDown(self):
         """Cleanup."""
