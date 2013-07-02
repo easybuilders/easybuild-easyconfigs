038bb5b6e9cb46cca3cf2869aa54ff95891b1364
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-07-02 12:28:42 +0200

    set  as pre make opt, to avoid overwriting required -D preprocessor options added in makefile

--- easybuild/easyblocks/m/mothur.py
+++ easybuild/easyblocks/m/mothur.py
@@ -57,7 +57,8 @@ class EB_Mothur(ConfigureMake):
             self.cfg.update('makeopts', 'USEREADLINE=yes')
         # enable MPI support
         if self.toolchain.options['usempi']:
-            self.cfg.update('makeopts', 'USEMPI=yes CXX="%s" CXXFLAGS="%s -DMPICH_IGNORE_CXX_SEEK"' % (os.getenv('MPICXX'), os.getenv('CXXFLAGS')))
+            self.cfg.update('makeopts', 'USEMPI=yes CXX="%s"' % os.getenv('MPICXX'))
+            self.cfg.update('premakeopts', 'CXXFLAGS="$CXXFLAGS -DMPICH_IGNORE_CXX_SEEK"')
         # enable compression
         if get_software_root('bzip2') or get_software_root('gzip'):
             self.cfg.update('makeopts', 'USE_COMPRESSION=yes')
