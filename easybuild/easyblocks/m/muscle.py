e55d3167017c623e765d73cda95eae74812ccd60
George Tsouloupas <george.tsouloupas@gmail.com>
2013-07-03 10:58:05 +0300

    Modify style

--- easybuild/easyblocks/m/muscle.py
+++ easybuild/easyblocks/m/muscle.py
@@ -2,7 +2,7 @@
 # This file is an EasyBuild reciPY as per https://github.com/hpcugent/easybuild
 #
 # Copyright:: Copyright 2012-2013 The Cyprus Institute
-# Authors::   Andreas Panteli <a.panteli@cyi.ac.cy>, George Tsouloupas <g.tsouloupas@cyi.ac.cy>
+# Authors::   George Tsouloupas <g.tsouloupas@cyi.ac.cy>, Andreas Panteli <a.panteli@cyi.ac.cy>
 # License::   MIT/GPL
 # $Id$
 #
@@ -10,7 +10,8 @@
 """
 EasyBuild support for building and installing MUSCLE, implemented as an easyblock
 
-@author: Andreas Panteli (CyI), George Tsouloupas (CyI)
+@author: George Tsouloupas (CyI)
+@author: Andreas Panteli (CyI)
 """
 
 import os
@@ -19,27 +20,28 @@ from easybuild.framework.easyblock import EasyBlock
 from easybuild.easyblocks.generic.packedbinary import PackedBinary
 from easybuild.tools.filetools import mkdir, extract_file
 
+
 class EB_MUSCLE(PackedBinary):
-	"""
-	Support for building MUSCLE
+    """
+    Support for building MUSCLE
 
-	"""
-	def extract_step(self):
-		"""
-		Unpack the source files.
-		"""
+    """
+    def extract_step(self):
+        """
+        Unpack the source files.
+        """
 
-		newdir = '%s-%s' % (self.name.lower(), self.version)
-		self.cfg['start_dir'] = os.path.join(self.builddir, newdir)
-		try:
-			mkdir(self.cfg['start_dir'])
-			self.log.debug("Created new directory %s" % (self.cfg['start_dir']))
-		except OSError, err:
-                        self.log.exception("Can't create new directory %s: %s" % (self.cfg['start_dir'], err))
-		for tmp in self.src:
-			self.log.info("Unpacking source %s" % tmp['name'])
-			srcdir = extract_file(tmp['path'], self.cfg['start_dir'], cmd=tmp['cmd'], extra_options=self.cfg['unpack_options'])
-			if srcdir:
-				self.src[self.src.index(tmp)]['finalpath'] = srcdir
-			else:
-				self.log.error("Unpacking source %s failed" % tmp['name'])	
+        newdir = '%s-%s' % (self.name.lower(), self.version)
+        self.cfg['start_dir'] = os.path.join(self.builddir, newdir)
+        try:
+            mkdir(self.cfg['start_dir'])
+            self.log.debug("Created new directory %s" % (self.cfg['start_dir']))
+        except OSError, err:
+            self.log.exception("Can't create new directory %s: %s" % (self.cfg['start_dir'], err))
+        for tmp in self.src:
+            self.log.info("Unpacking source %s" % tmp['name'])
+            srcdir = extract_file(tmp['path'], self.cfg['start_dir'], cmd=tmp['cmd'], extra_options=self.cfg['unpack_options'])
+            if srcdir:
+                self.src[self.src.index(tmp)]['finalpath'] = srcdir
+            else:
+                self.log.error("Unpacking source %s failed" % tmp['name'])    
