b3c99d60e0b99a0b60084e41d36f428b0f851d4d
Ward Poelmans <wpoely86@gmail.com>
2013-08-07 14:37:37 +0200

    GCC: unset env vars that cause build errors
    Unset some environment variables that can cause nasty build errors when
    bootstrapping GCC.

--- easybuild/easyblocks/g/gcc.py
+++ easybuild/easyblocks/g/gcc.py
@@ -31,6 +31,7 @@ EasyBuild support for building and installing GCC, implemented as an easyblock
 @author: Pieter De Baets (Ghent University)
 @author: Jens Timmerman (Ghent University)
 @author: Toon Willems (Ghent University)
+@author: Ward Poelmans (Ghent University)
 """
 
 import re
@@ -57,6 +58,10 @@ class EB_GCC(ConfigureMake):
         super(EB_GCC, self).__init__(*args, **kwargs)
 
         self.stagedbuild = False
+
+        # unset some environment variables as they can cause nasty build errors
+        # when bootstrapping
+        self.unwanted_env_vars = ['CPATH', 'C_INCLUDE_PATH', 'CPLUS_INCLUDE_PATH', 'OBJC_INCLUDE_PATH', 'LIBRARY_PATH']
 
     @staticmethod
     def extra_options():
