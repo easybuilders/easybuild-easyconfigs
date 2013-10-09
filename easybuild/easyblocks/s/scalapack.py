9dbe11c03b8c87698459517ce67e7e35b9a59776
Bart Verleye <b.verleye@auckland.ac.nz>
2013-10-09 14:53:26 +1300

    mpich2 is now a valid mpi version.

--- easybuild/easyblocks/s/scalapack.py
+++ easybuild/easyblocks/s/scalapack.py
@@ -87,7 +87,7 @@ class EB_ScaLAPACK(ConfigureMake):
         """Build ScaLAPACK using make after setting make options."""
 
         # MPI compiler commands
-        known_mpi_libs = [toolchain.OPENMPI, toolchain.MVAPICH2, toolchain.MPICH, toolchain.QLOGICMPI]  #@UndefinedVariable
+        known_mpi_libs = [toolchain.MPICH2,toolchain.OPENMPI, toolchain.MVAPICH2, toolchain.MPICH, toolchain.QLOGICMPI]  #@UndefinedVariable
         if os.getenv('MPICC') and os.getenv('MPIF77') and os.getenv('MPIF90'):
             mpicc = os.getenv('MPICC')
             mpif77 = os.getenv('MPIF77')
