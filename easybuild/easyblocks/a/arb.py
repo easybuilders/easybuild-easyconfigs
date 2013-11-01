bce6c8bf9c9050a15b013e614bdf3459447ab80a
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-11-01 19:45:25 +0100

    really ignore exit code of make to configure

--- easybuild/easyblocks/a/arb.py
+++ easybuild/easyblocks/a/arb.py
@@ -62,7 +62,7 @@ class EB_ARB(ConfigureMake):
         self.cfg.update('makeopts', 'all OPENGL=0 V=1 ARB_64=1')
 
         # run 'make' without arguments to configure build, ignore non-zero exit code
-        run_cmd("make", simple=False)
+        run_cmd("make", simple=False, log_all=False, log_ok=False)
 
         super(EB_ARB, self).build_step()
 
