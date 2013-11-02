8c9ff16ee4e33e45abd5c2671822bd32d5fe05d2
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-11-02 14:21:10 +0100

    style fixes, get rid of test.pl file that crept in

--- easybuild/easyconfigs/p/test.pl
+++
@@ -1,8 +0,0 @@
-use strict;
- use Test::More;
-use Config;
-    my $tainted_path = substr($^X,0,0) . "/no/such/file";
-    my $err;
-    # $! is used in a tainted expression, so gets tainted
-    open my $fh, $tainted_path or $err= "$!";
-    unlike($err, qr/^\d+$/, 'tainted $!');
