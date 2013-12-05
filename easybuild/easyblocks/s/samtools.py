b4bac923a931e3d5a46d98d3d5ceb87282800064
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-12-05 11:56:10 +0100

    clean up version-dependent list of binaries in SAMtools easyblock

--- easybuild/easyblocks/s/samtools.py
+++ easybuild/easyblocks/s/samtools.py
@@ -33,26 +33,18 @@ class EB_SAMtools(ConfigureMake):
         """Define lists of files to install."""
         super(EB_SAMtools, self).__init__(*args, **kwargs)
 
-        if LooseVersion(self.version) <= LooseVersion("0.1.18"):
-
-
-            self.bin_files = ["bcftools/vcfutils.pl", "bcftools/bcftools", "misc/blast2sam.pl",
-                          "misc/bowtie2sam.pl", "misc/export2sam.pl", "misc/interpolate_sam.pl",
-                          "misc/novo2sam.pl", "misc/psl2sam.pl", "misc/sam2vcf.pl", "misc/samtools.pl",
-                          "misc/soap2sam.pl", "misc/varfilter.py", "misc/wgsim_eval.pl",
-                          "misc/zoom2sam.pl", "misc/md5sum-lite", "misc/md5fa", "misc/maq2sam-short",
-                          "misc/maq2sam-long", "misc/wgsim", "misc/seqtk", "samtools"]
-
-        else:
-
-            self.bin_files = ["bcftools/vcfutils.pl", "bcftools/bcftools", "misc/blast2sam.pl",
-                          "misc/bowtie2sam.pl", "misc/export2sam.pl", "misc/interpolate_sam.pl",
-                          "misc/novo2sam.pl", "misc/psl2sam.pl", "misc/sam2vcf.pl", "misc/samtools.pl",
-                          "misc/soap2sam.pl", "misc/varfilter.py", "misc/wgsim_eval.pl",
-                          "misc/zoom2sam.pl", "misc/md5sum-lite", "misc/md5fa", "misc/maq2sam-short",
-                          "misc/maq2sam-long", "misc/wgsim", "misc/ace2sam",
-                          "misc/bamcheck","misc/plot-bamcheck","misc/r2plot.lua", "samtools"]
-            
+        self.bin_files = ["bcftools/vcfutils.pl", "bcftools/bcftools", "misc/blast2sam.pl",
+                      "misc/bowtie2sam.pl", "misc/export2sam.pl", "misc/interpolate_sam.pl",
+                      "misc/novo2sam.pl", "misc/psl2sam.pl", "misc/sam2vcf.pl", "misc/samtools.pl",
+                      "misc/soap2sam.pl", "misc/varfilter.py", "misc/wgsim_eval.pl",
+                      "misc/zoom2sam.pl", "misc/md5sum-lite", "misc/md5fa", "misc/maq2sam-short",
+                      "misc/maq2sam-long", "misc/wgsim", "samtools"]
+        if LooseVersion(self.version) <= LooseVersion('0.1.18'):
+            # seqtk is no longer there in v0.1.19
+            self.binfiles += ["misc/seqtk"]
+        elif LooseVersion(self.version) >= LooseVersion('0.1.19'):
+            # new tools in v0.1.18
+            self.binfiles += ["misc/ace2sam", "misc/bamcheck","misc/plot-bamcheck","misc/r2plot.lua"]
 
         self.lib_files = ["libbam.a"]
         self.include_files = ["bam.h", "bam2bcf.h", "bam_endian.h", "bgzf.h", "errmod.h", "faidx.h", "kaln.h",
