1b13509a158cf927a028a10092b28ff9dcf94e10
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-12-20 08:59:30 +0100

    add easyblock for GROMACS

---
+++ easybuild/easyblocks/g/gromacs.py
@@ -0,0 +1,97 @@
+##
+# Copyright 2013 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+EasyBuild support for building and installing GROMACS, implemented as an easyblock
+
+@author: Kenneth Hoste (Ghent University)
+"""
+import os
+from distutils.version import LooseVersion
+
+from easybuild.easyblocks.generic.cmakemake import CMakeMake
+from easybuild.tools.modules import get_software_root
+
+
+class EB_GROMACS(CMakeMake):
+    """Support for building/installing GROMACS."""
+
+    def configure_step(self):
+        """Custom configuration procedure for GROMACS: set configure options for configure or cmake."""
+
+        if LooseVersion(self.version) < LooseVersion('4.6'):
+            self.log.info("Using configure script for configuring GROMACS build.")
+            self.log.error("Configuration procedure for older GROMACS versions not implemented yet.")
+        else:
+            # always specify to use external BLAS/LAPACK
+            self.cfg.update('configopts', "-DGMX_EXTERNAL_BLAS=ON -DGMX_EXTERNAL_LAPACK=ON")
+
+            # disable GUI tools
+            self.cfg.update('configopts', "-DGMX_X11=OFF")
+
+            # enable MPI support if desired
+            if self.toolchain.options.get('usempi', None):
+                self.cfg.update('configopts', "-DGMX_MPI=ON")
+
+            # explicitely disable GPU support if CUDA is not available,
+            # to avoid that GROMACS find and uses a system-wide CUDA compiler
+            if not get_software_root('CUDA'):
+                self.cfg.update('configopts', "-DGMX_GPU=OFF")
+
+            if get_software_root('MKL'):
+                # using MKL for FFT, so it will also be used for BLAS/LAPACK
+                self.cfg.update('configopts', '-DGMX_FFT_LIBRARY=mkl -DMKL_INCLUDE_DIR="$EBROOTMKL/mkl/include" ')
+                mkl_libs = [os.path.join(os.getenv('LAPACK_LIB_DIR'), lib) for lib in ['libmkl.a', 'libmkl_lapack.a']]
+                self.cfg.update('configopts', '-DMKL_LIBRARIES="%s" ' % ';'.join(mkl_libs))
+            else:
+                for libname in ['BLAS', 'LAPACK']:
+                    lib_dir = os.getenv('%s_LIB_DIR' % libname)
+                    libs = os.getenv('LIB%s' % libname)
+                    self.cfg.update('configopts', '-DGMX_%s_USER="-L%s %s"' % (libname, lib_dir, libs))
+
+            # set regression test path
+            prefix = 'regressiontests'
+            if any([src['name'].startswith(prefix) for src in self.src]):
+                self.cfg.update('configopts', "-DREGRESSIONTEST_PATH='%%(builddir)s/%s-%%(version)s' " % prefix)
+            
+
+        # complete configuration with configure_method of parent
+        super(EB_GROMACS, self).configure_step()
+
+    def test_step(self):
+        """Specify to running tests is done using 'make check'."""
+        # allow to escape testing by setting runtest to False
+        if not self.cfg['runtest'] and not isinstance(self.cfg['runtest'], bool):
+            self.cfg['runtest'] = 'check'
+
+        super(EB_GROMACS, self).test_step()
+
+    def sanity_check_step(self):
+        """Custom sanity check for GROMACS."""
+
+        custom_paths = {
+            'files': ['bin/gromacs'],
+            'dirs': [],
+        }
+        super(EB_GROMACS, self).sanity_check_step(custom_paths=custom_paths)
