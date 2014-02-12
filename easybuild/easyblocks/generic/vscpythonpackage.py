f0bd842374c663176c9c365058819cc90b38d73d
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-02-12 22:09:57 +0100

    add log.deprecated in VSCPythonPackage constructor

--- easybuild/easyblocks/generic/vscpythonpackage.py
+++ easybuild/easyblocks/generic/vscpythonpackage.py
@@ -35,7 +35,12 @@ from easybuild.easyblocks.generic.versionindependendpythonpackage import Version
 # EasyBuild provides its own 'vsc' namespace, that shouldn't be mixed with a 'vsc' namespace available somewhere else
 # therefore, we need a dedicated easyblock for VSC-tools packages like vsc-base, vsc-mympirun, etc.
 class VSCPythonPackage(VersionIndependendPythonPackage):
-    """Support for install VSC-tools packages."""
+    """Support for install VSC Python packages."""
+
+    def __init__(self, *args, **kwargs):
+        """Custom constructor for VSC Python packages."""
+        super(VSCPythonPackage, self).__init__(*args, **kwargs)
+        self.log.deprecated("VSCPythonPackage is only there because EasyBuild provides a 'vsc' namespace too", '2.0')
 
     def sanity_check_step(self, *args, **kwargs):
         """Custom sanity check for VSC-tools packages."""
