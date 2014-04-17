2a9961e30f5c1b78bd5ce12b39a2b3ca9bc4777c
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-04-17 13:47:07 +0200

    really fix specifying subdir to build Charm++

--- easybuild/easyblocks/n/namd.py
+++ easybuild/easyblocks/n/namd.py
@@ -48,7 +48,7 @@ class EB_NAMD(MakeCp):
         extract_file(charm_tarballs[0], os.getcwd())
 
         cmd = "./build charm++ %s -j %s" % (self.cfg["charm_opts"], self.cfg['parallel'])
-        run_cmd(cmd, path=charm_tarballs[0].split('.')[0])
+        run_cmd(cmd, path=os.path.basename(charm_tarballs[0]).split('.')[0])
 
         cmd = "./config %s %s " % (self.cfg["namd_arch"], self.cfg["namd_cfg_opts"])
         run_cmd(cmd)
