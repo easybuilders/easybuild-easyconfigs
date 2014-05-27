9d5a5ffa45b5ad4f47e681a30ba29ad008b3ae65
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-05-27 23:10:19 +0200

    add comment to fix remark

--- easybuild/easyblocks/c/cplex.py
+++ easybuild/easyblocks/c/cplex.py
@@ -56,6 +56,7 @@ class EB_CPLEX(Binary):
     @staticmethod
     def extra_options():
         extra_vars = [
+            # staged install via a tmp dir can help with the hard (potentially faulty) check on available disk space
             ('staged_install', [False, "Should the installation should be staged via a temporary dir?", CUSTOM]),
         ]
         return Binary.extra_options(extra_vars)
