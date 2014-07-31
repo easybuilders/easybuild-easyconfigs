1ea3a95a441555056fba49b42e618d833a6543ac
Jens Timmerman <jens.timmerman@ugent.be>
2014-07-31 13:55:35 +0200

    new noqa entry for new versions of qt

--- easybuild/easyblocks/q/qt.py
+++ easybuild/easyblocks/q/qt.py
@@ -64,6 +64,7 @@ class EB_Qt(ConfigureMake):
             "WARNING .*",
             "Project MESSAGE:.*",
             "rm -f .*",
+            'Creating qmake...',
         ]
         run_cmd_qa(cmd, qa, no_qa=no_qa, log_all=True, simple=True)
 
