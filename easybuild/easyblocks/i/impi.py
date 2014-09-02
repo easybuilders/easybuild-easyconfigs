358d593905a454d15583084b00d69cfecd5996d5
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-09-02 14:12:07 +0200

    enhance impi easyblock to handle install subdir crap for impi v5.0.1 too

--- easybuild/easyblocks/i/impi.py
+++ easybuild/easyblocks/i/impi.py
@@ -33,6 +33,7 @@ EasyBuild support for installing the Intel MPI library, implemented as an easybl
 """
 
 import os
+import shutil
 from distutils.version import LooseVersion
 
 from easybuild.easyblocks.generic.intelbase import IntelBase, ACTIVATION_NAME_2012, LICENSE_FILE_NAME_2012
@@ -62,13 +63,20 @@ class EB_impi(IntelBase):
                     'license_file_name': LICENSE_FILE_NAME_2012,
                 })
 
-            if LooseVersion(self.version) == LooseVersion('4.1.1.036'):
-                # impi v4.1.1 installer creates impi/<version> subdir itself, so specify parent install dir
-                silent_cfg_names_map.update({
-                    'install_dir': install_path(),
-                })
-
             super(EB_impi, self).install_step(silent_cfg_names_map=silent_cfg_names_map)
+
+            # impi v4.1.1 and v5.0.1 installers create impi/<version> subdir, so stuff needs to be moved afterwards
+            if LooseVersion(self.version) in map(LooseVersion, ['4.1.1.036', '5.0.1.035']):
+                subdir = os.path.join(self.installdir, self.name, self.version)
+                self.log.debug("Moving contents of %s to %s" % (subdir, self.installdir))
+                try:
+                    for fil in os.listdir(subdir):
+                        shutil.move(os.path.join(subdir, fil), os.path.join(self.installdir, fil))
+                    # remove now senseless symlinks, e.g. impi_5.0.1 and impi_latest
+                    shutil.rmtree(os.path.join(self.installdir, 'impi_%s' % '.'.join(self.version.split('.')[:-1])))
+                    shutil.rmtree(os.path.join(self.installdir, 'impi_latest'))
+                except OSError, err:
+                    self.log.error("Failed to move contents of %s to %s" % (subdir, self.installdir))
         else:
             # impi up until version 4.0.0.x uses custom installation procedure.
             silent = \
