3946027aca3ea1a2becb651144f0f46d03b50462
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-10-07 15:27:35 +0200

    extend noqa list for OpenFOAM-Extend build command

--- easybuild/easyblocks/o/openfoam.py
+++ easybuild/easyblocks/o/openfoam.py
@@ -166,6 +166,7 @@ class EB_OpenFOAM(EasyBlock):
                 "configure: creating.*",
                 "%s .*" % os.environ['CC'],
                 "wmake .*",
+                "Making dependency list for source file.*",
             ]
             run_cmd_qa(cmd_tmpl % 'Allwmake.firstInstall', qa, no_qa=noqa, log_all=True, simple=True)
         else:
