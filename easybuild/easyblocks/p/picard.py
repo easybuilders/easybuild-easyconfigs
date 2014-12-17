b2886e91432ca4ca8e0c39829ca56510742a5996
pescobar <pescobar001@gmail.com>
2014-12-17 17:36:27 +0100

    improved sanity check

--- easybuild/easyblocks/p/picard.py
+++ easybuild/easyblocks/p/picard.py
@@ -36,6 +36,7 @@ import os
 import re
 import shutil
 
+from distutils.version import LooseVersion
 from easybuild.framework.easyblock import EasyBlock
 
 
@@ -70,8 +71,11 @@ class EB_picard(EasyBlock):
 
     def sanity_check_step(self):
         """Custom sanity check for picard"""
+        jar_files = ['picard']
+        if LooseVersion(self.version) < LooseVersion('1.115'):
+            jar_files.append('sam')
         custom_paths = {
-            'files': ["%s-%s.jar" % (x, self.version) for x in ['picard']],
+            'files': ["%s-%s.jar" % (x, self.version) for x in jar_files],
             'dirs': [],
         }
         super(EB_picard, self).sanity_check_step(custom_paths=custom_paths)
