c54d8ac9df2e9f206f12c5475806694d29141919
ocaisa <a.ocais@fz-juelich.de>
2015-03-13 09:47:22 +0100

    Incorporated use of toolchain.MPI_FAMILY
    Along the lines of the imkl easyblock, added support for checking the MPI compatibility based on the toolchain mpi_family() as well as keeping the fallback for weird situations

--- easybuild/easyblocks/c/cp2k.py
+++ easybuild/easyblocks/c/cp2k.py
@@ -272,14 +272,30 @@ class EB_CP2K(EasyBlock):
             regflags = 'NOOPT'
 
         # make sure a MPI-2 able MPI lib is used
-        mpi2libs = ['impi', 'MVAPICH2', 'OpenMPI', 'MPICH2', 'MPICH']
         mpi2 = False
-        for mpi2lib in mpi2libs:
-            if get_software_root(mpi2lib):
+        if hasattr(self.toolchain, 'MPI_FAMILY') and self.toolchain.MPI_FAMILY is not None:
+            mpi_spec_by_fam = {
+                toolchain.MPICH: 'mpi2',  # MPICH is MPICH v3.x, which is MPI2 compatible
+                toolchain.MPICH2: 'mpi2',
+                toolchain.MVAPICH2: 'mpi2',
+                toolchain.OPENMPI: 'mpi2',
+                toolchain.IMPI: 'mpi2',
+            }
+            mpi_fam = self.toolchain.mpi_family()
+            mpi_spec = mpi_spec_by_fam.get(mpi_fam)
+            if mpi_spec is not None:
                 mpi2 = True
-            else:
-                self.log.debug("MPI-2 supporting MPI library %s not loaded.")
-
+            self.log.debug("Determined MPI specification based on MPI toolchain component: %s" % mpi_spec)
+        else:
+            # can't use toolchain.mpi_family, because of dummy toolchain
+            mpi2libs = ['impi', 'MVAPICH2', 'OpenMPI', 'MPICH2', 'MPICH']
+            for mpi2lib in mpi2libs:
+                if get_software_root(mpi2lib):
+                    mpi2 = True
+                    self.log.debug("Determined MPI specification based on loaded MPI module: %s")
+                else:
+                    self.log.debug("MPI-2 supporting MPI library %s not loaded.")
+            
         if not mpi2:
             self.log.error("CP2K needs MPI-2, no known MPI-2 supporting library loaded?")
 
