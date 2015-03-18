1b28c8d02fc98f2aa47b60ba3ca83e198c49b5f1
Jens Timmerman <jens.timmerman@gmail.com>
2015-03-18 11:32:31 +0100

    fixed remarks

--- easybuild/easyblocks/x/xmipp.py
+++ easybuild/easyblocks/x/xmipp.py
@@ -1,5 +1,5 @@
 ##
-# Copyright 2009-2015 Ghent University
+# Copyright 2015-2015 Ghent University
 #
 # This file is part of EasyBuild,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
@@ -35,82 +35,118 @@ from easybuild.tools.filetools import mkdir, extract_file
 from easybuild.tools.modules import get_software_root, get_software_version
 from easybuild.tools.run import run_cmd
 
+from easybuild.easyblocks.generic.pythonpackage import det_pylibdir
+
 
 class EB_Xmipp(EasyBlock):
     """Support for building/installing Xmipp."""
 
     def __init__(self, *args, **kwargs):
         """Easyblock constructor, enable building in installation directory."""
-        super(self.__class__, self).__init__(*args, **kwargs)
+        super(EB_Xmipp, self).__init__(*args, **kwargs)
         self.build_in_installdir = True
 
     def extract_step(self):
         """Extract sources."""
         # strip off 'xmipp' part to avoid having everything in a 'xmipp' subdirectory
-        self.cfg['unpack_options'] = "--strip-components=1"
-        super(self.__class__, self).extract_step()
-
-    def configure_step(self):
-        """Configure xmipp build via a provided wrapper around sconss."""
-        # install step expects these to exist
-        mkdir('lib')
-        mkdir('bin')
-        # run interactive 'config' script to generate install.info file
-        try:
-            # ln -s `which python` xmipp_python
-            orig = os.path.join(get_software_root('Python'), 'bin', 'python')
-            dest = 'xmipp_python'
-            os.symlink(orig, dest)
-            # ln -s $EBROOTPYTHON/lib/python2.7 lib/python2.7
-            self.python_short_ver = ".".join(get_software_version('Python').split(".")[0:2])
-            orig = os.path.join(get_software_root('Python'), 'lib', 'python%s' % self.python_short_ver)
-            dest = os.path.join('lib', 'python%s' % self.python_short_ver)
-            os.symlink(orig, dest)
-        except OSError as err:
-            self.log.error("Failed to symlink %s to %s: %s" % (orig, dest, err))
+        self.cfg.update('unpack_options', '--strip-components=1')
+        super(EB_Xmipp, self).extract_step()
 
         # extract some dependencies that we really need and can't find anywhere else
         # alglib tarball has version in name, so lets find it with a glob
         alglib_tar = glob.glob(os.path.join(os.getcwd(), 'external', 'alglib*.tgz'))[0]
         for src in ['bilib.tgz', 'bilib.tgz', 'condor.tgz', alglib_tar]:
-            external_path = os.path.join(os.getcwd(), 'external')
+            external_path = os.path.join(self.cfg['start_dir'], 'external')
             extract_file(os.path.join(external_path, src), external_path)
 
-        cmd = ('{preconfigopts} PATH=$PWD:$PATH xmipp_python external/scons/scons.py mode=configure -j {parallel}'
-               ' --config=force  profile=no fast=yes warn=no release=yes gtest=no cuda=no debug=no matlab=no'
-               ' java=no LINKERFORPROGRAMS="$CXX" MPI_BINDIR="$EBROOTIMPI" JAVA_HOME="$JAVA_HOME" JAVAC=javac CC="$CC"'
-               ' CXXFLAGS="$CXXFLAGS -DMPICH_IGNORE_CXX_SEEK -I$EBROOTPYTHON/include/python{short_python_ver}"'
-               ' CXX="$CXX" MPI_CC="$MPICC" MPI_CXX="$MPICXX" MPI_INCLUDE="$MPI_INC_DIR" MPI_LIBDIR="$MPI_LIB_DIR"'
-               ' MPI_LINKERFORPROGRAMS="$MPICC" LIBPATH="$LD_LIBRARY_PATH" {configopts}').format(
-                   parallel=self.cfg['parallel'], short_python_ver=self.python_short_ver,
-                   preconfigopts=self.cfg['preconfigopts'], configopts=self.cfg['configopts']
-                   )
+    def configure_step(self):
+        """Configure xmipp build via a provided wrapper around sconss."""
+        # check if all our dependencies are in place
+        python_root = get_software_root('Python')
+        if not python_root:
+            self.log.error("Python not loaded as a dependency, which is required for %s" % self.name)
+        python_libdir = det_pylibdir()
+        self.python_short_ver = ".".join(get_software_version('Python').split(".")[0:2])
+        java_root = get_software_root('Python')
+        if not java_root:
+            self.log.error("Java not loaded as a dependency, which is required for %s" % self.name)
+
+        # build step expects these to exist
+        mkdir(os.path.join(self.cfg['start_dir'], 'bin'))
+        mkdir(os.path.join(self.cfg['start_dir'], 'lib'))
+
+        cmd = ' '.join([
+            '{preconfigopts}',
+            'python external/scons/scons.py mode=configure -j {parallel}',
+            '--config=force',
+            'profile=no',
+            'fast=yes',
+            'warn=no',
+            'release=yes',
+            'gtest=no',
+            'cuda=no',
+            'debug=no',
+            'matlab=no',
+            'java=no',
+            'LINKERFORPROGRAMS="$CXX"',
+            'MPI_BINDIR="{mpi_bindir}"',
+            'JAVA_HOME="{java_home}"',
+            'JAVAC=javac',
+            'CC="$CC"',
+            'CXXFLAGS="$CXXFLAGS -DMPICH_IGNORE_CXX_SEEK -I$EBROOTPYTHON/include/python{short_python_ver}" \
+                      "-I{python_libdir}/numpy/core/include/"',
+            'CXX="$CXX"',
+            'MPI_CC="$MPICC"',
+            'MPI_CXX="$MPICXX"',
+            'MPI_INCLUDE="$MPI_INC_DIR"',
+            'MPI_LIBDIR="$MPI_LIB_DIR"',
+            'MPI_LINKERFORPROGRAMS="$MPICC"',
+            'LIBPATH="$LD_LIBRARY_PATH"',
+            '{configopts}',
+        ])
+        cmd.format(
+            parallel=self.cfg['parallel'],
+            short_python_ver=self.python_short_ver,
+            python_libdir=python_libdir,
+            preconfigopts=self.cfg['preconfigopts'],
+            configopts=self.cfg['configopts'],
+            mpi_bindir=os.path.join(get_software_root(self.toolchain.MPI_MODULE_NAME[0]), 'bin'),
+            java_home=java_root
+        )
         run_cmd(cmd, log_all=True, simple=True)
 
     def build_step(self):
         """Custom build procedure for Xmipp: call the scons wrapper with compile argument"""
-        cmd = 'LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/lib PATH=$PWD:$PATH xmipp_python external/scons/scons.py' \
-              'mode=compile -j {parallel}'.format(parallel=self.cfg['parallel'])
-
+        cmd = ' '.join([
+            'LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/lib',
+            'python external/scons/scons.py',
+            'mode=compile -j {parallel}',
+        ])
+        cmd.format(parallel=self.cfg['parallel'])
         run_cmd(cmd, log_all=True, simple=True)
 
     def install_step(self):
         """install step for xmipp, this builds a local database and seems to do some tests?"""
-        cmd = 'XMIPP_HOME=$PWD PATH=$PWD:$PWD/bin:$PATH PYTHONPATH=$PYTHONPATH:$PWD/protocols:' \
-              '$PWD/libraries/bindings/python/:$EBROOTPYTHON/lib/python{short_python_ver}/lib-dynload/' \
-              ' python setup.py install'.format(self.python_short_ver)
+        cmd = ' '.join([
+            'XMIPP_HOME=$PWD',
+            'PATH=$PWD/bin:$PATH',
+            'PYTHONPATH="$PYTHONPATH:$PWD/protocols:$PWD/libraries/bindings/python/:' \
+            '$EBROOTPYTHON/lib/python{short_python_ver}/lib-dynload/"',
+            'python setup.py install'
+        ])
+        cmd.format(short_python_ver=self.python_short_ver)
         run_cmd(cmd, log_all=True, simple=True)
 
     def sanity_check_step(self):
-        """Custom sanity check for GAMESS-US."""
+        """Custom sanity check for Xmipp."""
         custom_paths = {
             'files': ['xmipp_%s' % x for x in ['imagej', 'mpi_run', 'phantom_create', 'tomo_project', 'volume_align']],
-            'dirs': ['lib/'],
+            'dirs': ['lib'],
         }
-        super(self.__class__, self).sanity_check_step(custom_paths=custom_paths)
+        super(EB_Xmipp, self).sanity_check_step(custom_paths=custom_paths)
 
     def make_module_extra(self):
-        """Define GAMESS-US specific variables in generated module file, i.e. $GAMESSUSROOT."""
-        txt = super(self.__class__, self).make_module_extra()
+        """Define Xmipp specific variables in generated module file, i.e. XMIPP_HOME."""
+        txt = super(EB_Xmipp, self).make_module_extra()
         txt += self.module_generator.set_environment('XMIPP_HOME', '$root')
         return txt
