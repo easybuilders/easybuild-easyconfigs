50a935f00e8b2f3100ca5497dbce64d6443945be
Jens Timmerman <jens.timmerman@gmail.com>
2015-03-18 16:11:32 +0100

    use self.installdir instead of cfg.install_dir

--- easybuild/easyblocks/x/xmipp.py
+++ easybuild/easyblocks/x/xmipp.py
@@ -157,5 +157,5 @@ class EB_Xmipp(EasyBlock):
     def make_module_extra(self):
         """Define Xmipp specific variables in generated module file, i.e. XMIPP_HOME."""
         txt = super(EB_Xmipp, self).make_module_extra()
-        txt += self.module_generator.set_environment('XMIPP_HOME', self.cfg['install_dir'])
+        txt += self.module_generator.set_environment('XMIPP_HOME', self.installdir)
         return txt
