954692f8dd452daeebf87ed940e7c795c788e434
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-03-24 11:18:50 +0100

    fix style issues

--- easybuild/easyblocks/x/xmipp.py
+++ easybuild/easyblocks/x/xmipp.py
@@ -47,20 +47,20 @@ class EB_Xmipp(EasyBlock):
         self.build_in_installdir = True
 
     def extract_step(self):
-        """Extract sources."""
+        """Extract Xmipp sources."""
         # strip off 'xmipp' part to avoid having everything in a 'xmipp' subdirectory
-        if not self.cfg['unpack_options']:
-            self.cfg['unpack_options'] = '--strip-components=1'
+        self.cfg.update('unpack_options', '--strip-components=1')
         super(EB_Xmipp, self).extract_step()
 
     def configure_step(self):
-        """Configure xmipp build via a provided wrapper around scons."""
+        """Configure Xmipp build via a provided wrapper around scons."""
         # check if all our dependencies are in place
         self.python_root = get_software_root('Python')
         if not self.python_root:
             self.log.error("Python not loaded as a dependency, which is required for %s" % self.name)
         python_libdir = det_pylibdir()
-        self.python_short_ver = ".".join(get_software_version('Python').split(".")[0:2])
+        self.python_short_ver = '.'.join(get_software_version('Python').split('.')[:2])
+
         java_root = get_software_root('Java')
         if not java_root:
             self.log.error("Java not loaded as a dependency, which is required for %s" % self.name)
@@ -74,18 +74,28 @@ class EB_Xmipp(EasyBlock):
         for src in ['bilib.tgz', 'bilib.tgz', 'condor.tgz', alglib_tar, 'scons.tgz']:
             extract_file(os.path.join(external_path, src), external_path)
 
-        # make sure we start in the start dir
+        # make sure we are back in the start dir
         os.chdir(self.cfg['start_dir'])
 
         # build step expects these to exist
         mkdir(os.path.join(self.cfg['start_dir'], 'bin'))
         mkdir(os.path.join(self.cfg['start_dir'], 'lib'))
 
+        python_inc_dir = os.path.join(self.python_root, 'include', 'python%s' % self.python_short_ver)
+        numpy_inc_dir = os.path.join(self.python_root, python_libdir, 'numpy', 'core', 'include')
+        mpi_bindir = os.path.join(get_software_root(self.toolchain.MPI_MODULE_NAME[0]), 'bin')
+
+        if not os.path.exists(numpy_inc_dir):
+            self.log.error("numpy 'include' directory %s not found" % numpy_inc_dir)
+
+        if not os.path.exists(mpi_bindir):
+            self.log.error("MPI 'bin' subdir %s does not exist" % mpi_bindir)
+
         cmd = ' '.join([
             self.cfg['preconfigopts'],
             'python external/scons/scons.py',
             'mode=configure',
-            '-j {parallel}',
+            '-j %s' % self.cfg['parallel'],
             '--config=force',
             'profile=no',
             'fast=yes',
@@ -97,11 +107,11 @@ class EB_Xmipp(EasyBlock):
             'matlab=no',
             'java=no',
             'LINKERFORPROGRAMS="$CXX"',
-            'MPI_BINDIR="{mpi_bindir}"',
-            'JAVA_HOME="{java_home}"',
+            'MPI_BINDIR=%s' % mpi_bindir,
+            'JAVA_HOME=%s' % java_root,
             'JAVAC=javac',
             'CC="$CC"',
-            'CXXFLAGS="$CXXFLAGS -DMPICH_IGNORE_CXX_SEEK -I{python_include_dir} -I{numpy_include_dir}"',
+            'CXXFLAGS="$CXXFLAGS -DMPICH_IGNORE_CXX_SEEK -I%s -I%s"' % (python_inc_dir, numpy_inc_dir),
             'CXX="$CXX"',
             'MPI_CC="$MPICC"',
             'MPI_CXX="$MPICXX"',
@@ -110,39 +120,40 @@ class EB_Xmipp(EasyBlock):
             'MPI_LINKERFORPROGRAMS="$MPICC"',
             'LIBPATH="$LD_LIBRARY_PATH"',
             self.cfg['configopts'],
-        ]).format(
-            parallel=self.cfg['parallel'],
-            python_libdir=python_libdir,
-            python_include_dir=os.path.join(self.python_root, 'include', 'python%s' % self.python_short_ver),
-            numpy_include_dir=os.path.join(self.python_root, python_libdir, 'numpy', 'core', 'include'),
-            mpi_bindir=os.path.join(get_software_root(self.toolchain.MPI_MODULE_NAME[0]), 'bin'),
-            java_home=java_root
-        )
+        ])
         run_cmd(cmd, log_all=True, simple=True)
 
     def build_step(self):
         """Custom build procedure for Xmipp: call the scons wrapper with compile argument"""
         cmd = ' '.join([
+            self.cfg['prebuildopts'],
             'LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/lib',
             'python external/scons/scons.py',
             'mode=compile',
             '-j %s' % self.cfg['parallel'],
+            self.cfg['buildopts'],
         ])
         run_cmd(cmd, log_all=True, simple=True)
 
     def install_step(self):
-        """install step for xmipp, this builds a local database and seems to do some tests?"""
+        """install step for Xmipp, this builds a local database and seems to do some tests?"""
         python_dynlib_dir = os.path.join(self.python_root, 'lib', 'python%s' % self.python_short_ver, 'lib-dynload')
+
+        if not os.path.exists(python_dynlib_dir):
+            self.log.error("Python lib-dynload dir %s not found" % python_dynlib_dir)
+
         extra_pythonpaths = [
             os.path.join(self.cfg['start_dir'], 'protocols'),
             os.path.join(self.cfg['start_dir'], 'libraries', 'bindings', 'python'),
             python_dynlib_dir,
         ]
         cmd = ' '.join([
+            self.cfg['preinstallopts'],
             'XMIPP_HOME=%s' % self.cfg['start_dir'],
             'PATH=%s:$PATH' % os.path.join(self.cfg['start_dir'], 'bin'),
             'PYTHONPATH="%s"' % os.pathsep.join(['$PYTHONPATH'] + extra_pythonpaths),
             'python setup.py install',
+            self.cfg['installopts'],
         ])
         run_cmd(cmd, log_all=True, simple=True)
 
