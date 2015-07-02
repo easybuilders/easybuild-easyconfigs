8f630e8f6938be8fba7cb5955f4ba813e9b7344d
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-07-02 09:38:39 +0200

    disable expand_relpaths for license file env var prepend-path statement in module file

--- easybuild/easyblocks/generic/intelbase.py
+++ easybuild/easyblocks/generic/intelbase.py
@@ -349,7 +349,8 @@ class IntelBase(EasyBlock):
         """Custom variable definitions in module file."""
         txt = super(IntelBase, self).make_module_extra()
 
-        txt += self.module_generator.prepend_paths(self.license_env_var, [self.license_file], allow_abs=True)
+        txt += self.module_generator.prepend_paths(self.license_env_var, [self.license_file],
+                                                   allow_abs=True, expand_relpaths=False)
 
         if self.cfg['m32']:
             nlspath = os.path.join('idb', '32', 'locale', '%l_%t', '%N')
