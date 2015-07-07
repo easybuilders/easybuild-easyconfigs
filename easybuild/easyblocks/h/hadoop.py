5d5fd758bb0eba227fcc83d39ca646d6de19368b
Ewan Higgs <ewan.higgs@ugent.be>
2015-07-07 16:35:40 +0200

    Hoist lib_src out of loop as per @boegel's comment.
    Well spotted.

--- easybuild/easyblocks/h/hadoop.py
+++ easybuild/easyblocks/h/hadoop.py
@@ -105,8 +105,8 @@ class EB_Hadoop(Tarball):
 
         not_found = []
         installdir = os.path.realpath(self.installdir)
+        lib_src = os.path.join([installdir, 'lib', 'native'])
         for native_lib, lib_path  in self.cfg['extra_native_libs']:
-            lib_src = os.path.join([installdir, 'lib', 'native'])
             if not re.search(r'%s: *true *%s' % (native_lib, lib_src), out):
                 not_found.append(native_lib)
         if not_found:
