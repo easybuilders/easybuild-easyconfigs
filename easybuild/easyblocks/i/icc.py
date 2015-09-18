15b48de51e8e6f582877d14b8755fb0e4af56ee4
Fokko Masselink <fokko.masselink@shell.com>
2015-09-18 12:07:45 +0200

    added support for Intel 2016 compilers

--- easybuild/easyblocks/i/icc.py
+++ easybuild/easyblocks/i/icc.py
@@ -74,21 +74,28 @@ class EB_icc(IntelBase):
                 'license_file_name': LICENSE_FILE_NAME_2012,
             }
 
-        super(EB_icc, self).install_step(silent_cfg_names_map=silent_cfg_names_map)
+        if LooseVersion(self.version) >= LooseVersion('2016'):
+            cfg_extras_map = {
+                'COMPONENTS': 'ALL',
+            }
+        super(EB_icc, self).install_step(silent_cfg_names_map=silent_cfg_names_map, silent_cfg_extras=cfg_extras_map)
 
     def sanity_check_step(self):
         """Custom sanity check paths for icc."""
 
         binprefix = "bin/intel64"
-        libprefix = "lib/intel64/lib"
+        libprefix = "lib/intel64"
         if LooseVersion(self.version) >= LooseVersion("2011"):
             if LooseVersion(self.version) <= LooseVersion("2011.3.174"):
                 binprefix = "bin"
-            elif LooseVersion(self.version) >= LooseVersion("2013_sp1"):
+            elif LooseVersion(self.version) >= LooseVersion("2013_sp1") and LooseVersion(self.version) < LooseVersion("2016"):
                 binprefix = "bin"
-                libprefix = "lib/intel64/lib"
+                libprefix = "lib/intel64"
+            elif LooseVersion(self.version) >= LooseVersion("2016"):
+                binprefix = "bin"
+                libprefix = "lib/intel64_lin"
             else:
-                libprefix = "compiler/lib/intel64/lib"
+                libprefix = "compiler/lib/intel64"
 
         binfiles = ["icc", "icpc"]
         if LooseVersion(self.version) < LooseVersion("2014"):
@@ -96,7 +103,7 @@ class EB_icc(IntelBase):
 
         custom_paths = {
             'files': ["%s/%s" % (binprefix, x) for x in binfiles] +
-                     ["%s%s" % (libprefix, x) for x in ["iomp5.a", "iomp5.so"]],
+                     ["%s/lib%s" % (libprefix, x) for x in ["iomp5.a", "iomp5.so"]],
             'dirs': [],
         }
 
@@ -105,53 +112,82 @@ class EB_icc(IntelBase):
     def make_module_req_guess(self):
         """Customize paths to check and add in environment.
         """
+        # New Directory Layout for Intel Parallel Studio XE 2016
+        # https://software.intel.com/en-us/articles/new-directory-layout-for-intel-parallel-studio-xe-2016
+        debuggerpath = 'debugger_%s' % self.version.split('.')[0]
         if self.cfg['m32']:
             # 32-bit toolchain
+            libpaths = ['lib', 'lib/ia32'],
             dirmap = {
                 'PATH': ['bin', 'bin/ia32', 'tbb/bin/ia32'],
-                'LD_LIBRARY_PATH': ['lib', 'lib/ia32'],
-                'LIBRARY_PATH': ['lib', 'lib/ia32'],
+                'LD_LIBRARY_PATH': libpaths,
+                'LIBRARY_PATH': libpaths,
                 'MANPATH': ['man', 'share/man', 'man/en_US'],
                 'IDB_HOME': ['bin/intel64']
             }
         else:
-            # 64-bit toolit
-            dirmap = {
-                'PATH': ['bin', 'bin/intel64', 'tbb/bin/emt64'],
-                'LD_LIBRARY_PATH': ['lib', 'lib/intel64'],
-                'LIBRARY_PATH': ['lib', 'lib/intel64'],
-                'MANPATH': ['man', 'share/man', 'man/en_US'],
-                'IDB_HOME': ['bin/intel64']
-            }
+            if LooseVersion(self.version) < LooseVersion("2016"):
+                # 64-bit toolkit
+                libpaths = ['compiler/lib/intel64', 'lib/intel64', 'debugger/ipt/intel64/lib', 'ipp/lib/intel64', 'tbb/lib/intel64']
+                dirmap = {
+                    'PATH': ['bin/intel64', 'tbb/bin/intel64', 'ipp/bin/intel64', 'debugger/gdb/intel64/bin'],
+                    'LD_LIBRARY_PATH': libpaths, 
+                    'LIBRARY_PATH': libpaths,
+                    'MANPATH': ['man', 'share/man', 'man/en_US', 'debugger/gdb/intel64/share/man'],
+                    'CPATH': ['ipp/include', 'tbb/include'],
+                    'INTEL_PYTHONHOME': ['debugger/python/intel64']
+                }
+            else:
+                # 64-bit toolkit
+                libpaths = ['daal/../compiler/lib/intel64_lin', 'daal/../tbb/lib/intel64_lin/gcc4.4', 'daal/lib/intel64_lin', '%s/libipt/intel64/lib' % debuggerpath,'tbb/lib/intel64/gcc4.4', 'mkl/lib/intel64', 'ipp/lib/intel64', 'ipp/../compiler/lib/intel64', 'compiler/lib/intel64']
+                dirmap = {
+                    'PATH': ['mpi/intel64/bin', 'ipp/bin/intel64', '%s/gdb/intel64/bin' % debuggerpath, 'bin/intel64'],
+                    'LD_LIBRARY_PATH': libpaths,
+                    'LIBRARY_PATH': libpaths,
+                    'MANPATH': ['man/common', 'man/en_US', 'debugger/gdb/intel64/share/man'],
+                    'CPATH': ['daal/include', 'tbb/include', 'mkl/include', 'ipp/include'],
+                    'INTEL_PYTHONHOME': ['%s/python/intel64' % debuggerpath],
+                    'DAALROOT': ['daal'],
+#                    'MKLROOT': ['mkl'],
+                    'TBBROOT': ['tbb'],
+                    'IPPROOT': ['ipp'],
+                    'CLASSPATH': ['daal/lib/daal.jar'],
+                }
+
 
         # in recent Intel compiler distributions, the actual binaries are
         # in deeper directories, and symlinked in top-level directories
         # however, not all binaries are symlinked (e.g. mcpcom is not)
-        if os.path.isdir("%s/composerxe-%s" % (self.installdir, self.version)):
-            prefix = "composerxe-%s" % self.version
+        # more recent versions of the Intel Compiler (2013.sp1 and newer)
+        if os.path.isdir("%s/composer_xe_%s" % (self.installdir, self.version)):
+            prefix = "composer_xe_%s" % self.version
             oldmap = dirmap
             dirmap = {}
             for k, vs in oldmap.items():
                 dirmap[k] = []
-                if k == "LD_LIBRARY_PATH":
-                    prefix = "composerxe-%s/compiler" % self.version
-                else:
-                    prefix = "composerxe-%s" % self.version
+                prefix = "composer_xe_%s" % self.version
                 for v in vs:
                     v2 = "%s/%s" % (prefix, v)
-                    dirmap[k].append(v2)
+                    if os.path.isdir("%s/%s" % (self.installdir, v2)):
+                        dirmap[k].append(v2)
 
-        elif os.path.isdir("%s/compiler" % (self.installdir)):
-            prefix = "compiler"
+        if os.path.isdir("%s/compilers_and_libraries_%s/linux" % (self.installdir, self.version)):
+            prefix = "compilers_and_libraries_%s/linux" % self.version
             oldmap = dirmap
             dirmap = {}
             for k, vs in oldmap.items():
                 dirmap[k] = []
-                prefix = ''
-                if k == "LD_LIBRARY_PATH":
-                    prefix = "compiler/"
                 for v in vs:
-                    v2 = "%s%s" % (prefix, v)
-                    dirmap[k].append(v2)
+                    v2 = "%s/%s" % (prefix, v)
+                    if os.path.exists("%s/%s" % (self.installdir, v2)):
+                        dirmap[k].append(v2)
 
         return dirmap
+
+    def make_module_extra(self):
+        """Add extra environment variables for icc, for license file and NLS path."""
+        txt = super(EB_icc, self).make_module_extra()
+        txt += self.module_generator.prepend_paths(self.license_env_var, self.cfg['license_file'], allow_abs=True)
+        txt += self.module_generator.prepend_paths('NLSPATH', '$root/idb/intel64/locale/%l_%t/%N')
+        return txt
+
