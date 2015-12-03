f07431be0a06cb04b33e092fef6c4efb9894a20f
Lumír Jasiok <lumir.jasiok@alfawolf.eu>
2015-12-03 16:07:47 +0100

    Add ipp.py with 2016 support, written from the scratch

--- easybuild/easyblocks/i/ipp.py
+++ easybuild/easyblocks/i/ipp.py
@@ -1,77 +1,82 @@
-##
-# Copyright 2009-2015 Ghent University
+#!/usr/bin/env python
 #
-# This file is part of EasyBuild,
-# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
-# with support of Ghent University (http://ugent.be/hpc),
-# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
-# the Hercules foundation (http://www.herculesstichting.be/in_English)
-# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#  Copyright (C) 2015 IT4Innovations
+#  Lumir Jasiok
+#  lumir.jasiok@vsb.cz
+#  http://www.it4i.cz
 #
-# http://github.com/hpcugent/easybuild
 #
-# EasyBuild is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation v2.
 #
-# EasyBuild is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
-##
+
 """
-EasyBuild support for installing the Intel Performance Primitives (IPP) library, implemented as an easyblock
-
-@author: Stijn De Weirdt (Ghent University)
-@author: Dries Verdegem (Ghent University)
-@author: Kenneth Hoste (Ghent University)
-@author: Pieter De Baets (Ghent University)
-@author: Jens Timmerman (Ghent University)
+EasyBuild support for building and installing Intel Performance Primitives (IPP) library, implemented as an easyblock. 
+@author: Lumir Jasiok (IT4Innovations)
 """
-from distutils.version import LooseVersion
 
-from easybuild.easyblocks.generic.intelbase import IntelBase, ACTIVATION_NAME_2012, LICENSE_FILE_NAME_2012
+import os
+import platform
 
+from easybuild.easyblocks.generic.intelbase import IntelBase
+from easybuild.tools.run import run_cmd
 
 class EB_ipp(IntelBase):
     """
-    Support for installing Intel Integrated Performance Primitives library
+    Support for installing Intel Performance Primitives
     """
+
     def install_step(self):
+        """Actual installation
+        
+        - create silent.cfg
+        - run install.sh
         """
-        Actual installation
-        - create silent cfg file
-        - execute command
-        """
-        silent_cfg_names_map = None
-
-        if LooseVersion(self.version) < LooseVersion('8.0'):
-            silent_cfg_names_map = {
-                'activation_name': ACTIVATION_NAME_2012,
-                'license_file_name': LICENSE_FILE_NAME_2012,
-            }
-
-        super(EB_ipp, self).install_step(silent_cfg_names_map=silent_cfg_names_map)
-
-    def sanity_check_step(self):
-        """Custom sanity check paths for IPP."""
-
-        if LooseVersion(self.version) < LooseVersion('8.0'):
-            dirs = ["compiler/lib/intel64", "ipp/bin", "ipp/include",
-                    "ipp/interfaces/data-compression", "ipp/tools/intel64"]
+        
+        machine = platform.machine()
+        if machine == "i386":
+            self.arch = "ia32"
         else:
-            dirs = ["composerxe/lib/intel64", "ipp/bin", "ipp/include",
-                    "ipp/tools/intel64"]
+            self.arch = "intel64"
 
-        custom_paths = {
-                        'files': ["ipp/lib/intel64/libipp%s" % y
-                                   for x in ["ac", "cc", "ch", "core", "cv", "dc", "di",
-                                             "i", "j", "m", "r", "s", "sc", "vc", "vm"]
-                                   for y in ["%s.a" % x, "%s.so" % x]],
-                        'dirs': dirs
-                       }
+        silent_cfg = """
+ACCEPT_EULA=accept
+CONTINUE_WITH_OPTIONAL_ERROR=yes
+PSET_INSTALL_DIR=%s
+CONTINUE_WITH_INSTALLDIR_OVERWRITE=yes
+COMPONENTS=ALL
+PSET_MODE=install
+ACTIVATION_LICENSE_FILE=%s
+ACTIVATION_TYPE=license_file
+PHONEHOME_SEND_USAGE_DATA=no
+SIGNING_ENABLED=yes
+ARCH_SELECTED=%s
+        """ % (self.installdir, self.license_file, self.arch.upper())
+        build_dir = self.cfg['start_dir']
+        silent_file = os.path.join(build_dir, 'silent.cfg')
+        fd = open(silent_file, 'w')
+        fd.write(silent_cfg)
+        fd.close()
 
-        super(EB_ipp, self).sanity_check_step(custom_paths=custom_paths)
+        os.chdir(build_dir)
+        self.log.info("Build dir is %s" % (build_dir))
+        cmd = "./install.sh -s silent.cfg --SHARED_INSTALL"
+        run_cmd(cmd, log_all=True, simple=True)
+        return True
+    
+    def make_module_req_guess(self):
+        """
+        A dictionary of possible directories to look for
+        """
+        guesses = super(EB_ipp, self).make_module_req_guess()
+
+        lib_path = 'lib/%s' % self.arch 
+        lib_arr = []
+        lib_arr.append(lib_path)
+
+        guesses.update({
+            'LD_LIBRARY_PATH': lib_arr,
+            'LIBRARY_PATH': lib_arr,
+            'CPATH': ['ipp/include'],
+            'INCLUDE': ['ipp/include'],
+        })
+
+        return guesses
