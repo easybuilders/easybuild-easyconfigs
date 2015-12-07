6a708bea07fc0a655d290e6ea27f1732c637cbda
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-12-07 10:46:20 +0100

    fix docstring in EB_SuiteSparse.make_module_req_guess

--- easybuild/easyblocks/s/suitesparse.py
+++ easybuild/easyblocks/s/suitesparse.py
@@ -160,8 +160,11 @@ class EB_SuiteSparse(ConfigureMake):
                     raise EasyBuildError("Failed to make symbolic link from %s to %s: %s", src, dst, err)
 
     def make_module_req_guess(self):
-        """Add config dir to CPATH so include file is found."""
-        """Add UMFPACK adn AMD library dirs needed for scikit-umfpack."""
+        """
+        Extra path to consider for module file:
+        * add config dir to $CPATH so include files are found
+        * add UMFPACK adn AMD library dirs to $LD_LIBRARY_PATH
+        """
         guesses = super(EB_SuiteSparse, self).make_module_req_guess()
         guesses.update({
             'CPATH': [self.config_name],
