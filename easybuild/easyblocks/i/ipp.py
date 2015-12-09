33589c06c173d3a23e4fffc6e279503ca3079008
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-12-09 21:35:39 +0100

    style/bug fixes in ipp.py

--- easybuild/easyblocks/i/ipp.py
+++ easybuild/easyblocks/i/ipp.py
@@ -33,13 +33,13 @@ EasyBuild support for installing the Intel Performance Primitives (IPP) library,
 @author: Lumir Jasiok (IT4Innovations)
 """
 
-import os
-import platform
-
-
 from distutils.version import LooseVersion
+import os
+
 from easybuild.easyblocks.generic.intelbase import INSTALL_MODE_NAME_2015, INSTALL_MODE_2015
 from easybuild.easyblocks.generic.intelbase import IntelBase, ACTIVATION_NAME_2012, LICENSE_FILE_NAME_2012
+from easybuild.tools.build_log import EasyBuildError
+from easybuild.tools.systemtools import get_platform_name
 
 
 class EB_ipp(IntelBase):
@@ -53,13 +53,16 @@ class EB_ipp(IntelBase):
         - execute command
         """
 
-        machine = platform.machine()
-        if machine == "i386":
-            self.arch = "ia32"
-        else:
+        platform_name = get_platform_name()
+        if platform_name.startswith('x86_64'):
             self.arch = "intel64"
-        
+        elif platform_name.startswith('i386'):
+            self.arch = 'ia32'
+        else:
+            raise EasyBuildError("Failed to determine system architecture based on %s", platform_name)
+
         silent_cfg_names_map = None
+        silent_cfg_extras = None
 
         if LooseVersion(self.version) < LooseVersion('8.0'):
             silent_cfg_names_map = {
@@ -67,13 +70,8 @@ class EB_ipp(IntelBase):
                 'license_file_name': LICENSE_FILE_NAME_2012,
             }
 
-        if LooseVersion(self.version) < LooseVersion('9.0'):
-            silent_cfg_names_map = {
-                'install_mode_name': INSTALL_MODE_NAME_2015,
-                'install_mode': INSTALL_MODE_2015,
-            }
-        # In case of IPP 2016 we have to specify ARCH_SELECTED in silent.cfg
-        if LooseVersion(self.version) > LooseVersion('9.0'):
+        # in case of IPP 9.x, we have to specify ARCH_SELECTED in silent.cfg
+        if LooseVersion(self.version) >= LooseVersion('9.0'):
             silent_cfg_extras = {
                 'ARCH_SELECTED': self.arch.upper()
             }
@@ -84,30 +82,22 @@ class EB_ipp(IntelBase):
         """Custom sanity check paths for IPP."""
 
         if LooseVersion(self.version) < LooseVersion('8.0'):
-            dirs = ["compiler/lib/intel64", "ipp/bin", "ipp/include",
-                    "ipp/interfaces/data-compression", "ipp/tools/intel64"]
-        elif LooseVersion(self.version) > LooseVersion('9.0'):
-            dirs = ["ipp/bin", "ipp/include", "ipp/tools/intel64"]
+            dirs = ['compiler/lib/intel64', 'ipp/bin', 'ipp/include',
+                    'ipp/interfaces/data-compression', 'ipp/tools/intel64']
+        elif LooseVersion(self.version) >= LooseVersion('9.0'):
+            dirs = ['ipp/bin', 'ipp/include', 'ipp/tools/intel64']
         else:
-            dirs = ["composerxe/lib/intel64", "ipp/bin", "ipp/include",
-                    "ipp/tools/intel64"]
-        
-        if LooseVersion(self.version) >= LooseVersion('9.0'):
-            custom_paths = {
-                'files': ["ipp/lib/intel64/libipp%s" % y
-                    for x in ["cc", "ch", "core", "cv", "dc", "i", "s", "vm"]
-                    for y in ["%s.a" % x, "%s.so" % x]],
-                'dirs': dirs
-            }
- 
-        else:
-            custom_paths = {
-                'files': ["ipp/lib/intel64/libipp%s" % y
-                    for x in ["ac", "cc", "ch", "core", "cv", "dc", "di",
-                              "i", "j", "m", "r", "s", "sc", "vc", "vm"]
-                    for y in ["%s.a" % x, "%s.so" % x]],
-                'dirs': dirs
-            }
+            dirs = ['composerxe/lib/intel64', 'ipp/bin', 'ipp/include',
+                    'ipp/tools/intel64']
+
+        ipp_libs = ['cc', 'ch', 'core', 'cv', 'dc', 'i', 's', 'vm']
+        if LooseVersion(self.version) < LooseVersion('9.0'):
+            ipp_libs.extend(['ac', 'di', 'j', 'm', 'r', 'sc', 'vc'])
+
+        custom_paths = {
+            'files': ['ipp/lib/intel64/libipp%s' % y for x in ipp_libs for y in ['%s.a' % x, '%s.so' % x]],
+            'dirs': dirs,
+        }
 
         super(EB_ipp, self).sanity_check_step(custom_paths=custom_paths)
 
@@ -117,10 +107,10 @@ class EB_ipp(IntelBase):
         """
         guesses = super(EB_ipp, self).make_module_req_guess()
 
-        if LooseVersion(self.version) > LooseVersion('9.0'):
+        if LooseVersion(self.version) >= LooseVersion('9.0'):
             lib_path = os.path.join('lib', self.arch)
             include_path = 'ipp/include'
- 
+
             guesses.update({
                 'LD_LIBRARY_PATH': [lib_path],
                 'LIBRARY_PATH': [lib_path],
