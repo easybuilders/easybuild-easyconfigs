2026a7e09496803c6ae8cb500a8a2264f39de641
Lumír Jasiok <lumir.jasiok@alfawolf.eu>
2015-12-12 09:26:34 +0100

    Fix minor style issues, add copyright

--- easybuild/easyblocks/a/advisor.py
+++ easybuild/easyblocks/a/advisor.py
@@ -1,3 +1,27 @@
+##
+# Copyright 2009-2015 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
 """
 EasyBuild support for installing the Intel Advisor XE, implemented as an easyblock
 
@@ -15,7 +39,7 @@ class EB_Advisor(IntelBase):
         """Custom sanity check paths for Advisor"""
 
         custom_paths = {
-            'files': ["advisor_xe/lib64/libadvixe_runtool_6.6.so", "advisor_xe/lib64/libadvixe_dvt_core_6.5.so"],
+            'files': [],
             'dirs': ['advisor_xe/bin64', 'advisor_xe/lib64']
         }
 
@@ -31,10 +55,10 @@ class EB_Advisor(IntelBase):
         include_path = 'advisor_xe/include'
  
         guesses.update({
-            'LD_LIBRARY_PATH': [lib_path],
-            'LIBRARY_PATH': [lib_path],
             'CPATH': [include_path],
             'INCLUDE': [include_path],
+            'LD_LIBRARY_PATH': [lib_path],
+            'LIBRARY_PATH': [lib_path],
             'PATH': ['advisor_xe/bin64'],
         })
 
