5126beae8c2aec64c30ec6a82599f0798ffcf93d
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-12-12 20:34:51 +0100

    improved implementation of determining value for $GXX_ROOT

--- easybuild/easyblocks/generic/intelbase.py
+++ easybuild/easyblocks/generic/intelbase.py
@@ -372,8 +372,14 @@ class IntelBase(EasyBlock):
         # In case people are using unusual locales need to set GXX_ROOT, see
         # https://software.intel.com/en-us/articles/
         # intel-fortran-compiler-for-linux-ifort-error-could-not-find-directory-in-which-g-resides
-        out, _ = run_cmd("g++ --print-search-dirs", log_all=True, simple=False, force_in_dry_run=True)
-        gxxroot = out.split('\n')[0].split(':')[1].strip()
+        cmd = "g++ --print-search-dirs"
+        out, _ = run_cmd(cmd, log_all=True, simple=False, force_in_dry_run=True)
+        install_line_parts = out.strip().split('\n')[0].split(':')
+        if len(install_line_parts) == 2 and install_line_parts[0] == 'install':
+            gxxroot = install_line_parts[1].strip()
+        else:
+            raise EasyBuildError("Unexpected output from %s: %s", cmd, out)
+
         txt += self.module_generator.set_environment('GXX_ROOT', gxxroot)
 
         return txt
