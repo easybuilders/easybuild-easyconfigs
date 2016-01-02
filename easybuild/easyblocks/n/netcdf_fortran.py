cb78f8c9a106dc7c81ea78a4a46c15e967bfbf00
rpfische <robert.fischer@nasa.gov>
2016-01-02 00:40:50 -0500

    Another .so fix

--- easybuild/easyblocks/n/netcdf_fortran.py
+++ easybuild/easyblocks/n/netcdf_fortran.py
@@ -37,6 +37,7 @@ import os
 import easybuild.tools.environment as env
 import easybuild.tools.toolchain as toolchain
 from easybuild.easyblocks.generic.configuremake import ConfigureMake
+from easybuild.tools.systemtools import get_shared_lib_ext
 
 
 class EB_netCDF_minus_Fortran(ConfigureMake):
@@ -61,8 +62,10 @@ class EB_netCDF_minus_Fortran(ConfigureMake):
         Custom sanity check for netCDF-Fortran
         """
 
+        SHLIB_EXT = get_shared_lib_ext()
+
         custom_paths = {
-                        'files': ["bin/nf-config"] + ["lib/%s" % x for x in ["libnetcdff.so", "libnetcdff.a"]] +
+                        'files': ["bin/nf-config"] + ["lib/%s" % x for x in ["libnetcdff."+SHLIB_EXT, "libnetcdff.a"]] +
                                  ["include/%s" % x for x in ["netcdf.inc", "netcdf.mod", "typesizes.mod"]],
                         'dirs': []
                        }
