7c02f2fac00a8f34019778b33bed9a55eb4f3f99
HPC core facility CalcUA <calcua@uantwerpen.be>
2016-01-05 13:56:41 +0100

    Update CPLEX easyblock
    - added extra PATHs
    - added LD_LIBRARY_PATH
    - added CPLEXDIR (as used in the Makefiles)

--- easybuild/easyblocks/c/cplex.py
+++ easybuild/easyblocks/c/cplex.py
@@ -110,11 +110,21 @@ class EB_CPLEX(Binary):
             raise EasyBuildError("No bins found using %s in %s", binglob, self.installdir)
 
     def make_module_extra(self):
-        """Add installdir to path and set CPLEX_HOME"""
+        """Add bin dirs and lib dirs and set CPLEX_HOME and CPLEXDIR"""
+
+        os.chdir(self.installdir)
+        binglob = '*/bin/x86-64*'
+        bins = glob.glob(binglob)
+        libglob = '*/lib/x86-64*/*pic'
+        libs = glob.glob(libglob)
 
         txt = super(EB_CPLEX, self).make_module_extra()
-        txt += self.module_generator.prepend_paths("PATH", [self.bindir])
+
+        txt += self.module_generator.prepend_paths("PATH", [path for path in bins])
+        txt += self.module_generator.prepend_paths("LD_LIBRARY_PATH", reduce(lambda x, y: x+y, [bins,libs]))
+
         txt += self.module_generator.set_environment("CPLEX_HOME", os.path.join(self.installdir, 'cplex'))
+        txt += self.module_generator.set_environment("CPLEXDIR", os.path.join(self.installdir, 'cplex'))
         self.log.debug("make_module_extra added %s" % txt)
         return txt
 
