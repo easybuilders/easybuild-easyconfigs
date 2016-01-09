9770a5c0f622b681e82186d78866727270dcd019
Kenneth Hoste <kenneth.hoste@ugent.be>
2016-01-09 23:44:53 +0100

    fix remarks w.r.t. using glob in installdir

--- easybuild/easyblocks/c/cplex.py
+++ easybuild/easyblocks/c/cplex.py
@@ -111,18 +111,23 @@ class EB_CPLEX(Binary):
 
     def make_module_extra(self):
         """Add bin dirs and lib dirs and set CPLEX_HOME and CPLEXDIR"""
-
-        os.chdir(self.installdir)
-        bins = glob.glob(os.path.join('*', 'bin', 'x86-64*'))
-        libs = glob.glob(os.path.join('*', 'lib', 'x86-64*', '*pic'))
-
         txt = super(EB_CPLEX, self).make_module_extra()
 
-        txt += self.module_generator.prepend_paths("PATH", [path for path in bins])
-        txt += self.module_generator.prepend_paths("LD_LIBRARY_PATH", [path for path in bins+libs])
+        try:
+            cwd = os.getcwd()
+            os.chdir(self.installdir)
+            bins = glob.glob(os.path.join('*', 'bin', 'x86-64*'))
+            libs = glob.glob(os.path.join('*', 'lib', 'x86-64*', '*pic'))
+            os.chdir(cwd)
+        except OSError as err:
+            raise EasyBuildError("Failed to determine bin/lib subdirs: %s", err)
 
-        txt += self.module_generator.set_environment("CPLEX_HOME", os.path.join(self.installdir, 'cplex'))
-        txt += self.module_generator.set_environment("CPLEXDIR", os.path.join(self.installdir, 'cplex'))
+        txt += self.module_generator.prepend_paths('PATH', [path for path in bins])
+        txt += self.module_generator.prepend_paths('LD_LIBRARY_PATH', [path for path in bins+libs])
+
+        txt += self.module_generator.set_environment('CPLEX_HOME', os.path.join(self.installdir, 'cplex'))
+        txt += self.module_generator.set_environment('CPLEXDIR', os.path.join(self.installdir, 'cplex'))
+
         self.log.debug("make_module_extra added %s" % txt)
         return txt
 
