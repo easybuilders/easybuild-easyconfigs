75207f475b8f52fc13c9b5966b2be631315e3b2f
Kenneth Hoste <kenneth.hoste@ugent.be>
2016-01-21 10:16:04 +0100

    enhance CrayToolchain easyblock: properly handle missing PrgEnv dep + Tcl/Lua conditional swap

--- easybuild/easyblocks/generic/craytoolchain.py
+++ easybuild/easyblocks/generic/craytoolchain.py
@@ -26,10 +26,11 @@
 EasyBuild support for installing Cray toolchains, implemented as an easyblock
 
 @author: Kenneth Hoste (Ghent University)
-Tweaked by: @gppezzi
+@author: Guilherme Peretti Pezzi (CSCS)
 """
 
 from easybuild.easyblocks.generic.bundle import Bundle
+from easybuild.tools.build_log import EasyBuildError
 
 
 KNOWN_PRGENVS = ['PrgEnv-cray', 'PrgEnv-gnu', 'PrgEnv-intel', 'PrgEnv-pgi']
@@ -43,32 +44,52 @@ class CrayToolchain(Bundle):
         """
         Generate load/swap statements for dependencies in the module file
         """
+        prgenv_name, prgenv_mod = None, None
+        unload_info = {}
+
         # build dict with info for 'module swap' statements for dependencies,
         # i.e. a mapping of full module name to the module name to unload before loading
         # for example: {'fftw/3.3.4.1': 'fftw', 'cray-libsci/13.0.4': 'cray-libsci'}
-        unload_info = {}
         for dep in self.toolchain.dependencies:
             mod_name = dep['full_mod_name']
             # determine versionless module name, e.g. 'fftw/3.3.4.1' => 'fftw'
-            depname = '/'.join(mod_name.split('/')[:-1])
-            if not mod_name.startswith('PrgEnv-'):
-                unload_info.update({mod_name: depname})
+            dep_name = '/'.join(mod_name.split('/')[:-1])
+
+            if dep_name.startswith('PrgEnv-'):
+                prgenv_name = dep_name
+                prgenv_mod = mod_name
             else:
-                my_prgenv_short = depname
-                my_prgenv_full = dep['full_mod_name']
+                unload_info.update({mod_name: dep_name})
+
+        if prgenv_name is None:
+            raise EasyBuildError("Could not find a PrgEnv-* module listed as dependency: %s",
+                                 self.toolchain.dependencies)
 
         self.log.debug("Swap info for dependencies of %s: %s", self.full_mod_name, unload_info)
         txt = super(CrayToolchain, self).make_module_dep(unload_info=unload_info)
-        swap_prgenv =  "if { [ is-loaded " + my_prgenv_short + " ] } { \n"
-        swap_prgenv += "    module swap " + my_prgenv_short + " " + my_prgenv_full + "\n}\n"
+
+        # include conditional swap for PrgEnv module,
+        # to handle the case where another version the specified PrgEnv module is already loaded
+        if self.module_generator.SYNTAX == 'Tcl':
+            cond = "is-loaded %s" % prgenv_name
+            body = "module swap %s %s" % (prgenv_name, prgenv_mod)
+        elif self.module_generator.SYNTAX == 'Lua':
+            cond = 'isloaded("%s")' % prgenv_name
+            body = 'swap("%s", "%s")' % (prgenv_name, prgenv_mod)
+        else:
+            raise EasyBuildError("Unknown module syntax: %s", self.module_generator.SYNTAX)
+
+        swap_prgenv = self.module_generator.conditional_statement(cond, body)
 
         # unload statements for PrgEnv-* modules must be included *first*
         comment = self.module_generator.comment("first, unload any PrgEnv module that may be loaded").strip()
         prgenv_unloads = ['', comment]
         for prgenv in KNOWN_PRGENVS:
-            if prgenv not in my_prgenv_short:
+            if prgenv not in prgenv_name:
                 prgenv_unloads.append(self.module_generator.unload_module(prgenv).strip())
 
         comment = self.module_generator.comment("next, load toolchain components")
+
         txt = '\n'.join(prgenv_unloads) + '\n\n' + comment + swap_prgenv + txt
+
         return txt
