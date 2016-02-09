f126b460a0bffdaa2c40c35fc87dda13986b743d
Kenneth Hoste <kenneth.hoste@ugent.be>
2016-02-09 10:57:47 +0100

    copyright update for 2016

--- easybuild/easyblocks/generic/__init__.py
+++ easybuild/easyblocks/generic/__init__.py
@@ -1,10 +1,10 @@
 ##
-# Copyright 2009-2015 Ghent University
-# Copyright 2009-2015 Stijn De Weirdt
+# Copyright 2009-2016 Ghent University
+# Copyright 2009-2016 Stijn De Weirdt
 # Copyright 2010 Dries Verdegem
-# Copyright 2010-2015 Kenneth Hoste
+# Copyright 2010-2016 Kenneth Hoste
 # Copyright 2011 Pieter De Baets
-# Copyright 2011-2015 Jens Timmerman
+# Copyright 2011-2016 Jens Timmerman
 #
 # This file is part of EasyBuild,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
