fbf72833080abdc66e15988a207a9687f6760aaf
Kenneth Hoste <kenneth.hoste@ugent.be>
2016-02-11 17:18:23 +0100

    modify DL_POLY_Classic easyblock to support building with Plumed support (WIP)

--- easybuild/easyblocks/d/dl_poly_classic.py
+++ easybuild/easyblocks/d/dl_poly_classic.py
@@ -1,5 +1,5 @@
 ##
-# Copyright 2013 Ghent University
+# Copyright 2013-2016 Ghent University
 #
 # This file is part of EasyBuild,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
@@ -26,27 +26,80 @@
 EasyBuild support for DL_POLY Classic, implemented as an easyblock
 
 @author: Jens Timmerman (Ghent University)
+@author: Kenneth Hoste (Ghent University)
 """
+import glob
 import os
 import shutil
 
+from easybuild.tools.build_log import EasyBuildError
 from easybuild.tools.filetools import copytree
-from easybuild.easyblocks.generic.configuremake import ConfigureMake
+from easybuild.tools.run import run_cmd
+from easybuild.easyblocks.generic.makecp import MakeCp
 
 
-class EB_DL_underscore_POLY_underscore_Classic(ConfigureMake):
+class EB_DL_underscore_POLY_underscore_Classic(MakeCp):
     """Support for building and installing DL_POLY Classic."""
+
+    def __init__(self, *args, **kwargs):
+        """Easyblock constructor; initialize class variables."""
+        super(EB_DL_underscore_POLY_underscore_Classic, self).__init__(*args, **kwargs)
+
+        # check whether PLUMED is listed as a dependency
+        self.with_plumed = 'PLUMED' in [dep['name'] for dep in self.cfg['dependencies']]
+
+    def extract_step(self):
+        """Move 'source' to 'srcmod' directory if PLUMED is used as a dependency."""
+        super(EB_DL_underscore_POLY_underscore_Classic, self).extract_step()
+
+        if self.with_plumed and not self.dry_run:
+            try:
+                os.rename('source', 'srcmod')
+            except OSError as err:
+                raise EasyBuildError("Failed to move 'source' directory to 'srcmod': %s", err)
+
+    def patch_step(self):
+        """Generate PLUMED patch if PLUMED is listed as a dependency."""
+        diff_pat = 'dlpoly-*.diff'
+        try:
+            diff_hits = glob.glob(os.path.join(self.builddir, diff_pat))
+        except OSError as err:
+            raise EasyBuildError("Failed to find list of files/dirs that match '%s': %s", diff_pat, err)
+
+        if len(diff_hits) == 1:
+            plumed_patch = os.path.splitext(os.path.basename(diff_hits[0]))[0]
+        else:
+            raise EasyBuildError("Expected to find exactly one match for '%s', found: %s", diff_pat, diff_hits)
+
+        run_cmd("plumed-patch -p --runtime %s -d %s.diff" % (plumed_patch, os.path.join(self.builddir, plumed_patch)))
+
+        super(EB_DL_underscore_POLY_underscore_Classic, self).patch_step()
 
     def configure_step(self):
         """Copy the makefile to the source directory and use MPIF90 to do a parrallel build"""
-        shutil.copy("build/MakePAR", "source/Makefile")
-        os.chdir("source")
+
         self.cfg.update('buildopts', 'LD="$MPIF90 -o" FC="$MPIF90 -c" par')
+
+        if self.with_plumed:
+            source_dir = 'srcmod'
+        else:
+            source_dir = 'source'
+
+        try:
+            shutil.copy(os.path.join('build', 'MakePAR'), os.path.join(source_dir, 'Makefile'))
+            os.chdir(source_dir)
+
+        except OSError as err:
+            raise EasyBuildError("Failed to prepare configuration in %s: %s", source_dir, err)
+
+        self.cfg['with_configure'] = True
+
+        super(EB_DL_underscore_POLY_underscore_Classic, self).configure_step()
 
     def install_step(self):
         """Copy the executables to the installation directory"""
         self.log.debug("copying %s/execute to %s, (from %s)", self.cfg['start_dir'], self.installdir, os.getcwd())
-        # create a /bin, this way we also get the PATH to be set correctly automatically
-        bin_path = os.path.join(self.installdir, "bin")
+        # create a 'bin' subdir, this way we also get $PATH to be set correctly automatically
         install_path = os.path.join(self.cfg['start_dir'], 'execute')
+        bin_path = os.path.join(self.installdir, 'bin')
         copytree(install_path, bin_path)
