2b823d3c456d931e32e306f20d59c2f669e4d0c8
Kenneth Hoste <kenneth.hoste@ugent.be>
2016-03-01 15:14:37 +0100

    improve dry-run output for values based on all_pylibdirs

--- easybuild/easyblocks/l/libxml2.py
+++ easybuild/easyblocks/l/libxml2.py
@@ -58,6 +58,8 @@ class EB_libxml2(ConfigureMake, PythonPackage):
         # We will do the python bindings ourselves so force them off
         self.cfg.update('configopts', '--without-python')
         ConfigureMake.configure_step(self)
+        # make sure self.all_pylibdirs is defined properly
+        PythonPackage.set_pylibdirs(self)
 
     def build_step(self):
         """
