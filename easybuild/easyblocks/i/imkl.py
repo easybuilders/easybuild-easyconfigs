e8e4a0782200a48460da05edc3ef8d9f844dc83b
Alan <a.ocais@fz-juelich.de>
2016-03-11 15:43:23 +0100

    Only do patch once

--- easybuild/easyblocks/i/imkl.py
+++ easybuild/easyblocks/i/imkl.py
@@ -301,13 +301,12 @@ class EB_imkl(IntelBase):
                     except OSError, err:
                         raise EasyBuildError("Can't change to interface %s directory %s: %s", lib, intdir, err)
 
-                    if lib in cdftlibs and compopt == 'compiler=pgi':
+                    if lib in cdftlibs and compopt == 'compiler=pgi' and lib not in patched:
                         # Have to patch the makefile for this library
-                        if lib not in patched:
-                            run_cmd("sed -i s/gnu/pgi/g makefile", log_all=True, simple=True)
-                            run_cmd("sed -i s/gcc/pgcc/g makefile", log_all=True, simple=True)
-                            run_cmd("sed -i s/-Wall//g makefile", log_all=True, simple=True)
-                            patched += [lib]
+                        run_cmd("sed -i s/gnu/pgi/g makefile", log_all=True, simple=True)
+                        run_cmd("sed -i s/gcc/pgcc/g makefile", log_all=True, simple=True)
+                        run_cmd("sed -i s/-Wall//g makefile", log_all=True, simple=True)
+                        patched += [lib]
 
                     fullcmd = "%s %s" % (cmd, ' '.join(buildopts + extraopts))
                     res = run_cmd(fullcmd, log_all=True, simple=True)
