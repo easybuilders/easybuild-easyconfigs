40eea55153033642db4750989bc42d2134ff604c
Kenneth Hoste <kenneth.hoste@ugent.be>
2016-05-18 16:15:55 +0200

    bump version to 2.9.0.dev0

--- easybuild/easyblocks/__init__.py
+++ easybuild/easyblocks/__init__.py
@@ -44,7 +44,7 @@ from pkgutil import extend_path
 # recent setuptools versions will *TRANSFORM* something like 'X.Y.Zdev' into 'X.Y.Z.dev0', with a warning like
 #   UserWarning: Normalizing '2.4.0dev' to '2.4.0.dev0'
 # This causes problems further up the dependency chain...
-VERSION = LooseVersion('2.8.0')
+VERSION = LooseVersion('2.9.0.dev0')
 UNKNOWN = 'UNKNOWN'
 
 
