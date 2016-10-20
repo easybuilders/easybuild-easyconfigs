4e9b27f9d538cda03a7ff64ade872345da4d8fd7
Damian Alvarez <d.alvarez.mallon@fz-juelich.de>
2016-10-20 16:49:24 +0200

    Now actually working...

--- easybuild/easyblocks/e/esmf.py
+++ easybuild/easyblocks/e/esmf.py
@@ -88,12 +88,14 @@ class EB_ESMF(ConfigureMake):
 
             # C++
             netcdfcxx = get_software_root('netCDF-C++')
-            if netcdfcxx is None:
-                netcdfcxx = get_software_root('netCDF-C++4')
             if netcdfcxx:
                 netcdf_libs = ["-L%s/lib" % netcdfcxx] + netcdf_libs + ["-lnetcdf_c++"]
             else:
-                netcdf_libs.append('-lnetcdf_c++')
+                netcdfcxx = get_software_root('netCDF-C++4')
+                if netcdfcxx:
+                    netcdf_libs = ["-L%s/lib" % netcdfcxx] + netcdf_libs + ["-lnetcdf_c++4"]
+                else:
+                    netcdf_libs.append('-lnetcdf_c++')
 
             env.setvar('ESMF_NETCDF_LIBS', ' '.join(netcdf_libs))
 
