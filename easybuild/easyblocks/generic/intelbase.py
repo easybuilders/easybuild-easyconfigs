b37899f9ff989cde2a8dfdbccced353573fde5b0
Alan O'Cais <a.ocais@fz-juelich.de>
2017-02-24 10:14:55 +0100

    Make sure the licence envvar doesn't end up in the final module when we set `licensed = False`

--- easybuild/easyblocks/generic/intelbase.py
+++ easybuild/easyblocks/generic/intelbase.py
@@ -381,8 +381,9 @@ class IntelBase(EasyBlock):
         """Custom variable definitions in module file."""
         txt = super(IntelBase, self).make_module_extra()
 
-        txt += self.module_generator.prepend_paths(self.license_env_var, [self.license_file],
-                                                   allow_abs=True, expand_relpaths=False)
+        if self.cfg['licensed']:
+            txt += self.module_generator.prepend_paths(self.license_env_var, [self.license_file],
+                                                       allow_abs=True, expand_relpaths=False)
 
         if self.cfg['m32']:
             nlspath = os.path.join('idb', '32', 'locale', '%l_%t', '%N')
