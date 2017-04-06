bc729c965cfc20cc68579fff3a7a81c2e4197cd8
ppomorsk <ppomorsk@users.noreply.github.com>
2017-04-06 14:00:07 -0400

    Fix problems when usempi not defined in toolchain
    Many common build configurations of NAMD do not use MPI.  This change makes namd.py work with toolchains without MPI.

--- easybuild/easyblocks/n/namd.py
+++ easybuild/easyblocks/n/namd.py
@@ -59,7 +59,7 @@ class EB_NAMD(MakeCp):
 
         # complete Charm ++ and NAMD architecture string with compiler family
         comp_fam = self.toolchain.comp_family()
-        if self.toolchain.options['usempi']:
+        if 'usempi' in self.toolchain.options and self.toolchain.options['usempi']:
             charm_arch_comp = 'mpicxx'
         else:
             charm_arch_comps = {
