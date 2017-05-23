9240e23c364b34a7cd0f6f670449c811b875cb95
Ward Poelmans <wpoely86@gmail.com>
2017-05-23 13:44:38 +0200

    Fix remarks

--- easybuild/easyblocks/t/tkinter.py
+++ easybuild/easyblocks/t/tkinter.py
@@ -26,19 +26,15 @@
 EasyBuild support for building and installing Tkinter. This is the Python core
 module to use Tcl/Tk.
 
-@author: Stijn De Weirdt (Ghent University)
-@author: Dries Verdegem (Ghent University)
-@author: Kenneth Hoste (Ghent University)
-@author: Pieter De Baets (Ghent University)
-@author: Jens Timmerman (Ghent University)
 @author: Adam Huffman (The Francis Crick Institute)
 @author: Ward Poelmans (Free University of Brussels)
 """
-import glob
 import os
 import shutil
+import tempfile
 from distutils.version import LooseVersion
 
+from easybuild.easyblocks.generic.pythonpackage import det_pylibdir
 from easybuild.easyblocks.p.python import EB_Python
 from easybuild.tools.build_log import EasyBuildError
 from easybuild.tools.filetools import copy, mkdir, rmtree2
@@ -53,7 +49,7 @@ class EB_Tkinter(EB_Python):
     """
 
     def configure_step(self):
-        """Check for Tk"""
+        """Check for Tk before configuring"""
         tk = get_software_root('Tk')
         if not tk:
             raise EasyBuildError("Tk is mandatory to build Tkinter")
@@ -64,26 +60,18 @@ class EB_Tkinter(EB_Python):
         """Install python but only keep the bits we need"""
         super(EB_Tkinter, self).install_step()
 
-        tmpname = "eb-tmp"
-        tmpdir = os.path.join(self.installdir, tmpname)
-        mkdir(tmpdir)
+        tmpdir = tempfile.mkdtemp(dir=self.builddir)
 
-        pyver = '.'.join(self.version.split('.')[:2])
-        libdir = os.path.join(self.installdir, "lib", "python%s" % pyver)
-        tkdirs = ["lib-tk", "lib-dynload/_tkinter.so"]
+        pylibdir = os.path.join(self.installdir, os.path.dirname(det_pylibdir()))
+        tkparts = ["lib-tk", "lib-dynload/_tkinter.so"]
 
-        copy([os.path.join(libdir, x) for x in tkdirs], tmpdir)
+        copy([os.path.join(pylibdir, x) for x in tkparts], tmpdir)
 
-        delete_dirs = [x for x in os.listdir(self.installdir) if not x == tmpname]
+        rmtree2(self.installdir)
 
-        for deldir in delete_dirs:
-            rmtree2(os.path.join(self.installdir, deldir))
-
-        mkdir(libdir, parents=True)
-        shutil.move(os.path.join(tmpdir, tkdirs[0]), libdir)
-        shutil.move(os.path.join(tmpdir, os.path.basename(tkdirs[1])), libdir)
-
-        rmtree2(tmpdir)
+        mkdir(pylibdir, parents=True)
+        shutil.move(os.path.join(tmpdir, tkparts[0]), pylibdir)
+        shutil.move(os.path.join(tmpdir, os.path.basename(tkparts[1])), pylibdir)
 
     def sanity_check_step(self):
         """Custom sanity check for Python."""
@@ -93,19 +81,11 @@ class EB_Tkinter(EB_Python):
             tkinter = 'Tkinter'
         custom_commands = ["python -c 'import %s'" % tkinter]
 
-        pyver = 'python' + '.'.join(self.version.split('.')[:2])
         shlib_ext = get_shared_lib_ext()
-
-        # check whether _tkinter*.so is found, exact filename doesn't matter
-        tkinter_so = os.path.join(self.installdir, 'lib', pyver, '_tkinter*.' + shlib_ext)
-        tkinter_so_hits = glob.glob(tkinter_so)
-        if len(tkinter_so_hits) == 1:
-            self.log.info("Found exactly one _tkinter*.so: %s", tkinter_so_hits[0])
-        else:
-            raise EasyBuildError("Expected to find exactly one _tkinter*.so: %s", tkinter_so_hits)
+        pylibdir = os.path.dirname(det_pylibdir())
 
         custom_paths = {
-            'files': [],
+            'files': ['%s/_tkinter.%s' % (pylibdir, shlib_ext)],
             'dirs': ['lib']
         }
 
@@ -114,7 +94,7 @@ class EB_Tkinter(EB_Python):
     def make_module_extra(self):
         """Set PYTHONPATH"""
         txt = super(EB_Tkinter, self).make_module_extra()
-        pyver = '.'.join(self.version.split('.')[:2])
-        txt += self.module_generator.prepend_paths('PYTHONPATH', os.path.join("lib", "python%s" % pyver))
+        pylibdir = os.path.dirname(det_pylibdir())
+        txt += self.module_generator.prepend_paths('PYTHONPATH', pylibdir)
 
         return txt
