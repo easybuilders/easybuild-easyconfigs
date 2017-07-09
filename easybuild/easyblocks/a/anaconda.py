c31b4e38c202df8609a8f575e65b4b0e93dc38ab
Pablo Escobar <pescobar001@gmail.com>
2017-07-09 23:54:24 +0200

    removed ipython from sanity check

--- easybuild/easyblocks/a/anaconda.py
+++ easybuild/easyblocks/a/anaconda.py
@@ -70,7 +70,7 @@ class EB_Anaconda(Binary):
         Custom sanity check for Anaconda
         """
         custom_paths = {
-            'files': [os.path.join('bin', x) for x in ['2to3', 'conda', 'ipython', 'pydoc', 'python', 'sqlite3']],
+            'files': [os.path.join('bin', x) for x in ['2to3', 'conda', , 'pydoc', 'python', 'sqlite3']],
             'dirs': ['bin', 'etc', 'lib', 'pkgs'],
         }
         super(EB_Anaconda, self).sanity_check_step(custom_paths=custom_paths)
