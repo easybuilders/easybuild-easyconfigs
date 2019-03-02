dccb6e7e852812c33c96ad5834969e82196c730e
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-03-02 09:40:54 +0100

    restore checksums check for R-* easyconfigs

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -342,7 +342,7 @@ class EasyConfigTest(TestCase):
 
         # list of software for which checksums can not be required,
         # e.g. because 'source' files need to be constructed manually
-        whitelist = ['Kent_tools-*', 'MATLAB-*', 'R-*']
+        whitelist = ['Kent_tools-*', 'MATLAB-*']
 
         # the check_sha256_checksums function (again) creates an EasyBlock instance
         # for easyconfigs using the Bundle easyblock, this is a problem because the 'sources' easyconfig parameter
