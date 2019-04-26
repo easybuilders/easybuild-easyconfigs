79b9132236c5b9ad3e57743d5ea04638adeae2e5
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2019-04-26 12:52:53 +0200

    Use a more generic wrapper and use Core level CUDA.

--- easybuild/easyconfigs/g/Gctf/Gctf-wrapper.sh
+++ easybuild/easyconfigs/g/Gctf/Gctf-wrapper.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Wrap around Gctf to load the requrired toolchain so it can use the
-# expected version of CUDA
+# Wrap around Gctf to load the required toolchain so it can use the
+# expected version of CUDA.
 
 # Save the path to the install tree
 gctf_base=$EBROOTGCTF
 
 ml purge > /dev/null 2>&1
-ml GCC/5.4.0-2.26
-ml CUDA/8.0.61_375.26
+ml CUDA/#CUDAVER#
 
-exec $gctf_base/bin/Gctf-v1.06_sm_30_cu8.0_x86_64 "$@"
+exec $gctf_base/bin/#GCTF# "$@"
