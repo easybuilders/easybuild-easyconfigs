e1de056f0ea8823bdf46ab2218dab347bb3ecc05
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2019-04-26 15:55:22 +0200

    Use full "module" command instead of Lmod's "ml" shorthand.

--- easybuild/easyconfigs/m/MotionCor2/Motioncor2-wrapper.sh
+++ easybuild/easyconfigs/m/MotionCor2/Motioncor2-wrapper.sh
@@ -6,7 +6,7 @@
 # Save path to the install tree.
 mc_base=$EBROOTMOTIONCOR2
 
-ml purge > /dev/null 2>&1
-ml CUDA/#CUDAVER#
+module purge > /dev/null 2>&1
+module CUDA/#CUDAVER#
 
 exec $mc_base/bin/#MOTIONCOR2# "$@"
