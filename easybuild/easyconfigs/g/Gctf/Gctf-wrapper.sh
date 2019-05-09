d4b3fc265dd02755ce270d1f7458dc13100695e5
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2019-05-09 08:25:27 +0200

    Just unload CUDA in the Gctf-wrapper. The binary also needs libstdc++.so.6 so trust that there is at least a GCCcore loaded or that the OS has libstdc++ installed.

--- easybuild/easyconfigs/g/Gctf/Gctf-wrapper.sh
+++ easybuild/easyconfigs/g/Gctf/Gctf-wrapper.sh
@@ -6,7 +6,7 @@
 # Save the path to the install tree
 gctf_base=$EBROOTGCTF
 
-module purge > /dev/null 2>&1
+module unload CUDA > /dev/null 2>&1
 module CUDA/#CUDAVER#
 
 exec $gctf_base/bin/#GCTF# "$@"
