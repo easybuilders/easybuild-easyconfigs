ea22e455c490bec9daa770d3bffb5426fbeb9d90
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2019-05-09 11:14:29 +0200

    Only unload CUDA instead of doing module purge in MotionCor2 wrapper

--- easybuild/easyconfigs/m/MotionCor2/Motioncor2-wrapper.sh
+++ easybuild/easyconfigs/m/MotionCor2/Motioncor2-wrapper.sh
@@ -6,7 +6,7 @@
 # Save path to the install tree.
 mc_base=$EBROOTMOTIONCOR2
 
-module purge > /dev/null 2>&1
+module unload CUDA > /dev/null 2>&1
 module add CUDA/#CUDAVER#
 
 exec $mc_base/bin/#MOTIONCOR2# "$@"
