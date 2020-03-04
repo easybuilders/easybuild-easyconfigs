472501dc88c9f1d9b0f366a611b55d1cd3f98c74
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2020-03-04 09:27:51 +0100

    Split Slurm argument patches in two parts, a more generic and a HPC2N specific. Add explanation to the submit file template on how it works. Add comments to the env vars added to the module file.

--- easybuild/easyconfigs/r/RELION/relion_sbatch.sh
+++ easybuild/easyconfigs/r/RELION/relion_sbatch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH -J Relion
+# Unset XXX...XXX values will be empty, and SBATCH without argument is ignored.
 #SBATCH XXXaccountXXX
 #SBATCH XXXtimelimitXXX
 #SBATCH -n XXXmpinodesXXX
