b5a72d0ff698322aac80f8e514384454d5dfc4b6
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-12-16 10:42:24 +0100

    extend exceptions made for Pysam and TensorFlow 2.2.0 for medaka to decona (which depends on medaka)

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -280,12 +280,14 @@ class EasyConfigTest(TestCase):
                 # artic-ncov2019 requires medaka
                 ('1.13.1;', ['medaka-0.11.4-', 'medaka-0.12.0-', 'artic-ncov2019-2020.04.13']),
                 # medaka 1.1.* requires TensorFlow 2.2.0 (while other 2019b easyconfigs use TensorFlow 2.1.0 as dep);
-                # TensorFlow 2.2.0 is also used as a dep for Horovod 0.19.5
-                ('2.2.0;', ['medaka-1.1.[13]-', 'Horovod-0.19.5-']),
+                # TensorFlow 2.2.0 is also used as a dep for Horovod 0.19.5;
+                # decona 0.1.2 depends on medaka 1.1.3
+                ('2.2.0;', ['medaka-1.1.[13]-', 'Horovod-0.19.5-', 'decona-0.1.2']),
             ],
             # medaka 1.1.* requires Pysam 0.16.0.1,
-            # which is newer than what others use as dependency w.r.t. Pysam version in 2019b generation
-            'Pysam': [('0.16.0.1;', ['medaka-1.1.[13]-'])],
+            # which is newer than what others use as dependency w.r.t. Pysam version in 2019b generation;
+            # decona 0.1.2 depends on medaka 1.1.3
+            'Pysam': [('0.16.0.1;', ['medaka-1.1.[13]-', 'decona-0.1.2'])],
         }
         if dep in old_dep_versions and len(dep_vars) > 1:
             for key in list(dep_vars):
