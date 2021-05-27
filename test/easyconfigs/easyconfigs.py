34fd36b9b88580c1b17f0f63511054fa4548b043
afraz-ahmed <m.a.ahmed@bham.ac.uk>
2021-05-27 19:06:41 +0100

    upstream changes to easyconfigs.py and undo easyconfig changes

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -667,8 +667,8 @@ class EasyConfigTest(TestCase):
             whitelist_python_suffix = ['Amber-16-*-2018b-AmberTools-17-patchlevel-10-15.eb',
                                        'Amber-16-intel-2017b-AmberTools-17-patchlevel-8-12.eb']
             whitelisted = any(re.match(regex, ec_fn) for regex in whitelist_python_suffix)
-            has_python_dep = any(dep['name'] == 'Python' for dep in ec['dependencies']
-                                 if LooseVersion(dep['version']) < LooseVersion('3.8.6'))
+            has_python_dep = any(LooseVersion(dep['version']) < LooseVersion('3.8.6')
+                                 for dep in ec['dependencies'] if dep['name'] == 'Python')
             if has_python_dep and ec.name != 'Tkinter' and not whitelisted:
                 if not re.search(r'-Python-[23]\.[0-9]+\.[0-9]+', ec['versionsuffix']):
                     msg = "'-Python-%%(pyver)s' should be included in versionsuffix in %s" % ec_fn
@@ -679,8 +679,8 @@ class EasyConfigTest(TestCase):
                     else:
                         print('\nNote: Failed non-critical check: ' + msg)
             else:
-                has_recent_python3_dep = any(dep['name'] == 'Python' for dep in ec['dependencies']
-                                             if LooseVersion(dep['version']) >= LooseVersion('3.8.6'))
+                has_recent_python3_dep = any(LooseVersion(dep['version']) >= LooseVersion('3.8.6')
+                                             for dep in ec['dependencies'] if dep['name'] == 'Python')
                 if has_recent_python3_dep and re.search(r'-Python-3\.[0-9]+\.[0-9]+', ec['versionsuffix']):
                     msg = "'-Python-%%(pyver)s' should no longer be included in versionsuffix in %s" % ec_fn
                     failing_checks.append(msg)
