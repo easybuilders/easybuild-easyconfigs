b418ff517cc5058b0e570354772ac004e533be1e
Jasper Grimm <jasper.grimm@york.ac.uk>
2022-06-27 11:47:38 +0100

    drop minimap2, mappy dependencies to 2.22 add test exception for Pysam dependency version

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -521,7 +521,7 @@ class EasyConfigTest(TestCase):
             'Pysam': [
                 ('0.16.0.1;', ['medaka-1.2.[0]-', 'medaka-1.1.[13]-', 'medaka-1.4.3-', 'decona-0.1.2-',
                                'NGSpeciesID-0.1.1.1-']),
-                ('0.18.0;', ['WhatsHap-1.4-']),
+                ('0.18.0;', ['WhatsHap-1.4-', 'medaka-1.6.0-']),
             ],
             # OPERA requires SAMtools 0.x
             'SAMtools': [(r'0\.', [r'ChimPipe-0\.9\.5', r'Cufflinks-2\.2\.1', r'OPERA-2\.0\.6',
