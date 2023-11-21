5f088c75f0c7b63339a22cf3185b0b79899050db
Thomas Hoffmann <81254262+ThomasHoffmann77@users.noreply.github.com>
2023-11-21 16:42:19 +0100

    Update easyconfigs.py
    alt dep excp again

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -576,11 +576,8 @@ class EasyConfigTest(TestCase):
                                'NGSpeciesID-0.1.1.1-']),
                 ('0.18.0;', ['medaka-1.6.0-', 'NGSpeciesID-0.1.2.1-', 'WhatsHap-1.4-']),
             ],
-            'Rust': [
-                ('1.65.0;', ['Evcxr-REPL-0.14.2-', 'JupyterLab-4.0.3-', 'Python-3.10.8-', 'Transformers-4.30.2-',
-                             'fastparquet-2023.4.0-', 'maturin-1.1.0-', 'ripunzip-0.4.0', 'tokenizers-0.13.3-']),
-                ('1.70.0;', ['pydantic-2.3.0-']),
-            ],
+            # pydantic 2.3.0 requires Rust > 1.65 as builddependency
+            'Rust': [(r'1\.70\.0', [r'pydantic-2\.3\.0-foss-2022b'])],
             # OPERA requires SAMtools 0.x
             'SAMtools': [(r'0\.', [r'ChimPipe-0\.9\.5', r'Cufflinks-2\.2\.1', r'OPERA-2\.0\.6',
                                    r'CGmapTools-0\.1\.2', r'BatMeth2-2\.1'])],
