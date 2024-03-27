17e819ffcc782992418f5661a900313bd7c4805d
Thomas Hoffmann <81254262+ThomasHoffmann77@users.noreply.github.com>
2024-03-27 10:02:57 +0100

    Update easyconfigs.py
    test exception: use VisPy 0.14.1 for napari 0.4.19post1

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -631,6 +631,9 @@ class EasyConfigTest(TestCase):
             # for the sake of backwards compatibility, keep UCX-CUDA v1.11.0 which depends on UCX v1.11.0
             # (for 2021b, UCX was updated to v1.11.2)
             'UCX': [('1.11.0;', ['UCX-CUDA-1.11.0-'])],
+            # Napari 0.4.19post1 requires VisPy >=0.14.1 <0.15
+            'VisPy': [('0.14.1;',['napari-0.4.19post1-'])],
+            
             # WPS 3.9.1 requires WRF 3.9.1.1
             'WRF': [(r'3\.9\.1\.1', [r'WPS-3\.9\.1'])],
             # wxPython 4.2.0 depends on wxWidgets 3.2.0
