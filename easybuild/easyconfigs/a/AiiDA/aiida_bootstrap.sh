3b6627c82cdfceb4a87c31df609fb4a353f42052
crivella <davide.grassano@epfl.ch>
2024-05-03 16:51:33 +0200

    Functioning script

--- easybuild/easyconfigs/a/AiiDA/aiida_bootstrap.sh
+++ easybuild/easyconfigs/a/AiiDA/aiida_bootstrap.sh
@@ -5,8 +5,12 @@ module load PostgreSQL
 module load aiida-core
 
 LOGHEADER="== "
-PG_LOGHEADER="${LOGHEADER}PostgreSQL:"
 EB_CONFIG_DIR=$HOME/.eb_aiida
+
+#######################################################################
+# PostgreSQL
+#######################################################################
+PG_LOGHEADER="${LOGHEADER}PostgreSQL:"
 export PGDATA=${EB_CONFIG_DIR}/TEST
 PG_CONF=$PGDATA/postgresql.conf
 PG_PORT=25432
@@ -50,9 +54,26 @@ function database_get_set_pgdata {
     fi
 }
 
+function database_get_password {
+    if [ -f ${EB_CONFIG_DIR}/psql_password ]; then
+        PG_PASSWORD=$(cat ${EB_CONFIG_DIR}/psql_password)
+    fi
+}
+
+function database_get_set_password {
+    if [ -f ${EB_CONFIG_DIR}/psql_password ]; then
+        PG_PASSWORD=$(cat ${EB_CONFIG_DIR}/psql_password)
+    else
+        PG_PASSWORD=$(uuidgen)
+        echo $PG_PASSWORD > ${EB_CONFIG_DIR}/psql_password
+        chmod 600 ${EB_CONFIG_DIR}/psql_password
+    fi
+
+}
+
 function database_init {
     echo "${PG_LOGHEADER} Initializing database..."
-    read -r -s -p "${PG_LOGHEADER} Enter the database (new) password: " PG_PASSWORD
+    database_get_set_password
     initdb -D ${PGDATA} -A scram-sha-256 --pwfile=<(echo ${PG_PASSWORD})
 }
 
@@ -67,7 +88,14 @@ function database_start {
     fi
 }
 
+function database_cleanup_vars {
+    unset PGDATA
+    unset PG_PORT
+    unset PG_PASSWORD
+}
+
 function database_bootstrap {
+    echo "${PG_LOGHEADER} Bootstrapping..."
     if [ ! -d ${EB_CONFIG_DIR} ]; then
         mkdir -p ${EB_CONFIG_DIR}
     fi
@@ -80,13 +108,10 @@ function database_bootstrap {
     database_start
 }
 
-# database_bootstrap
-# if [ $? -ne 0 ]; then
-#     echo "${PG_LOGHEADER} failed to start server"
-#     exit 1
-# fi
-# echo "PASSWORD: $PG_PASSWORD"
 
+#######################################################################
+# RabbitMQ
+#######################################################################
 RMQ_LOGHEADER="${LOGHEADER}RabbitMQ:"
 RMQ_BASEDIR=${EB_CONFIG_DIR}/rabbitmq
 
@@ -96,10 +121,15 @@ export RABBITMQ_MNESIA_BASE=${RMQ_BASEDIR}/mnesia
 export RABBITMQ_LOG_BASE=${RMQ_BASEDIR}/logs
 export RABBITMQ_CONFIG_FILES=${RMQ_BASEDIR}/config
 export RABBITMQ_ADVANCED_CONFIG_FILE=${RMQ_BASEDIR}/advanced.config
-export RABBITMQ_NODE_IP_ADDRESS="localhost"
+# export RABBITMQ_NODE_IP_ADDRESS="localhost"
+export RABBITMQ_DEFAULT_USER=aiida
 
 function rabbitmq_set_port {
-    _RMQ_PORT=25672
+    if [ ! -z ${RABBITMQ_NODE_PORT} ]; then
+        _RMQ_PORT=${RABBITMQ_NODE_PORT}
+    else
+        _RMQ_PORT=25672
+    fi
     while [ $_RMQ_PORT -le 65535 ]; do
         ss -tln | grep -q ":$_RMQ_PORT " || break
         _RMQ_PORT=$((_RMQ_PORT + 1))
@@ -120,7 +150,15 @@ function rabbitmq_get_port {
 }
 
 function rabbitmq_set_dist_port {
-    _RMQ_PORT=25672
+    if [ ! -z ${RABBITMQ_DIST_PORT} ]; then
+        _RMQ_PORT=${RABBITMQ_DIST_PORT}
+    else
+        if [ ! -z ${RABBITMQ_NODE_PORT} ]; then
+            _RMQ_PORT=$((RABBITMQ_NODE_PORT + 1))
+        else
+            _RMQ_PORT=25673
+        fi
+    fi
     while [ $_RMQ_PORT -le 65535 ]; do
         ss -tln | grep -q ":$_RMQ_PORT " || break
         _RMQ_PORT=$((_RMQ_PORT + 1))
@@ -136,29 +174,217 @@ function rabbitmq_set_dist_port {
 
 function rabbitmq_get_dist_port {
     if [ -f ${EB_CONFIG_DIR}/rmq_dist_port ]; then
-        export RABBITMQ_DISTPORT=$(cat ${EB_CONFIG_DIR}/rmq_port)
+        export RABBITMQ_DIST_PORT=$(cat ${EB_CONFIG_DIR}/rmq_dist_port)
     fi
 }
 
 function rabbitmq_check_running {
-    rabbitmqctl status | grep -q "RabbitMQ is running"
+    rabbitmqctl status 2>&1 | grep -q "Node data directory: "
     if [ $? -ne 0 ]; then
         echo "${RMQ_LOGHEADER} server not running"
         return 1
     fi
+    echo "${RMQ_LOGHEADER} server running"
+    DATA=`rabbitmqctl status 2>&1`
+    echo "$DATA" | grep -q "Node data directory: ${RABBITMQ_MNESIA_BASE}/${RABBITMQ_NODENAME}@${HOSTNAME}"
+    if [ $? -ne 0 ]; then
+        echo "${RMQ_LOGHEADER} server not running on correct data directory"
+        exit 1
+    fi
+
+    _PORT=`echo "$DATA" | grep "amqp" | cut -d, -f2 | awk '{print $2}' | head -n 1`
+    if [ ! -z ${RABBITMQ_NODE_PORT} ] && [ ${_PORT} -ne ${RABBITMQ_NODE_PORT} ]; then
+        echo "${RMQ_LOGHEADER} server not running on correct PORT"
+        exit 1
+    else
+        export RABBITMQ_NODE_PORT=${_PORT}
+        echo "${RMQ_LOGHEADER} server running on PORT ${RABBITMQ_NODE_PORT}"
+        echo ${RABBITMQ_NODE_PORT} > ${EB_CONFIG_DIR}/rmq_port
+    fi
+    _DIST_PORT=`echo "$DATA" | grep "clustering" | cut -d, -f2 | awk '{print $2}' | head -n 1`
+    if [ ! -z ${RABBITMQ_DIST_PORT} ] && [ ${_DIST_PORT} -ne ${RABBITMQ_DIST_PORT} ]; then
+        echo "${RMQ_LOGHEADER} server not running on correct DIST_PORT"
+        exit 1
+    else
+        export RABBITMQ_DIST_PORT=${_DIST_PORT}
+        echo "${RMQ_LOGHEADER} server running on DIST_PORT ${RABBITMQ_DIST_PORT}"
+        echo ${RABBITMQ_DIST_PORT} > ${EB_CONFIG_DIR}/rmq_dist_port
+    fi
+
     return 0
+}
 
+function rabbitmq_get_password {
+    if [ -f ${EB_CONFIG_DIR}/rmq_password ]; then
+        export RABBITMQ_DEFAULT_PASS=$(cat ${EB_CONFIG_DIR}/rmq_password)
+    fi
+}
 
+function rabbitmq_get_set_password {
+    if [ -f ${EB_CONFIG_DIR}/rmq_password ]; then
+        export RABBITMQ_DEFAULT_PASS=$(cat ${EB_CONFIG_DIR}/rmq_password)
+    else
+        export RABBITMQ_DEFAULT_PASS=$(uuidgen)
+        echo $RABBITMQ_DEFAULT_PASS > ${EB_CONFIG_DIR}/rmq_password
+        chmod 600 ${EB_CONFIG_DIR}/rmq_password
+    fi
+
+}
+
+function rabbitmq_aiida_config {
+    if [ `grep 'consumer_timeout, undefined' $RABBITMQ_ADVANCED_CONFIG_FILE | wc -l` -eq 0 ]; then
+        echo "Adding consumer_timeout to $RABBITMQ_ADVANCED_CONFIG_FILE"
+        echo "[{rabbit, [{consumer_timeout, undefined}]}]." >> $RABBITMQ_ADVANCED_CONFIG_FILE
+    fi
+}
 
 function rabbitmq_bootstrap {
-    echo "${RMQ_LOGHEADER} starting server..."
+    echo "${RMQ_LOGHEADER} Bootstrapping..."
+
+    if [ ! -d ${RMQ_BASEDIR} ]; then
+        echo "${RMQ_LOGHEADER} creating directories..."
+        mkdir -p ${RABBITMQ_MNESIA_BASE}
+        mkdir -p ${RABBITMQ_LOG_BASE}
+        mkdir -p ${RABBITMQ_CONFIG_FILES}
+    fi
 
     rabbitmq_get_port
     rabbitmq_get_dist_port
 
+    rabbitmq_check_running
+    if [ $? -eq 0 ]; then
+        return 0
+    fi
+
+    rabbitmq_set_port
+    rabbitmq_set_dist_port
+    rabbitmq_get_set_password
+
+    rabbitmq_aiida_config
+
+    echo "${RMQ_LOGHEADER} starting server..."
     rabbitmq-server -detached
     if [ $? -ne 0 ]; then
         echo "${RMQ_LOGHEADER} failed to start server"
         exit 1
     fi
 }
+
+#######################################################################
+# AiiDA
+#######################################################################
+AIIDA_LOGHEADER="${LOGHEADER}AiiDA:"
+
+export AIIDA_PATH=${HOME}
+AIIDA_CONFIG_FILE=${AIIDA_PATH}/.aiida/config.json
+
+function aiida_create_profile {
+    read -r -p "${AIIDA_LOGHEADER} Enter email address (default to ${USER}@${HOSTNAME}.xz): " AIIDA_EMAIL
+    if [ -z ${AIIDA_EMAIL} ]; then
+        AIIDA_EMAIL=${USER}@${HOSTNAME}.xz
+    fi
+    read -r -p "${AIIDA_LOGHEADER} Enter first name (default to ${USER}): " AIIDA_FIRSTNAME
+    if [ -z ${AIIDA_FIRSTNAME} ]; then
+        AIIDA_FIRSTNAME=${USER}
+    fi
+    read -r -p "${AIIDA_LOGHEADER} Enter last name (default to ${USER}): " AIIDA_LASTNAME
+    if [ -z ${AIIDA_LASTNAME} ]; then
+        AIIDA_LASTNAME=${USER}
+    fi
+    read -r -p "${AIIDA_LOGHEADER} Enter institution (default to ${HOSTNAME}): " AIIDA_INSTITUTION
+    if [ -z ${AIIDA_INSTITUTION} ]; then
+        AIIDA_INSTITUTION=${HOSTNAME}
+    fi
+    read -r -p "${AIIDA_LOGHEADER} Enter data repository directory (default to ${EB_CONFIG_DIR}/aiida): " AIIDA_DIR
+    if [ -z ${AIIDA_DIR} ]; then
+        AIIDA_DIR=${EB_CONFIG_DIR}/aiida
+    fi
+
+    database_get_password
+    rabbitmq_get_password
+
+    echo "${AIIDA_LOGHEADER} Creating profile..."
+    verdi quicksetup \
+        --non-interactive \
+        --profile ${USER} \
+        --email ${AIIDA_EMAIL} \
+        --first-name ${AIIDA_FIRSTNAME} \
+        --last-name ${AIIDA_LASTNAME} \
+        --institution ${AIIDA_INSTITUTION} \
+        --db-host localhost \
+        --db-port ${PG_PORT} \
+        --su-db-name postgres \
+        --su-db-username ${USER} \
+        --su-db-password ${PG_PASSWORD} \
+        --broker-port ${RABBITMQ_NODE_PORT} \
+        --broker-username ${RABBITMQ_DEFAULT_USER} \
+        --broker-password ${RABBITMQ_DEFAULT_PASS}
+
+    if [ $? -ne 0 ]; then
+        echo "${AIIDA_LOGHEADER} failed to create profile"
+        exit 1
+    fi
+}
+
+function aiida_verify_db_port {
+    _AIIDA_PORT=$(verdi profile show ${USER} | grep "database_port" | awk '{print $2}')
+    if [ $_AIIDA_PORT -ne $PG_PORT ]; then
+        echo "${AIIDA_LOGHEADER} AiiDA database PORT mismatch, updating the profile..."
+        sed -i "s/: *${_AIIDA_PORT}/: ${PG_PORT}/" ${AIIDA_CONFIG_FILE}
+    fi
+}
+
+function aiida_verify_rmq_port {
+    _AIIDA_PORT=$(verdi profile show ${USER} | grep "broker_port" | awk '{print $2}')
+    if [ $_AIIDA_PORT -ne $RABBITMQ_NODE_PORT ]; then
+        echo "${AIIDA_LOGHEADER} AiiDA RabbitMQ PORT mismatch, updating the profile..."
+        sed -i "s/: *${_AIIDA_PORT}/: ${RABBITMQ_NODE_PORT}/" ${AIIDA_CONFIG_FILE}
+    fi
+}
+
+function aiida_verify_rabbitmq {
+    if [ "rabbitmqctl environment | grep consumer_timeout | grep undefined" != "" ]; then
+        echo "${AIIDA_LOGHEADER} consumer_timeout was properly set in RabbitMQ, disabling warnings..."
+        verdi config set warnings.rabbitmq_version false
+    else
+        echo "${AIIDA_LOGHEADER} consumer_timeout was wrongly set in RabbitMQ"
+        echo "${AIIDA_LOGHEADER}   Manually kill the RabbitMQ server and relaunch the bootstrap script"
+        exit 1
+    fi
+}
+
+function aiida_bootstrap {
+    echo "${AIIDA_LOGHEADER} Bootstrapping..."
+
+    if [ "`verdi profile list 2>&1 | grep 'no profiles'`" != "" ]; then
+        aiida_create_profile
+    fi
+
+    aiida_verify_rabbitmq
+
+    aiida_verify_db_port
+    aiida_verify_rmq_port
+}
+
+database_bootstrap
+if [ $? -ne 0 ]; then
+    echo "${PG_LOGHEADER} failed to start server"
+    exit 1
+fi
+echo "PASSWORD: $PG_PASSWORD"
+echo ""
+
+rabbitmq_bootstrap
+if [ $? -ne 0 ]; then
+    echo "${RMQ_LOGHEADER} failed to start server"
+    exit 1
+fi
+echo "PASSWORD: $RABBITMQ_DEFAULT_PASS"
+echo ""
+
+aiida_bootstrap
+if [ $? -ne 0 ]; then
+    echo "${AIIDA_LOGHEADER} failed to start server"
+    exit 1
+fi
+echo "AiiDA is ready to use"
