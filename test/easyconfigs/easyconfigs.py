8882e934332b4ba1b0c7ebde31eec069de0f2827
Kenneth Hoste <kenneth.hoste@ugent.be>
2024-10-01 21:10:59 +0200

    add check to catch PYTHONPATH being updated via modextrapaths with standard lib/python*/site-packages path

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -1764,6 +1764,21 @@ def template_easyconfig_test(self, spec):
             fail_msg += f"'{orig_val}' vs '{dumped_val}'"
             failing_checks.append(fail_msg)
 
+    # don't allow updating of $PYTHONPATH with standard lib/python*/site-packages path,
+    # since that's already taken care of by EasyBuild framework now
+    # (cfr. https://github.com/easybuilders/easybuild-framework/pull/4539)
+    modextrapaths = ec.get('modextrapaths', {})
+    regex = re.compile(r'^lib*/python[0-9]\.[0-9]+/site-packages$')
+    for key, value in modextrapaths.items():
+        value = resolve_template(value, ec.template_values)
+        if key == 'PYTHONPATH':
+            if isinstance(value, str):
+                value = [value]
+            if any(regex.match(x) for x in value):
+                fail_msg = "PYTHONPATH should not be specified in modextrapaths with standard path that matches "
+                fail_msg += f"'{regex.pattern}'"
+                failing_checks.append(fail_msg)
+
     if failing_checks:
         self.fail('Verification for %s failed:\n' % os.path.basename(spec) + '\n'.join(failing_checks))
 
