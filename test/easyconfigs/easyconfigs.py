5004545d5112102b2d9d7f061a239f95388bd167
Simon Pinches <Simon.Pinches@iter.org>
2025-07-30 11:28:45 +0200

    Update easyconfigs.py for MDSplus using Java/21

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -580,7 +580,8 @@ class EasyConfigTest(TestCase):
             # jax 0.2.24 is used as dep for AlphaFold 2.1.2 (other easyconfigs with foss/2021a use jax 0.3.9)
             'jax': [(r'0\.2\.24', [r'AlphaFold-2\.1\.2-foss-2021a'])],
             # Java 21 is used as dep for Octave 9.2.0 (other 2023b easyconfigs use Java 11)
-            'Java': [(r'21', [r'Octave-9\.2\.0'])],
+            # and MDSplus
+            'Java': [(r'21', [r'Octave-9\.2\.0', r'MDSplus-7\.1'])],
             # libxc 4.x is required by libGridXC
             # (Qiskit depends on PySCF), Elk 7.x requires libxc >= 5
             'libxc': [
