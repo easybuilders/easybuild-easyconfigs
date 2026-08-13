7eb974793c3cbb5d711ea1b381ea0afb011349af
Emik <phoenix0919mik@gmail.com>
2026-08-13 17:18:03 +0800

    Update test/easyconfigs/easyconfigs.py
    Co-authored-by: Davide Grassano <34096612+Crivella@users.noreply.github.com>

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -603,7 +603,10 @@ class EasyConfigTest(TestCase):
             'jax': [(r'0\.2\.24', [r'AlphaFold-2\.1\.2-foss-2021a'])],
             'Java': [
                 # AnnotSV 3.5.10 requires Java 17 (other 2023b easyconfigs use Java 11)
-                (r'17', [r'AnnotSV-3\.5\.10-foss-2023b\.eb$']),
+                (r'17', [
+                    # EC's that requires Java 17 in 2023b easyconfigs, while others use Java 11
+                    r'AnnotSV-3\.5\.10-foss-2023b', r'GATK-4\.6\.0\.0-GCCcore-13\.2\.0', r'Spark-3\.5\.4-foss-2023b',
+                ]),
                 # Java 21 is used by Octave 9.2.0, MDSplus 7.1, JPype 1.5.0, and IMAS-* modules
                 (r'21', [r'Octave-9\.2\.0', r'MDSplus-7\.1', r'JPype-1\.5\.0',
                          r'IMAS-.*-2023b', r'IMAS-.*-GCCcore-13\.2\.0']),
